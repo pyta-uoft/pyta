dbd0371d2a8f79b4e4d1f029133b879cd6349f98
hayleyZHL <hayleyyyo@gmail.com>
2016-05-09 14:43:37 -0400

    Update W0623_redefine_in_handler.py

--- examples/pylint/W0623_redefine_in_handler.py
+++ examples/pylint/W0623_redefine_in_handler.py
@@ -4,5 +4,4 @@
 
 class error_class(Exception):
     def __int__(self, value): # Default __init__ of Exceptions has been overridden.
-        self.value = value
-        
+        self.value = value 

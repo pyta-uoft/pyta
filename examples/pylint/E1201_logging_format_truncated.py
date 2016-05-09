492201e4642651c5b8a7580c8d3601561974824c
hayleyZHL <hayleyyyo@gmail.com>
2016-05-09 16:28:28 -0400

    Update E1201_logging_format_truncated.py

--- examples/pylint/E1201_logging_format_truncated.py
+++ examples/pylint/E1201_logging_format_truncated.py
@@ -6,4 +6,4 @@ import logging
 
 # String truncated on the next line.
 logging.basicConfig(format='%(asctime).40s %(message).40s', datefmt='%m/%d/%Y %I:%M:%S %p')
-logging.warning('is when this event was logged.')
+logging.warning('is when this event was logged.') 

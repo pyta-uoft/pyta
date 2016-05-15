db6231f1cc336e369d6983ed8741c243e881d20f
hayleyyyo@gmail.com <hayleyyyo@gmail.com>
2016-05-15 03:32:42 -0400

    Removed message W0640 file.

--- examples/pylint/W0640_cell_var_from_loop.py
+++
@@ -1,9 +0,0 @@
-"""pylint: Cell variable %s defined in loop A variable used in a
-closure is defined in a loop. This will result in all closures
-using the same value for the closed-over variable.
-
-"""
-
-for i in range(5):
-    def print_fun():
-        print(i) # Error on this line 

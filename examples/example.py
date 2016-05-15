c01d8135ad778cb3d5b9dc7137af9917feedd9af
JiaXin <jasminejx.wu@mail.utoronto.ca>
2016-05-15 19:11:10 -0400

    I/O_function_example

---
+++ examples/example.py
@@ -0,0 +1,11 @@
+"""This module is to test the I/O checker.
+"""
+
+
+def hello():
+    """Print a message to the user."""
+    name = input("What is your name?")
+    print('hello, ' + name)
+
+if __name__ == '__main__':
+    hello()

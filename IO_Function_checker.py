0d2a5d629cebf2bae19c0761a6b651047961ae50
JiaXin <jasminejx.wu@mail.utoronto.ca>
2016-05-16 16:32:21 -0400

    IO_Function_checker

--- IO_Function_checker.py
+++ IO_Function_checker.py
@@ -6,20 +6,28 @@ from pylint.interfaces import IAstroidChecker
 from pylint.checkers import BaseChecker
 from pylint.checkers.utils import check_messages
 
+FORBIDDEN_BUILTIN = ['input', 'print', 'open']
 
 class IOFunctionChecker(BaseChecker):
 
     __implements__ = IAstroidChecker
 
-    name = 'I/O-functions'
-    msgs = {'E9998': ('Used I/O function %s',
-                      'I/O-function-not-allowed',
-                      'Used when you use the I/O functions "print", "open" or'
+    name = 'IO-functions'
+    msgs = {'E9998': ('Used IO function %s',
+                      'IO-function-not-allowed',
+                      'Used when you use the IO functions "print", "open" or'
                       '"input". Pylint just try to discourage this usage. '
                       'That doesn\'t mean you can not use it !'),
            }
+    options = (('functions-not-allowed',
+                 {'default': FORBIDDEN_BUILTIN,
+                  'type': 'csv', 'metavar': '<builtin function names>',
+                  'help': 'List of builtins function names that should not be '
+                          'used, separated by a comma'}
+                 ),
+               )
 
-    @check_messages("I/O-function-not-allowed")
+    @check_messages("IO-function-not-allowed")
     def visit_call(self, node):
         if isinstance(node.func, astroid.Name):
             name = node.func.name
@@ -27,10 +35,10 @@ class IOFunctionChecker(BaseChecker):
             # locals nor globals scope)
             if not (name in node.frame() or name in node.root()):
                 # if name in self.config.io_functions:
-                if name == "print" or name == "input" or name == "open":
+                if name in self.config.functions_not_allowed:
                     args = "{} on line {}".format(name, node.lineno)
                     # add the message
-                    self.add_message('I/O-function-not-allowed', node=node, args=args)
+                    self.add_message('IO-function-not-allowed', node=node, args=args)
 
 
 def register(linter):

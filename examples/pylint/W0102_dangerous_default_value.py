976b96557ddf9f9fe4ba2a91a9edd9688af1fc97
Shweta Mogalapalli <shweta.mogalapalli@mail.utoronto.ca>
2016-05-16 01:46:51 -0400

    Modified the example for 'Dangerous default value' to a more clearer example

--- examples/pylint/W0102_dangerous_default_value.py
+++ examples/pylint/W0102_dangerous_default_value.py
@@ -13,3 +13,19 @@ def add(lst=[]):
     for item in lst:
         temp += item
     return temp
+
+
+def add1(lst=[]):
+    """ Adds five digits to the end of the list.
+
+    @type lst: list
+    @rtype: list
+    """
+    temp = 5
+    for i in range(temp):
+        lst.append(i)
+    return lst
+
+if __name__ == '__main__':
+    print(add1())
+    print(add1())

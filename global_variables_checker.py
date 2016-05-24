ae8bb1ca4d7efeb71c79e762c09bffd45a9847df
Shweta Mogalapalli <shweta.mogalapalli@mail.utoronto.ca>
2016-05-24 00:02:10 -0400

    Undid the changes I made here instead of on a new branch

--- global_variables_checker.py
+++ global_variables_checker.py
@@ -24,15 +24,6 @@ class GlobalVariablesChecker(BaseChecker):
         args = "you used the keyword 'nonlocal' on line {}".format(node.lineno)
         self.add_message('forbidden-global-variables', node=node, args=args)
 
-    def visit_assign(self, node):
-        if isinstance(node.frame(), astroid.scoped_nodes.Module):
-            # info = str(node.targets[0])
-            # x = info.split()
-            # print(x)
-            args = "you declared global variables on line {}".format(node.lineno)
-            self.add_message('forbidden-global-variables', node=node, args=args)
-
-
 def register(linter):
     """required method to auto register this checker"""
     linter.register_checker(GlobalVariablesChecker(linter))

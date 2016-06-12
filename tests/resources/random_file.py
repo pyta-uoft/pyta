2775417dfa2b15e78806f5489e6f394916a5151f
Chris K <christopher.koehler@mail.utoronto.ca>
2016-06-12 17:55:46 -0400

    Added unit tests for the documented functions

---
+++ tests/resources/random_file.py
@@ -0,0 +1,11 @@
+class MyTestClass:
+    """Some documentation"""
+
+    def __init__(self, a):
+        """Init docs"""
+        self.a = a
+
+
+def top_level_func():
+    """A top level function"""
+    return 5

bf89c768e48ccc2d1458a1086a0b281c06f21283
JiaXin <jasminejx.wu@mail.utoronto.ca>
2016-06-13 21:49:45 -0400

    change the example

--- examples/invalid_range_index_example.py
+++ examples/invalid_range_index_example.py
@@ -1,4 +1,6 @@
 for i in range(0):
     for j in range(0, 1, 3):
         for k in range(9, 1, -9):
-            k += 1
+            for m in range(0, 1):
+                for n in range(0, 1.1):
+                    k += 1

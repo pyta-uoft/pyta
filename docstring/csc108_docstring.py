49bc966595c64b61b7b9ae2c274151129cea493f
Chris K <christopher.koehler@mail.utoronto.ca>
2016-06-17 23:25:32 -0400

    Docstring parser for 108 operational fully and tested

--- docstring/csc108_docstring.py
+++ docstring/csc108_docstring.py
@@ -1,19 +1,17 @@
 """
-BNF:
-    basetype = ( "int" | "float" | "bool" | "str" | "obj" | "NoneType" )
-    tuple = "(" element [ "," element ]+ ")"
-    dict = "{" element [ "," element ]+ "}"
-    collection = ( "list" "of" element | "set" "of" element | "tuple" "of" tuple | "dict" "of" dict )
-    element = ( basetype | collection )
-    elements = element [ "," element ]+
-    type_contract = "(" elements ")" "->" elements
-    
-NOTE: Whitespace doesn't matter for symbols, but does for 'collection' elements.
+BNF for csc108 docstring:
+    set = "set" "of" element
+    dict = "dict" "of" "{" element "," element "}"
+    tuple = "tuple" "of" "(" elements ")"
+    list = "list" "of" element
+    collection = list | tuple | dict | set
+    element = collection | <ANY_STRING>
+    elements = element ("," element)*
+    typeContract = "(" elements ")" "->" element
 """
 
 _VALID_TOKENS = [',', '(', ')', '[', ']', '{', '}']
 _COLLECTION_TYPES = ['list', 'set', 'tuple', 'dict']
-_BASE_TYPES = ['int', 'float', 'bool', 'str', 'obj', 'NoneType']
 
 
 class NotA108DocstringException(Exception):
@@ -30,10 +28,9 @@ class DocNode:
     """Represents a node in a tree of nodes which can be analyzed to get the
     parsed elements of a docstring header.
     """
-    def __init__(self, node_type=None, val=''):
+    def __init__(self, node_type=None):
         self.children = []
         self.node_type = node_type
-        self.val = val
 
 
 def tokenize_docstring(docstr):
@@ -86,17 +83,29 @@ class DocstringParser:
     Can throw either NotA108DocstringException or UnexpectedToken from the
     constructor."""
     def __init__(self, entire_docstring):
+        """Creates an object that will have the entire parsed docstring set in
+        the args_node and return_node DocNode objects.
+        entire_docstring: The docstring to parse, which does not have to be a
+        single line.
+        Raises UnexpectedToken if the tokenizer detects an invalid token.
+        Raises NotA108DocstringException if the docstring is not in a form
+        that can be parsed properly (implies malformed docstring).
+        """
         docstring_header = trim_docstring_down_to_type_contract(entire_docstring)
-        self.tokens = tokenize_docstring(docstring_header)
-        self.markers = []
-        self.token_index = 0
         self.args_node = DocNode()
         self.return_node = DocNode()
-        self.root_doc_node = DocNode()
-        self.root_doc_node.children = [self.args_node, self.return_node]
-        self.parse_type_contract()
+        self.node_stack = [self.args_node]
+        self.tokens = tokenize_docstring(docstring_header)
+        self.token_index = 0
+        try:
+            self.parse_type_contract()
+        except IndexError:
+            raise NotA108DocstringException
 
     def parse_type_contract(self):
+        """Parses the entire type contract, of which it will have raised an
+        exception or fully parsed the docstring.
+        """
         self.consume_token_type_or_error('(')
         self.parse_elements_until_right_paren()
         self.consume_token_type_or_error(')')
@@ -104,79 +113,137 @@ class DocstringParser:
         self.parse_elements_until_end()
         
     def parse_elements_until_right_paren(self):
-        while self.tokens[self.token_index] != ')':
-            # TODO - Start a new element
+        """Parses the docstring data to the left of the arrow and inside the
+        brackets. Raises an exception if it is not a proper docstring.
+        """
+        if self.get_current_token() == ')':
+            return
+        self.parse_element()
+        while self.get_current_token() == ',':
+            self.consume_token_type_or_error(',')
             self.parse_element()
-            while self.tokens[self.token_index] == ',':
-                self.consume_token_type_or_error(',')
-                # TODO - Start a new element
-                self.parse_element()
 
     def parse_elements_until_end(self):
-        while self.token_index < len(self.tokens):
-            # TODO - Start a new element
-            self.parse_element()
+        """Parses the results side of the docstring. Raises an exception if it
+        is not a proper docstring.
+        """
+        self.node_stack = [self.return_node]
+        self.parse_element()
+        self.raise_if_not_all_tokens_consumed()
         
     def parse_element(self):
-        if not self.parse_collection():
-            if not self.parse_base_type():
-                raise NotA108DocstringException()
+        """Parses an element recursively in a docstring. Raises an exception
+        if it is not a proper docstring.
+        """
+        node_type = self.get_current_token()
+        self.advance_to_next_token()
+        if node_type in _COLLECTION_TYPES:
+            self.consume_token_type_or_error('of')
+            if node_type in ['list', 'set']:
+                self.parse_list_or_set(node_type)
+            elif node_type == 'dict':
+                self.parse_dict()
+            elif node_type == 'tuple':
+                self.parse_tuple()
+            else:
+                raise NotA108DocstringException('Unexpected collection: ' + node_type)
+        else:
+            self.add_as_child_node(DocNode(node_type))
 
-    def parse_collection(self):
-        if self.tokens[self.token_index] == 'list':
-            # TODO - is a list
-            self.token_index += 1
-            self.consume_token_type_or_error('of')
-            self.parse_element()
-            return True
-        elif self.tokens[self.token_index] == 'set':
-            # TODO - is a set
-            self.token_index += 1
-            self.consume_token_type_or_error('of')
-            self.parse_element()
-            return True
-        elif self.tokens[self.token_index] == 'tuple':
-            # TODO - is a tuple
-            self.token_index += 1
-            self.consume_token_type_or_error('of')
-            self.parse_tuple()
-            return True
-        elif self.tokens[self.token_index] == 'dict':
-            # TODO - is a dict
-            self.token_index += 1
-            self.consume_token_type_or_error('of')
-            self.parse_dict()
-            return True
-        return False
+    def parse_list_or_set(self, node_type):
+        """Parses a list or set. Raises an exception if it is not a proper
+        docstring.
+        node_type: The node type to be set (should be 'list' or 'set').
+        """
+        node = DocNode(node_type)
+        self.add_as_child_node(node)
+        self.push_node_on_stack(node)
+        self.parse_element()
+        self.pop_node_stack()
 
     def parse_dict(self):
+        """Parses a dictionary element recursively. Raises an exception if it
+        is not a proper docstring.
+        """
         self.consume_token_type_or_error('{')
-        self.parse_element()
+        node = DocNode('dict')
+        self.add_as_child_node(node)
+        self.push_then_parse_then_pop(node)
         self.consume_token_type_or_error(',')
-        self.parse_element()
+        self.push_then_parse_then_pop(node)
         self.consume_token_type_or_error('}')
 
     def parse_tuple(self):
+        """Parses a tuple recursively. Raises an exception if it is not a
+        proper docstring.
+        """
         self.consume_token_type_or_error('(')
-        self.parse_element()
-        while self.tokens[self.token_index] == ',':
+        node = DocNode('tuple')
+        self.add_as_child_node(node)
+        self.push_then_parse_then_pop(node)
+        while self.get_current_token() == ',':
             self.consume_token_type_or_error(',')
-            self.parse_element()
+            self.push_then_parse_then_pop(node)
         self.consume_token_type_or_error(')')
 
-    def parse_base_type(self):
-        if self.tokens[self.token_index] not in _BASE_TYPES:
-            return False
-        # TODO - Convert to type and store in DocNode
-        return True
-
     def consume_token_type_or_error(self, token_str):
+        """Consumes the token that is provided in the argument. If the token
+        is not found, then it will throw a NotA108DocstringException. The
+        stream will be advanced.
+        token_str: The string that should be found on the current token in the
+        stream.
+        """
         if self.token_index >= len(self.tokens):
             raise NotA108DocstringException('Expected "%s", terminated early' % token_str)
-        token = self.tokens[self.token_index]
+        token = self.get_current_token()
         if token != token_str:
             raise NotA108DocstringException('Expected "%s", got "%s" instead' % (token_str, token))
+        self.advance_to_next_token()
+
+    def push_then_parse_then_pop(self, node):
+        """A convenience method that will push the node onto the stack, then
+        parse the element recursively, then pop this node off. This is used to
+        push a node that has child definitions onto the stack so it is
+        assigned the children appropriately, and then cleans up the stack.
+        node: The DocNode to go on the stack.
+        """
+        self.push_node_on_stack(node)
+        self.parse_element()
+        self.pop_node_stack()
+
+    def get_current_token(self):
+        """Gets the current token."""
+        return self.tokens[self.token_index]
+
+    def advance_to_next_token(self):
+        """Advances to the next token. Note: This does not throw an exception
+        if there is not a following token, the pointer is incremented."""
         self.token_index += 1
+
+    def add_as_child_node(self, node):
+        """Adds a node as a child to the last element on the node stack.
+        node: The node to add.
+        """
+        self.node_stack[-1].children.append(node)
+
+    def push_node_on_stack(self, node):
+        """Pushes a node onto the stack.
+        node: The DocNode to push onto the stack.
+        """
+        self.node_stack.append(node)
+
+    def pop_node_stack(self):
+        """Pops a node off of the stack."""
+        self.node_stack.pop(-1)
+
+    def raise_if_not_all_tokens_consumed(self):
+        """Checks to make sure all the tokens have been consumed, which is
+        required for the final parsing step since the user may have added
+        additional tokens that make it an invalid string.
+        Example: '(int) -> str bool' is invalid.
+        """
+        if self.token_index < len(self.tokens):
+            raise NotA108DocstringException
 
     
 def trim_docstring_down_to_type_contract(full_docstring):
@@ -207,7 +274,7 @@ def trim_docstring_down_to_type_contract(full_docstring):
         # assume there is supposed to be a space to replace the new line.
         # Since the parser will skip any extra space, this is safe to do.
         merged_docstring += stripped_line + ' '
-    return merged_docstring[:-1]  # Trim last space (TODO - do this better)
+    return merged_docstring[:-1]  # Trim last space (TODO - do this better...)
 
 
 def parse_csc108_docstring(full_docstring):
--- tests/test_csc108_docstring.py
+++ tests/test_csc108_docstring.py
@@ -1,5 +1,5 @@
+from csc108_docstring import *
 import unittest
-from docstring.csc108_docstring import *
 
 
 class CSC108DocstringTest(unittest.TestCase):
@@ -37,6 +37,140 @@ class CSC108DocstringTest(unittest.TestCase):
             tokenize_docstring('(int +) -> bool')
             self.fail()  # TODO - is there a better way? Like an expected exception decorator?
         except UnexpectedToken:
+            pass
+
+
+class DocstringParserTest(unittest.TestCase):
+    def test_simple_docstring(self):
+        dsp = DocstringParser('(int) -> bool')
+        self.assertEqual(1, len(dsp.args_node.children))
+        self.assertEqual('int', dsp.args_node.children[0].node_type)
+        self.assertEqual(1, len(dsp.return_node.children))
+        self.assertEqual('bool', dsp.return_node.children[0].node_type)
+
+    def test_empty_docstring(self):
+        dsp = DocstringParser('() -> NoneType')
+        self.assertEqual(0, len(dsp.args_node.children))
+        self.assertEqual(1, len(dsp.return_node.children))
+        self.assertEqual('NoneType', dsp.return_node.children[0].node_type)
+
+    def test_multi_arg_docstring(self):
+        dsp = DocstringParser('(int, str) -> NoneType')
+        self.assertEqual(2, len(dsp.args_node.children))
+        self.assertEqual('int', dsp.args_node.children[0].node_type)
+        self.assertEqual('str', dsp.args_node.children[1].node_type)
+        self.assertEqual(1, len(dsp.return_node.children))
+        self.assertEqual('NoneType', dsp.return_node.children[0].node_type)
+
+    def test_list_docstring(self):
+        dsp = DocstringParser('(set of bool) -> list of int')
+        self.assertEqual(1, len(dsp.args_node.children))
+        child = dsp.args_node.children[0]
+        self.assertEqual('set', child.node_type)
+        self.assertEqual(1, len(child.children))
+        self.assertEqual('bool', child.children[0].node_type)
+        self.assertEqual(1, len(dsp.return_node.children))
+        child = dsp.return_node.children[0]
+        self.assertEqual('list', child.node_type)
+        self.assertEqual(1, len(child.children))
+        self.assertEqual('int', child.children[0].node_type)
+
+    def test_dict_tuple_docstring(self):
+        dsp = DocstringParser('(dict of {int, str}) -> tuple of (int, float, int)')
+        self.assertEqual(1, len(dsp.args_node.children))
+        child = dsp.args_node.children[0]
+        self.assertEqual('dict', child.node_type)
+        self.assertEqual(2, len(child.children))
+        self.assertEqual('int', child.children[0].node_type)
+        self.assertEqual('str', child.children[1].node_type)
+        self.assertEqual(1, len(dsp.return_node.children))
+        child = dsp.return_node.children[0]
+        self.assertEqual('tuple', child.node_type)
+        self.assertEqual(3, len(child.children))
+        self.assertEqual('int', child.children[0].node_type)
+        self.assertEqual('float', child.children[1].node_type)
+        self.assertEqual('int', child.children[2].node_type)
+
+    def test_nested_collections(self):
+        dsp = DocstringParser('(list of dict of {int, str}) -> int')
+        self.assertEqual(1, len(dsp.args_node.children))
+        child = dsp.args_node.children[0]
+        self.assertEqual('list', child.node_type)
+        child = child.children[0]
+        self.assertEqual(2, len(child.children))
+        self.assertEqual('int', child.children[0].node_type)
+        self.assertEqual('str', child.children[1].node_type)
+        self.assertEqual(1, len(dsp.return_node.children))
+        self.assertEqual('int', dsp.return_node.children[0].node_type)
+
+    def test_malformed_docstring_no_end(self):
+        try:
+            DocstringParser('() -> ')
+            self.fail()
+        except NotA108DocstringException:
+            pass
+
+    def test_malformed_docstring_missing_of(self):
+        try:
+            DocstringParser('(list int) -> int')
+            self.fail()
+        except NotA108DocstringException:
+            pass
+
+    def test_malformed_docstring_extra_basetype(self):
+        try:
+            DocstringParser('(int, str bool) -> bool')
+            self.fail()
+        except NotA108DocstringException:
+            pass
+
+    def test_malformed_docstring_premature_termination(self):
+        try:
+            DocstringParser('(int, str')
+            self.fail()
+        except NotA108DocstringException:
+            pass
+
+    def test_malformed_docstring_missing_arrow(self):
+        try:
+            DocstringParser('(int, str bool) -> int')
+            self.fail()
+        except NotA108DocstringException:
+            pass
+
+    def test_malformed_docstring_bad_dict(self):
+        try:
+            DocstringParser('(dict of {int}) -> bool')
+            self.fail()
+        except NotA108DocstringException:
+            pass
+
+    def test_malformed_docstring_empty_tuple(self):
+        try:
+            DocstringParser('(tuple of ()) -> bool')
+            self.fail()
+        except NotA108DocstringException:
+            pass
+
+    def test_malformed_docstring_tuple_missing_arg(self):
+        try:
+            DocstringParser('(tuple of (int, str, )) -> bool')
+            self.fail()
+        except NotA108DocstringException:
+            pass
+
+    def test_malformed_docstring_multi_return_args(self):
+        try:
+            DocstringParser('(int) -> bool, int')
+            self.fail()
+        except NotA108DocstringException:
+            pass
+
+    def test_malformed_docstring_empty_string(self):
+        try:
+            DocstringParser('')
+            self.fail()
+        except NotA108DocstringException:
             pass
 
 

3e5327cbc8fdb3260918628e6b66b3e4b4e9f93c
hayleyyyo@gmail.com <hayleyyyo@gmail.com>
2016-07-04 13:36:05 -0400

    trying to find class types in typing function.
    not yet finished

--- type_inference_visitor.py
+++ type_inference_visitor.py
@@ -1,6 +1,7 @@
 from astroid.transforms import TransformVisitor
 import astroid
 import warnings
+import typing
 
 
 class TypeVisitor(TransformVisitor):
@@ -19,61 +20,56 @@ class TypeVisitor(TransformVisitor):
                 setattr(node, field, visited)
         return self._transform(node)
 
-    def _transform(self, node):
-        """Call matching transforms for the given node if any and return the
-        transformed node.
-        """
-        node_class = node.__class__
-        if node_class not in self.transforms:
-            # If the node has no transfrom functions, directly return the
-            # node type.
-            set_const(node)
-            return node
-        # transforms collects all the transformation functions for node_class.
-        transforms = self.transforms[node_class]
-        # making a copy of the original node.
-        original_node = node
-        for transform_func, predicate in transforms:
-            if predicate is None or predicate(node):
-                transform_node = transform_func(node)
-                if transform_node is not None:
-                    if node is not original_node:
-                        warnings.warn('node %s already got substituted ' %
-                                      node)
-                    node = transform_node
-
-        print('Visiting node ' + str(node))
-        print('Type constraints:' + str(node.type_constraints))
-        return node
-
 
 def set_const(node):
     """Populate type constraints for astroid nodes."""
     if isinstance(node, astroid.Const):
-        # # astroid.Const represent a constant node like num/str/bool/None/bytes.
-        # if isinstance(node.parent, astroid.UnaryOp):
-        #     print(node.value)
-        #     Expr(value=astroid.UnaryOp(op=USub(), operand=Name(id='a', ctx=Load())))
+        # astroid.Const represent a constant node like num/str/bool/None/bytes.
         node.type_constraints = [type(node.value)]
-    elif isinstance(node, astroid.Tuple):
-        node.type_constraints = [type(())]
-    elif isinstance(node, astroid.List):
-        node.type_constraints = [type([])]
-    elif isinstance(node, astroid.Dict):
-        node.type_constraints = [type({})]
+    else:
+        warnings.warn('node %s is not const type.' % node)
 
-    elif isinstance(node, astroid.BinOp):
+
+def set_tuple(node):
+    if isinstance(node, astroid.Tuple):
+        node.type_constraints = [type(())]
+    else:
+        warnings.warn('node %s is not tuple type.' % node)
+
+
+def set_list(node):
+    if isinstance(node, astroid.List):
+        node.type_constraints = [type([])]
+    else:
+        warnings.warn('node %s is not list type.' % node)
+
+
+def set_dict(node):
+    if isinstance(node, astroid.Dict):
+        node.type_constraints = [type({})]
+    else:
+        warnings.warn('node %s is not dict type.' % node)
+
+
+def set_binop(node):
+    if isinstance(node, astroid.BinOp):
         op = node.op
         left_operand = node.left.value
         right_operand = node.right.value
         result = eval(str(left_operand) + op + str(right_operand))
         node.type_constraints = [type(result)]
+    else:
+        warnings.warn('node %s is not binary operator type.' % node)
 
-    elif isinstance(node, astroid.UnaryOp):
+
+def set_unaryop(node):
+    if isinstance(node, astroid.UnaryOp):
         op = node.op
         operand = node.operand.value
         result = eval(op + str(operand))
         node.type_constraints = [type(result)]
+    else:
+        warnings.warn('node %s is not unary operator type.' % node)
 
 
 if __name__ == '__main__':
@@ -82,12 +78,14 @@ if __name__ == '__main__':
     type_visitor = TypeVisitor()
     # Registering a transform function to astroid nodes.
     type_visitor.register_transform(astroid.Const, set_const)
-    type_visitor.register_transform(astroid.List, set_const)
-    type_visitor.register_transform(astroid.Tuple, set_const)
-    type_visitor.register_transform(astroid.Dict, set_const)
-    type_visitor.register_transform(astroid.BinOp, set_const)
-    type_visitor.register_transform(astroid.UnaryOp, set_const)
+    type_visitor.register_transform(astroid.List, set_list)
+    type_visitor.register_transform(astroid.Tuple, set_tuple)
+    type_visitor.register_transform(astroid.Dict, set_dict)
+    type_visitor.register_transform(astroid.BinOp, set_binop)
+    type_visitor.register_transform(astroid.UnaryOp, set_unaryop)
     with open('examples/type_inference/const.py') as f:
         content = f.read()
     module = astroid.parse(content)
     type_visitor.visit(module)
+    # print('Visiting node ' + str(node))
+    # print('Type constraints:' + str(node.type_constraints))

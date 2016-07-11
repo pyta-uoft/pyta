1e81c189d71d3be3fc94a9ae3c65d76c60780ce3
Nigel Fong <nigel.fong@mail.utoronto.ca>
2016-07-11 12:45:19 -0400

    file had wrong contents by mistake

--- nodes/ExtSlice.py
+++ nodes/ExtSlice.py
@@ -1,22 +1,6 @@
-def fun_testing():  # correct Arguments() 16
-    """Stringy"""
+class Foo(object):
+    def __getitem__(self, item):
+        return item
 
-    x = 1  # correct Assign() 4-9, Const() 8-9
-
-    for i in range(4):  # correct
-        pass  # correct
-
-    try:
-        if 3 is not None:
-            pass
-        elif 2 is not None:  # 27 is RHS of colon.
-            fun_testing()
-        else:
-            pass
-        x = 2  # correct
-    except:  # correct
-        return False  # correct
-    else:
-        pass
-    finally:
-        pass
+foo = Foo()
+print(foo[1, 2:3])  # (1, slice(2, 3, None))

0b1bccf7c0186f0bfc02af264d1eae4ce90ac78b
Shweta Mogalapalli <shweta.mogalapalli@mail.utoronto.ca>
2016-07-17 23:08:41 -0400

    Added the template file

--- reporters/html_reporter.py
+++ reporters/html_reporter.py
@@ -9,7 +9,7 @@ class HTMLReporter(PlainReporter):
     # Override this method
     def print_message_ids(self):
         # Sort the messages.
-        self.sort_messages()
+        self._messages.sort(key=lambda s: s[0])
 
         #template = Environment(keep_trailing_newline=True, autoescape=False).from_string(HTML)
         template = Environment().get_template('template.txt')

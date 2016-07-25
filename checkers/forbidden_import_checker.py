e3ecad3938943c61a53dd0a05899040a5482259c
Shweta Mogalapalli <shweta.mogalapalli@mail.utoronto.ca>
2016-07-25 13:01:53 -0400

    Modified the forbidden_import_checker to allow certain modules to be imported through setting options in the .pylintrc file

--- checkers/forbidden_import_checker.py
+++ checkers/forbidden_import_checker.py
@@ -8,22 +8,35 @@ from pylint.checkers.utils import check_messages
 class ForbiddenImportChecker(BaseChecker):
     __implements__ = IAstroidChecker
 
-    name = 'custom'
+    name = 'forbiddenimport'
     msgs = {'E9999':
                 ('You may not import any modules - you imported \033[4;34m%s\033[0m on line %s.',
                  'forbidden-import',
                  'Used when you use import')}
-    options = ()
+    options = (('allowed_import_modules',
+                {'default': (),
+                 'type': 'csv',
+                 'metavar': '<modules>',
+                 'help': 'Allowed modules to be imported.'}
+               ),)
+
     # this is important so that your checker is executed before others
     priority = -1
 
     @check_messages("forbidden-import")
     def visit_import(self, node):
         """visit and Import node"""
-        self.add_message('forbidden-import',
-                         node=node,
-                         args=(', '.join(map(lambda x: x[0], node.names)),
-                               node.lineno))
+        temp = []
+        for name in node.names:
+            if name[0] not in self.config.allowed_import_modules:
+                temp.append(name)
+        if temp != []:
+            self.add_message('forbidden-import', node=node, args=(', '.join(map(lambda x: x[0], temp)), node.lineno))
+
+        #self.add_message('forbidden-import',
+        #                 node=node,
+        #                 args=(', '.join(map(lambda x: x[0], node.names)),
+        #                       node.lineno))
 
     @check_messages("forbidden-import")
     def visit_call(self, node):
@@ -33,10 +46,12 @@ class ForbiddenImportChecker(BaseChecker):
             # locals nor globals scope)
             if not (name in node.frame() or name in node.root()):
                 if name == "__import__":
-                    args = (node.args[0].as_string(), node.lineno)
-                    # add the message
-                    self.add_message('forbidden-import', node=node,
-                                     args=args)
+                    if node.args[0].value not in self.config.allowed_import_modules:
+                        args = (node.args[0].value, node.lineno)
+                        #args = (node.args[0].as_string(), node.lineno)
+                        # add the message
+                        self.add_message('forbidden-import', node=node,
+                                         args=args)
 
 
 def register(linter):

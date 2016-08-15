845ae7a52e92a393a9d1e05609213d4a922778cf
Chris K <christopher.koehler@mail.utoronto.ca>
2016-08-15 07:26:04 -0400

    Functioning changes (not complete, needs further cleanup)

--- docstring/csc108_docstring.py
+++ docstring/csc108_docstring.py
@@ -1,7 +1,22 @@
 import typing
-from funcparserlib.parser import a, forward_decl, finished, many, maybe, skip, oneplus, some
+import re
+from funcparserlib.parser import forward_decl, finished, many, maybe, skip, oneplus, some
 from tokenize import generate_tokens
 from io import StringIO
+
+
+def flatten(nested_list):
+    output_list = []
+    recursively_flatten(nested_list, output_list)
+    return output_list
+
+
+def recursively_flatten(item, output_list):
+    if not isinstance(item, list) and not isinstance(item, tuple):
+        output_list.append(item)
+        return
+    for x in item:
+        recursively_flatten(x, output_list)
 
 
 def a_str(s):
@@ -13,6 +28,8 @@ def skip_str(s):
 
 
 def combine_elements_to_list(data):
+    if len(data) <= 1:
+        return data
     output_list = [x for x in data[0]]
     output_list.append(data[-1])
     return output_list
@@ -30,10 +47,9 @@ def compile_dict_type(data):
     return typing.Dict[data[1], data[2]]
 
 
-# Despite the data being a list of class types, Tuple[...] will not accept a
-# list, but will accept a tuple() that was converted from a list.
 def compile_tuple_type(data):
-    return typing.Tuple[tuple(data[-1])]
+    flat_data = flatten(data[1:])
+    return typing.Tuple[tuple(flat_data)]
 
 
 def to_simple_type(token):
@@ -53,42 +69,37 @@ def to_simple_type(token):
     return s
 
 
-# -----------------------------------------------------------------------------
-
 element = forward_decl()
 elements = forward_decl()
-
 skip_of = skip_str('of')
-any_class = some(lambda x: True) >> to_simple_type
-
+any_class = some(lambda x: re.match('[a-zA-Z0-9_]+', x.string)) >> to_simple_type
 set_parser = (a_str('set') + skip_of + element) >> compile_set_type
-
 list_parser = (a_str('list') + skip_of + element) >> compile_list_type
-
 dict_parser = (a_str('dict') + skip_of + skip_str('{') + element + skip_str(',') + element + skip_str('}')) >> \
               compile_dict_type
-
-tuple_parser = (a_str('tuple') + skip_of + skip_str('(') + (elements >> combine_elements_to_list) + skip_str(')')) >> \
-               compile_tuple_type
-
+tuple_parser = (a_str('tuple') + skip_of + skip_str('(') + elements + skip_str(')')) >> compile_tuple_type
 element.define(set_parser | list_parser | dict_parser | tuple_parser | any_class)
 elements.define(maybe(many(element + skip_str(','))) + element)
+docstring_parser = skip_str('(') + maybe(elements) + skip_str(')') + skip_str('->') + element
 
-# TODO - Need to strip a [] leftover if there's one element at the beginning.
-docstring_parser = skip_str('(') + elements + skip_str(')') + skip_str('->') + element
+non_right_arrow = some(lambda x: x != '>')
+single_arrow = a_str('>') + non_right_arrow
+double_arrow = a_str('>>') + non_right_arrow
+docstring_description = oneplus(non_right_arrow | single_arrow | double_arrow) >> to_str
 
-# -----------------------------------------------------------------------------
+docstring_doctest = a_str('>>>') + (oneplus(some(is_valid_char)) >> to_str)
 
-# non_right_arrow = some(lambda x: x != '>')
-# single_arrow = a_str('>') + non_right_arrow
-# double_arrow = a_str('>>') + non_right_arrow
-# docstring_description = oneplus(non_right_arrow | single_arrow | double_arrow) >> to_str
+entire_docstring = maybe(docstring_parser) + maybe(docstring_description) + maybe(docstring_doctest) + finished
 
-# docstring_doctest = a_str('>>>') + (oneplus(some(is_valid_char)) >> to_str)
 
-# -----------------------------------------------------------------------------
-
-docstring_raw = '(dict of {SomeFakeClass, tuple of (bool, int, str, NoneType)}, int) -> list of list of SomeClass'
-output = list(generate_tokens(StringIO(docstring_raw).readline))
-out = docstring_parser.parse(output)
-print(out)
+def parse_csc108_docstring(docstring):
+    """Reads a docstring in the CSC108 format and extracts the argument types.
+    @param str docstring: The docstring to read.
+    @return: A parsed output of the docstring as a argument -> type list.
+    """
+    output = list(generate_tokens(StringIO(docstring.strip()).readline))
+    out = docstring_parser.parse(output)
+    out = [x for x in out if x is not None]
+    output_list = combine_elements_to_list(out[:-1])
+    output_list.append(out[-1])
+    return output_list
--- tests/test_csc108_docstring.py
+++ tests/test_csc108_docstring.py
@@ -2,85 +2,56 @@ from typing import *
 from docstring.csc108_docstring import *
 
 
-def test_missing_all():
-    parsed = ParsedDocstring108("""""")
-    assert not parsed.signature_args_list
-    assert not parsed.signature_return_type
-    assert not parsed.description
-    assert not parsed.doctests
-
-
-def test_missing_signature():
-    parsed = ParsedDocstring108("""Description\n>>> myFunc()\nNone""")
-    assert not parsed.signature_args_list
-    assert not parsed.signature_return_type
-    assert parsed.description == 'Description'
-    assert parsed.doctests == '>>> myFunc()\nNone'
-
-
-def test_missing_description():
-    parsed = ParsedDocstring108("""(int) -> bool\n>>> f(5)\n'Yes'""")
-    assert len(parsed.signature_args_list) == 1
-    assert parsed.signature_args_list[0] == int
-    assert parsed.signature_return_type == bool
-    assert not parsed.description
-    assert parsed.doctests == ">>> f(5)\n'Yes'"
-
-
-def test_missing_doctests():
-    parsed = ParsedDocstring108("""(int) -> bool\nDescription\nHere""")
-    assert len(parsed.signature_args_list) == 1
-    assert parsed.signature_args_list[0] == int
-    assert parsed.signature_return_type == bool
-    assert parsed.description == 'Description\nHere'
-    assert not parsed.doctests
-
-
 def test_simple_docstring():
-    parsed = ParsedDocstring108("""(int) -> bool
-    Description
-    >>> f(5)
-    'Yes'""")
-    assert len(parsed.signature_args_list) == 1
-    assert parsed.signature_args_list[0] == int
-    assert parsed.signature_return_type == bool
-    assert parsed.description == 'Description'
-    assert parsed.doctests == ">>> f(5)\n'Yes'"
+    parsed_docstring = parse_csc108_docstring('(int) -> bool')
+    assert len(parsed_docstring) == 2
+    assert parsed_docstring[0] == int
+    assert parsed_docstring[1] == bool
 
 
-def test_advanced_docstring():
-    parsed = ParsedDocstring108("""(list of int, set of set of bool, tuple of (str, int), dict of {obj, NoneType}) -> list of list of list of CustomClass
-    Description
-    >>> f(5)
-    'Yes'""")
-    assert len(parsed.signature_args_list) == 4
-    assert parsed.signature_args_list[0] == List[int]
-    assert parsed.signature_args_list[1] == Set[Set[bool]]
-    assert parsed.signature_args_list[2] == Tuple[str, int]
-    assert parsed.signature_args_list[3] == Dict[Any, type(None)]
-    assert parsed.signature_return_type == List[List[List['CustomClass']]]
-    assert parsed.description == 'Description'
-    assert parsed.doctests == ">>> f(5)\n'Yes'"
+def test_collection_simple_docstring():
+    parsed_docstring = parse_csc108_docstring('(list of list of int) -> dict of {int, bool}')
+    assert len(parsed_docstring) == 2
+    assert parsed_docstring[0] == List[List[int]]
+    assert parsed_docstring[1] == Dict[int, bool]
 
 
-def test_valid_empty_docstring():
-    parsed = ParsedDocstring108("""() -> bool""")
-    assert len(parsed.signature_args_list) == 0
-    assert parsed.signature_return_type == bool
+def test_simple_docstring_with_spaces():
+    parsed_docstring = parse_csc108_docstring('  (int)  ->    bool ')
+    assert len(parsed_docstring) == 2
+    assert parsed_docstring[0] == int
+    assert parsed_docstring[1] == bool
 
 
-def test_description_with_right_arrows():
-    parsed = ParsedDocstring108("""(int) -> bool\nTest > arrows >> Test""")
-    assert len(parsed.signature_args_list) == 1
-    assert parsed.signature_args_list[0] == int
-    assert parsed.signature_return_type == bool
-    assert parsed.description == 'Test > arrows >> Test'
-    assert not parsed.doctests
+def test_collection_all_docstring():
+    parsed_docstring = parse_csc108_docstring('(list of tuple of (int, bool), set of RandomClass) -> NoneType')
+    assert len(parsed_docstring) == 3
+    assert parsed_docstring[0] == List[Tuple[int, bool]]
+    # Comparing with forwardref's fails for some unknown reason (needs investigating).
+    desired_type = Set[typing._ForwardRef('RandomClass')]
+    assert str(desired_type) == str(parsed_docstring[1])
+    assert parsed_docstring[2] == None.__class__
+
+
+def test_tuple_single_element():
+    parsed_docstring = parse_csc108_docstring('(tuple of (int)) -> int')
+    print(parsed_docstring)
+    assert len(parsed_docstring) == 2
+    desired_type = Tuple[int]
+    assert str(desired_type) == str(parsed_docstring[0])
+    assert parsed_docstring[1] == int
+
+
+def test_empty_docstring():
+    parsed_docstring = parse_csc108_docstring('() -> int')
+    print(parsed_docstring)
+    assert len(parsed_docstring) == 1
+    assert parsed_docstring[0] == int
 
 
 def test_malformed_substring():
     try:
-        ParsedDocstring108("""(int) -> -> bool""")
+        parse_csc108_docstring("""(int) -> -> bool""")
         assert False
     except:
         pass
@@ -88,7 +59,7 @@ def test_malformed_substring():
 
 def test_malformed_docstring_list():
     try:
-        ParsedDocstring108("""(list of) -> -> bool""")
+        parse_csc108_docstring("""(list of) -> -> bool""")
         assert False
     except:
         pass
@@ -96,7 +67,7 @@ def test_malformed_docstring_list():
 
 def test_malformed_docstring_list_nested():
     try:
-        ParsedDocstring108("""(list of set int) -> -> bool""")
+        parse_csc108_docstring("""(list of set int) -> -> bool""")
         assert False
     except:
         pass
@@ -104,7 +75,83 @@ def test_malformed_docstring_list_nested():
 
 def test_malformed_docstring_tuple():
     try:
-        ParsedDocstring108("""(tuple of (int, )) -> -> bool""")
+        parse_csc108_docstring("""(tuple of (int, )) -> -> bool""")
         assert False
     except:
         pass
+
+
+# def test_missing_all():
+#     parsed = ParsedDocstring108("""""")
+#     assert not parsed.signature_args_list
+#     assert not parsed.signature_return_type
+#     assert not parsed.description
+#     assert not parsed.doctests
+#
+#
+# def test_missing_signature():
+#     parsed = ParsedDocstring108("""Description\n>>> myFunc()\nNone""")
+#     assert not parsed.signature_args_list
+#     assert not parsed.signature_return_type
+#     assert parsed.description == 'Description'
+#     assert parsed.doctests == '>>> myFunc()\nNone'
+#
+#
+# def test_missing_description():
+#     parsed = ParsedDocstring108("""(int) -> bool\n>>> f(5)\n'Yes'""")
+#     assert len(parsed.signature_args_list) == 1
+#     assert parsed.signature_args_list[0] == int
+#     assert parsed.signature_return_type == bool
+#     assert not parsed.description
+#     assert parsed.doctests == ">>> f(5)\n'Yes'"
+#
+#
+# def test_missing_doctests():
+#     parsed = ParsedDocstring108("""(int) -> bool\nDescription\nHere""")
+#     assert len(parsed.signature_args_list) == 1
+#     assert parsed.signature_args_list[0] == int
+#     assert parsed.signature_return_type == bool
+#     assert parsed.description == 'Description\nHere'
+#     assert not parsed.doctests
+#
+#
+# def test_simple_docstring():
+#     parsed = ParsedDocstring108("""(int) -> bool
+#     Description
+#     >>> f(5)
+#     'Yes'""")
+#     assert len(parsed.signature_args_list) == 1
+#     assert parsed.signature_args_list[0] == int
+#     assert parsed.signature_return_type == bool
+#     assert parsed.description == 'Description'
+#     assert parsed.doctests == ">>> f(5)\n'Yes'"
+#
+#
+# def test_advanced_docstring():
+#     parsed = ParsedDocstring108("""(list of int, set of set of bool, tuple of (str, int), dict of {obj, NoneType}) -> list of list of list of CustomClass
+#     Description
+#     >>> f(5)
+#     'Yes'""")
+#     assert len(parsed.signature_args_list) == 4
+#     assert parsed.signature_args_list[0] == List[int]
+#     assert parsed.signature_args_list[1] == Set[Set[bool]]
+#     assert parsed.signature_args_list[2] == Tuple[str, int]
+#     assert parsed.signature_args_list[3] == Dict[Any, type(None)]
+#     assert parsed.signature_return_type == List[List[List['CustomClass']]]
+#     assert parsed.description == 'Description'
+#     assert parsed.doctests == ">>> f(5)\n'Yes'"
+#
+#
+# def test_valid_empty_docstring():
+#     parsed = ParsedDocstring108("""() -> bool""")
+#     assert len(parsed.signature_args_list) == 0
+#     assert parsed.signature_return_type == bool
+#
+#
+# def test_description_with_right_arrows():
+#     parsed = ParsedDocstring108("""(int) -> bool\nTest > arrows >> Test""")
+#     assert len(parsed.signature_args_list) == 1
+#     assert parsed.signature_args_list[0] == int
+#     assert parsed.signature_return_type == bool
+#     assert parsed.description == 'Test > arrows >> Test'
+#     assert not parsed.doctests

bb8b1f031630a85e1d6c9c3d6e32cbe7e47962bc
Nigel Fong <nigel.fong@mail.utoronto.ca>
2016-09-10 17:53:12 -0400

    Stylistic adjustments in client. Output is correct for all nodes but Dict and Expr.

--- sample_usage/print_nodes.py
+++ sample_usage/print_nodes.py
@@ -6,7 +6,16 @@ from colorama import Fore, Back, Style
 import python_ta.transforms.setendings as setendings
 import os 
 
-colorama.init(strip=False)
+colorama.init(strip=False, autoreset=True)
+
+# Stying for each key part from, 
+# (col_offset, fromlineno) to (end_col_offset, end_lineno).
+MAIN_COL = Style.BRIGHT + Fore.WHITE + Back.BLACK
+
+
+def wrap_color(code_string):
+    """Wrap key parts in styling and resets."""
+    return MAIN_COL + code_string + Back.RESET
 
 
 def print_node(filename, node_class):
@@ -22,28 +31,50 @@ def print_node(filename, node_class):
 
     for node in module.nodes_of_class(node_class):
         if node.fromlineno == node.end_lineno:
-            line = source_lines[node.fromlineno - 1]
-            out = [Style.DIM +
-                   line[:node.col_offset] +
-                   Style.BRIGHT + Fore.WHITE + Back.BLACK +
-                   line[node.col_offset:node.end_col_offset] +
-                   Style.DIM +
-                   line[node.end_col_offset:] + Style.RESET_ALL]
-        else:
-            first_line = source_lines[node.fromlineno - 1]
-            last_line = source_lines[node.end_lineno - 1]
-            middle_lines = source_lines[node.fromlineno: node.end_lineno - 1]
+            line = source_lines[node.fromlineno - 1]  # string
+            out = [
+                Style.DIM +
+                line[:node.col_offset] +
 
-            out = (
-                [Style.DIM +
-                 first_line[:node.col_offset] +
-                 Style.BRIGHT + Fore.WHITE + Back.BLACK +
-                 first_line[node.col_offset:]] +
+                # The key part:
+                MAIN_COL +
+                line[node.col_offset : node.end_col_offset] +
+
+                Style.DIM + Fore.RESET + Back.RESET +
+                line[node.end_col_offset:]
+            ]
+        else:
+            first_line = source_lines[node.fromlineno - 1]  # string
+            middle_lines = source_lines[node.fromlineno: node.end_lineno - 1]  # list
+            last_line = source_lines[node.end_lineno - 1]  # string
+
+            if middle_lines:
+                # For each item in the list of lines of strings,
+                # add colorama style to middle like the first and last lines
+                middle_lines = '\n'.join(list(map(wrap_color, middle_lines))) + '\n'
+            else:
+                middle_lines = ''  # coerce list to string
+
+            if first_line:  # Add a spacing after first_line
+                middle_lines = '\n' + middle_lines
+
+            out = [
+                Style.DIM + 
+                first_line[:node.col_offset] +
+                
+                # The key part:
+                MAIN_COL + 
+                first_line[node.col_offset:] +
+
+                Back.RESET + 
                 middle_lines +
-                [last_line[:node.end_col_offset] +
-                 Style.DIM +
-                 last_line[node.end_col_offset:] +
-                 Style.RESET_ALL])
+
+                MAIN_COL + 
+                last_line[:node.end_col_offset] + 
+                
+                Style.DIM + Back.RESET +
+                last_line[node.end_col_offset:]
+            ]
         print('\n'.join(out))
 
 
@@ -55,7 +86,7 @@ if __name__ == '__main__':
         try:
             print_node(file_location, node_class)
         except FileNotFoundError:
-            print('ERROR: No file for class {}'.format(node_class))
+            print('WARNING: No file for class {}'.format(node_class))
         except AttributeError:
             print('ERROR: for class {}'.format(node_class))
 

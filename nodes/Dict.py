14cf15d21f9d8b439417b57d9aa94386730c0ede
Niayesh Ilkhani <n.ilkhani@mail.utoronto.ca>
2016-09-20 21:28:03 -0400

    rop299-node-docs: docs for List-YieldFrom

--- nodes/Dict.py
+++ nodes/Dict.py
@@ -1,14 +1,28 @@
 """
 Dict astroid node
 
-This node represents the Python dictionary objects.
+This is the node that represents the Python dictionary data type. It stores
+the keys and values of a dict as parallel lists of equal length.
 
 Attributes:
-    - items  (List[Tuple])
-        - Contains the tuple of key and value pair.
+    - keys    (List[Expr])
+        - The keys of this dict, which must be immutable/hashable type nodes
+          (such as numbers, strings, and tuples).
+    - values  (List[Expr])
+        - The values of this dict, in the same order as the keys. These can
+          be any expression node.
 
-Example:
-    - items  -> [('b', 1)]
+Example 1:
+    - keys    -> ['b']
+    - values  -> [1]
+
+Example 2:
+    - keys    -> [5, "hello"]
+    - values  -> [[1, "cat" (6, 7, 8)], "goodbye"]
 """
 
+# Example 1
 a = {'b': 1}
+
+# Example 2
+b = {5: [1, "cat", (6, 7, 8)], "hello": "goodbye"}
--- nodes/Tuple.py
+++ nodes/Tuple.py
@@ -1,1 +1,32 @@
+"""
+Tuple astroid node
+
+This node represents the Python tuple objects.
+
+Attributes:
+    - elts  (List[Expr])
+        - The elements in this tuple, which can be any expression.
+    - ctx   (class[expr_context])
+        - The context in which this list is to be used, either Load or Store.
+
+Example 1:
+    - elts  -> []
+    - ctx   -> Load
+
+Example 2:
+    - elts  -> [Num(1), Num(2)]
+    - ctx   -> Load
+
+Example 3:
+    - elts  -> [Name('x', Store()), Name('y', Store())]
+    - ctx   -> Store
+"""
+
+# Example 1:
+()
+
+# Example 2:
 (1, 2)
+
+# Example 3:
+(x, y) = [7, 8]

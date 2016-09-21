ff317cadfc542c1ef7aba7aaa84ae93debb081cf
Niayesh Ilkhani <n.ilkhani@mail.utoronto.ca>
2016-09-20 21:28:03 -0400

    Documented 4 more nodes: List, ListComp, Name, Nonlocal

--- nodes/List.py
+++ nodes/List.py
@@ -1,1 +1,14 @@
+"""
+List astroid node
+
+Attributes:
+    - elts  (list)   The list of nodes of the elements in this list.
+    - ctx   (class)  The context in which this lis is to be used,
+                     either Load or Store.
+
+Example:
+    - elts  -> []
+    - ctx   -> Load
+"""
+
 []

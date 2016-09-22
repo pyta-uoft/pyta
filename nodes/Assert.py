29239db5ee3e85305df9f3a637f4b2f38a6d71c0
Rebecca Kay <rebecca.kay@mail.utoronto.ca>
2016-09-22 01:30:30 -0400

    first 4

--- nodes/Assert.py
+++ nodes/Assert.py
@@ -1,1 +1,13 @@
+"""
+Assert astroid node
+
+Attributes:
+    - test  (compare)  the condition to be evaluated True or False
+    - msg   (str)      the message shown if condition is False
+
+Example:
+    - test  -> x
+    - msg   -> '' # AssertionError and this message if condition is False
+"""
+
 assert x
--- nodes/Assign.py
+++ nodes/Assign.py
@@ -1,1 +1,13 @@
+"""
+Assign astroid node
+
+Attributes:
+    - targets  (list)  a list of nodes
+    - value    (type)  a single node
+
+Example:
+    - targets  -> x
+    - value    -> 3
+"""
+
 x = 3

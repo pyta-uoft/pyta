01789cddb06146701e1ff0e7796daa2b29354a60
Rebecca Kay <rebecca.kay@mail.utoronto.ca>
2016-10-03 02:13:22 -0400

    last 2, one remainder!

--- nodes/Const.py
+++ nodes/Const.py
@@ -1,1 +1,14 @@
+"""
+Const astroid node
+
+Represents a constant node like num, str, bool, None, bytes.
+
+Attributes:
+    - value  (num|str|bool|None|bytes)
+        - A single node.
+
+Example:
+    - value  -> 1
+"""
+
 1

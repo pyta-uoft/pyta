9d4bb3aa10be00cb311d9e48219462c51097e97f
Wendy <yunyiwendy.liu@mail.utoronto.ca>
2016-10-04 13:00:01 -0400

    Added comments of how to design the functions

--- python_ta/reporters/stat_reporter.py
+++ python_ta/reporters/stat_reporter.py
@@ -1,5 +1,5 @@
 from .plain_reporter import PlainReporter
 
-
+# record, collect, and count each type of errors and report it.
 class StatReporter(PlainReporter):
     pass
--- stats/pyta_stats.py
+++ stats/pyta_stats.py
@@ -1,2 +1,7 @@
+# Use os.walk to walk through all the files in a directory from top to bottem.
+# Everytime when os.walk walks a file, check if the file is a python file, if
+# it is, then run a check_all on it.
+# os.walk doesn't return anything
+# return stat_reporter in the end
 def pyta_statistics():
     pass

60eab718bd21a25feefd93ef1c2c5a1cb30366c3
Rebecca Kay <rebecca.kay@mail.utoronto.ca>
2016-12-01 03:37:34 -0500

    classdef and todo

--- nodes/ClassDef.py
+++ nodes/ClassDef.py
@@ -4,6 +4,8 @@ ClassDef astroid node
 A class definition.
 
 Attributes:
+    - name        (str)
+        - A raw string for the class name.
     - decorators  (Decorators)
         - The decorator to be applied on this function.
     - bases       (List[Node])
@@ -12,11 +14,12 @@ Attributes:
         - List of nodes representing the code within the class definition.
 
 Example:
+    - name        -> 'Foo'
     - decorators  -> @wrapper
     - bases       -> [Name(id='base1', ctx=Load()),Name(id='base2', ctx=Load())]
     - body        -> [Pass()]
 """
 
 @wrapper
-class foo(base1, base2):
+class Foo(base1, base2):
     pass

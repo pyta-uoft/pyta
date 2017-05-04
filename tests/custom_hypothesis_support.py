2b23a82310d76f59287a17e4d7ab0179a4a68f05
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-05-04 17:53:36 -0400

    Use kw arguments for strategies.

--- tests/custom_hypothesis_support.py
+++ tests/custom_hypothesis_support.py
@@ -1,5 +1,6 @@
 import astroid
 import hypothesis.strategies as hs
+from typing import Any, Dict, List, Tuple
 
 # Custom strategies for hypothesis testing framework
 primitive_types = hs.sampled_from([
@@ -22,16 +23,19 @@ index_values = index_types.flatmap(lambda s: s())
 integer = hs.integers()
 
 # function that returns a strategy for generating strings from english alphabet with minimum length
-string = (lambda min, **kw: hs.text(alphabet="abcdefghijklmnopqrstuvwxyz", min_size=min, **kw))
+string = (lambda **kwargs: hs.text(alphabet="abcdefghijklmnopqrstuvwxyz", **kwargs))
 
 # function that returns a strategy for generating tuples from with minimum length
-tuple =  (hs.lists(primitive_values, min_size=2)).map(tuple)
+def tuple_strategy(**kwargs):
+    tuple_elements =  hs.lists(primitive_values, **kwargs)
+    return tuple(tuple_elements)
+
 
 # function that returns a strategy for generating lists of uniform type minimum length
-homogeneous_list = (lambda min, **kw: primitive_types.flatmap(lambda s: hs.lists(s(), min_size=min, **kw)))
+homogeneous_list = (lambda **kwargs: primitive_types.flatmap(lambda s: hs.lists(s(), **kwargs)))
 
 # function that returns a strategy for generating random lists with minimum length
-random_list = (lambda min, **kw: hs.lists(primitive_values, min_size=min, **kw))
+random_list = (lambda **kwargs: hs.lists(primitive_values, **kwargs))
 
 # strategy to generate dictionaries of uniform key:value types
 homogeneous_dictionary = primitive_types.flatmap(lambda s: hs.dictionaries(s(), s(),  min_size=1))

5c0bf62c66272b8b16c55575275d54c09c604906
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-05-04 13:57:02 -0400

    Add more strategies for generating integers, strings. Add helper for formatting inputs in index tests.

--- tests/custom_hypothesis_support.py
+++ tests/custom_hypothesis_support.py
@@ -15,9 +15,13 @@ PRIMITIVE_VALUES = PRIMITIVE_TYPES.flatmap(lambda s: s())
 
 INDEX_TYPES = hs.sampled_from([
     hs.integers,
-    lambda: hs.text(alphabet="abcdefghijklmnopqrstuvwxyz", min_size=1),
+    lambda: STRING,
 ])
 INDEX_VALUES = INDEX_TYPES.flatmap(lambda s: s())
+
+INTEGER = hs.integers()
+
+STRING = hs.text(alphabet="abcdefghijklmnopqrstuvwxyz", min_size=1)
 
 TUPLE =  (hs.lists(PRIMITIVE_VALUES, min_size=1)).map(tuple)
 
@@ -28,6 +32,8 @@ HETERO_LIST = hs.lists(PRIMITIVE_VALUES, min_size=2)
 HOMO_DICT = PRIMITIVE_TYPES.flatmap(lambda s: hs.dictionaries(s(), s(),  min_size=1))
 
 HETERO_DICT = hs.dictionaries(PRIMITIVE_VALUES, PRIMITIVE_VALUES, min_size=2)
+
+
 
 # Helper functions for testing
 def _verify_type_setting(module, ast_class, expected_type):
@@ -40,3 +46,8 @@ def _verify_type_inf_child(module):
     """Helper to verify that AST node has the same type as it's value/child's"""
     for n in module.nodes_of_class(astroid.Expr):
         assert n.value.type_constraints.type == n.type_constraints.type
+
+def _index_input_formatter(var_input, index):
+    """Helper to format input for testing index type inference visitor."""
+    input_index = repr(var_input) + "[" + repr(index) + "]"
+    return input_index

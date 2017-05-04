c1a56d7dd813d323b0140cbd1772313f08b237c1
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-05-04 12:32:30 -0400

    Import helper module and  refactor test cases using helpers.

--- tests/type_inference/test_literals.py
+++ tests/type_inference/test_literals.py
@@ -2,16 +2,10 @@ import astroid
 import nose
 from hypothesis import assume, given
 import hypothesis.strategies as hs
-import tests.custom_hypothesis_strategies as cs
+import tests.custom_hypothesis_support as cs
 from typing import Any, Dict, List, Tuple
 
 from python_ta.transforms.type_inference_visitor import register_type_constraints_setter, environment_transformer
-
-
-def _verify_type_setting(module, ast_class, expected_type):
-    """Helper to verify nodes visited by type inference visitor of astroid class has been properly transformed."""
-    result = [n.type_constraints.type for n in module.nodes_of_class(ast_class)]
-    assert [expected_type] == result
 
 
 @given(cs.PRIMITIVE_VALUES)
@@ -19,21 +13,21 @@ def test_simple_literal(const):
     """Test Const nodes representing int, bool, float, and None literal values."""
     assume(not isinstance(const, str))
     module = _parse_text(str(const))
-    _verify_type_setting(module, astroid.Const, type(const))
+    cs._verify_type_setting(module, astroid.Const, type(const))
 
 
 @given(cs.TUPLE)
 def test_tuple(t_tuple):
     """ Test Tuple nodes representing a tuple of various types."""
     module = _parse_text(str(t_tuple))
-    _verify_type_setting(module, astroid.Tuple, Tuple[tuple(type(x) for x in t_tuple)])
+    cs._verify_type_setting(module, astroid.Tuple, Tuple[tuple(type(x) for x in t_tuple)])
 
 
 @given(cs.HOMO_LIST)
 def test_homogeneous_lists(lst):
     """Test List nodes representing a list of values of the same primitive type."""
     module = _parse_text(str(lst))
-    _verify_type_setting(module, astroid.List, List[type(lst[0])])
+    cs._verify_type_setting(module, astroid.List, List[type(lst[0])])
 
 
 @given(cs.HETERO_LIST)
@@ -41,14 +35,14 @@ def test_heterogeneous_lists(lst):
     """Test List nodes representing a list of values of different primitive types."""
     assume(not isinstance(lst[0], type(lst[1])))
     module = _parse_text(str(lst))
-    _verify_type_setting(module, astroid.List, List[Any])
+    cs._verify_type_setting(module, astroid.List, List[Any])
 
 
 @given(cs.HOMO_DICT)
 def test_homogeneous_dict(dictionary):
     """Test Dictionary nodes representing a dictionary with all key:value pairs of same types."""
     module = _parse_text(str(dictionary))
-    _verify_type_setting(module, astroid.Dict, Dict[type(list(dictionary.keys())[0]), type(list(dictionary.values())[0])])
+    cs._verify_type_setting(module, astroid.Dict, Dict[type(list(dictionary.keys())[0]), type(list(dictionary.values())[0])])
 
 
 @given(cs.HETERO_DICT)
@@ -56,7 +50,7 @@ def test_heterogeneous_dict(dictionary):
     """Test Dictionary nodes representing a dictionary with some key:value pairs of different types."""
     assume(not isinstance(list(dictionary.keys())[0], type(list(dictionary.keys())[1])))
     module = _parse_text(str(dictionary))
-    _verify_type_setting(module, astroid.Dict, Dict[Any, Any])
+    cs._verify_type_setting(module, astroid.Dict, Dict[Any, Any])
 
 
 @hs.composite
@@ -115,32 +109,28 @@ def test_dict_index(index):
 def test_const_expr(expr):
     """Test visitor for expression node representing a constant"""
     module = _parse_text(repr(expr))
-    for n in module.nodes_of_class(astroid.Expr):
-        assert n.value.type_constraints.type == n.type_constraints.type
+    cs._verify_type_inf_child(module)
 
 
 @given(cs.TUPLE)
 def test_tuple_expr(expr):
     """Test visitor for expression node representing a tuple"""
     module = _parse_text(repr(expr))
-    for n in module.nodes_of_class(astroid.Expr):
-        assert n.value.type_constraints.type == n.type_constraints.type
+    cs._verify_type_inf_child(module)
 
 
 @given(cs.HETERO_LIST)
 def test_list_expr(expr):
     """Test visitor for expression node representing a list"""
     module = _parse_text(repr(expr))
-    for n in module.nodes_of_class(astroid.Expr):
-        assert n.value.type_constraints.type == n.type_constraints.type
+    cs._verify_type_inf_child(module)
 
 
 @given(cs.HETERO_DICT)
 def test_dict_expr(expr):
     """Test visitor for expression node representing a dictionary"""
     module = _parse_text(repr(expr))
-    for n in module.nodes_of_class(astroid.Expr):
-        assert n.value.type_constraints.type == n.type_constraints.type
+    cs._verify_type_inf_child(module)
 
 
 def _parse_text(source: str) -> astroid.Module:

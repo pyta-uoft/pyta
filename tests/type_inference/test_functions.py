71f793ddd36e968b3475ca588cf91309eb67ea41
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-05-17 10:34:22 -0400

    Implement test case for functionDef nodes representing function definition with no parameters and primitive return value.

--- tests/type_inference/test_functions.py
+++ tests/type_inference/test_functions.py
@@ -14,6 +14,16 @@ def _parse_to_function(function_name, args_list, return_value):
     return "def " + function_name + "(" + ", ".join(args_list) + "):\n\t" + "return " + repr(return_value)
 
 
+@given(hs.text(alphabet="abcdefghijklmnopqrstuvwxyz", min_size=1), cs.primitive_values)
+def test_function_def_no_args(function_name, return_value):
+    """Test FunctionDef node visitors representing function definitions with no parameters and primitive return val."""
+    assume(not iskeyword(function_name))
+    program = _parse_to_function(function_name, [], return_value)
+    module = _parse_text(program)
+    function_type_var = module.type_environment.lookup_in_env(function_name)
+    assert TYPE_CONSTRAINTS.lookup_concrete(function_type_var) == Callable[[], type(return_value)]
+
+
 def _parse_text(source: str) -> astroid.Module:
     """Parse source code text and output an AST with type inference performed."""
     module = astroid.parse(source)

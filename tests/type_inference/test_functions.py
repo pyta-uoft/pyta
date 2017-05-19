a7fc5434b235df89ba283585b620eb763e281995
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-05-19 15:26:50 -0400

    More variety for non-returning test case.

--- tests/type_inference/test_functions.py
+++ tests/type_inference/test_functions.py
@@ -44,15 +44,16 @@ def test_function_def_call_no_args(function_name, return_value):
 
 @given(hs.text(alphabet="abcdefghijklmnopqrstuvwxyz", min_size=1), hs.lists(cs.valid_identifier(), min_size=0))
 def test_function_def_no_return(function_name, arguments):
-    """Test FunctionDef node visitors representing function definitions with no parameters and primitive return val."""
+    """Test FunctionDef node visitors representing non-returning function definitions with parameter(s)."""
     assume(not iskeyword(function_name) and function_name not in arguments)
-    program = _parse_to_function_no_return(function_name, arguments, 'None')
-    module = cs._parse_text(program)
-    function_def_node = next(module.nodes_of_class(astroid.FunctionDef))
-    expected_arg_type_vars = [function_def_node.type_environment.lookup_in_env(argument) for argument in arguments]
-    expected_arg_types = [TYPE_CONSTRAINTS.lookup_concrete(type_var) for type_var in expected_arg_type_vars]
-    function_type_var = module.type_environment.lookup_in_env(function_name)
-    assert TYPE_CONSTRAINTS.lookup_concrete(function_type_var) == Callable[expected_arg_types, type(None)]
+    for return_value in ['return None', 'None', 'pass']:
+        program = _parse_to_function_no_return(function_name, arguments, return_value)
+        module = cs._parse_text(program)
+        function_def_node = next(module.nodes_of_class(astroid.FunctionDef))
+        expected_arg_type_vars = [function_def_node.type_environment.lookup_in_env(argument) for argument in arguments]
+        expected_arg_types = [TYPE_CONSTRAINTS.lookup_concrete(type_var) for type_var in expected_arg_type_vars]
+        function_type_var = module.type_environment.lookup_in_env(function_name)
+        assert TYPE_CONSTRAINTS.lookup_concrete(function_type_var) == Callable[expected_arg_types, type(None)]
 
 
 if __name__ == '__main__':

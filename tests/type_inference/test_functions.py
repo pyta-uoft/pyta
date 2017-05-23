294b8578c93e9bddc0ef077501770be36f4d579c
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-05-23 09:53:39 -0400

    Address changes in PR.

--- tests/type_inference/test_functions.py
+++ tests/type_inference/test_functions.py
@@ -8,33 +8,32 @@ from python_ta.transforms.type_inference_visitor import TYPE_CONSTRAINTS
 from keyword import iskeyword
 
 
-def _parse_to_function(function_name, args_list, return_value):
+def _parse_to_function(function_name, args_list, return_statement):
     """Helper to parse given data into function definition."""
     return f'def {function_name}({", ".join(args_list)}):' \
-           f'   return {repr(return_value)}'
+           f'   return {repr(return_statement)}'
 
 
-def _parse_to_function_no_return(function_name, args_list, return_value):
+def _parse_to_function_no_return(function_name, args_list, return_statement):
     """Helper to parse given data into function definition."""
     return f'def {function_name}({", ".join(args_list)}):\n' \
-           f'     {return_value}'
+           f'     {return_statement}'
 
 
 @given(cs.valid_identifier(), cs.primitive_values)
-def test_function_def_no_args(function_name, return_value):
-    """Test FunctionDef node visitors representing function definitions with no parameters and primitive return val."""
+def test_function_def_no_args(function_name, return_statement):
+    """Test FunctionDef node visitors representing function definitions with no parameters and primitive return type."""
     assume(not iskeyword(function_name))
-    program = _parse_to_function(function_name, [], return_value)
+    program = _parse_to_function(function_name, [], return_statement)
     module = cs._parse_text(program)
     function_type_var = module.type_environment.lookup_in_env(function_name)
-    assert TYPE_CONSTRAINTS.lookup_concrete(function_type_var) == Callable[[], type(return_value)]
+    assert TYPE_CONSTRAINTS.lookup_concrete(function_type_var) == Callable[[], type(return_statement)]
 
 
 @given(cs.valid_identifier(), cs.primitive_values)
 def test_function_def_call_no_args(function_name, return_value):
     """Test type setting in environment of a function call for a function with no parameters."""
     TYPE_CONSTRAINTS.clear_tvars()
-    assume(not iskeyword(function_name))
     program = _parse_to_function(function_name, [], return_value) + "\n" + function_name + "()\n"
     module = cs._parse_text(program)
     # there should be a single Expr node in this program
@@ -45,7 +44,6 @@ def test_function_def_call_no_args(function_name, return_value):
 @given(cs.valid_identifier(), hs.lists(cs.valid_identifier(), min_size=0), cs.primitive_values)
 def test_function_def_no_return(function_name, arguments, body):
     """Test FunctionDef node visitors representing non-returning function definitions with parameter(s)."""
-    assume(not iskeyword(function_name) and function_name not in arguments)
     for return_value in ['return None', repr(body), 'pass']:
         program = _parse_to_function_no_return(function_name, arguments, return_value)
         module = cs._parse_text(program)
@@ -53,7 +51,7 @@ def test_function_def_no_return(function_name, arguments, body):
         expected_arg_type_vars = [function_def_node.type_environment.lookup_in_env(argument) for argument in arguments]
         expected_arg_types = [TYPE_CONSTRAINTS.lookup_concrete(type_var) for type_var in expected_arg_type_vars]
         function_type_var = module.type_environment.lookup_in_env(function_name)
-        assert TYPE_CONSTRAINTS.lookup_concrete(function_type_var) == Callable[expected_arg_types, type(None)]
+        assert TYPE_CONSTRAINTS.lookup_concrete(function_type_var) == Callable[expected_arg_types, None]
 
 
 if __name__ == '__main__':

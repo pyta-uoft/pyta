4b4b6fde5c6afc1cdb506db456ba881e83388ba1
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-06-05 10:58:59 -0400

    Undo accidental delete.

--- tests/type_inference/test_subscript.py
+++ tests/type_inference/test_subscript.py
@@ -15,5 +15,14 @@ def test_inference_list_subscript(input_list, index):
     assert subscript_node.type_constraints.type == type(input_list[0])
 
 
+@given(cs.random_dict_variable_value(min_size=1))
+def test_inference_dict_subscript(input_dict):
+    for index in input_dict:
+        program = f'{input_dict}[{index}]'
+        module = cs._parse_text(program)
+        subscript_node = list(module.nodes_of_class(astroid.Subscript))[0]
+        assert subscript_node.type_constraints.type == type(input_dict[index])
+
+
 if __name__ == '__main__':
     nose.main()

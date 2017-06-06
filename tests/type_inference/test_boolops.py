c613d01adca81e9c933499d652ca9351722dd3a8
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-06-06 17:05:30 -0400

    Reimplement test case for BoolOp node representing multi-binary-boolean operation.

--- tests/type_inference/test_boolops.py
+++ tests/type_inference/test_boolops.py
@@ -1,18 +1,32 @@
 import astroid
 import nose
 from hypothesis import given, settings, assume
+import hypothesis.strategies as hs
 import tests.custom_hypothesis_support as cs
-from typing import Union
+from typing import Any
 settings.load_profile("pyta")
 
 
-@given(cs.primitive_values, cs.binary_bool_operator, cs.primitive_values)
-def test_binary_boolop(left_operand, operator, right_operand):
+@given(hs.lists(cs.binary_bool_operator), hs.lists(cs.primitive_values))
+def test_binary_boolop(op_list, operand_list):
     """Test type setting of binary BoolOp node(s)."""
-    program = f'{repr(left_operand)} {operator} {repr(right_operand)}\n'
+    # get every permutation?
+    assume(len(op_list) > 0 and len(operand_list) == len(op_list) + 1)
+    pre_format_program = [str(pair) for operand_op_pair in zip(operand_list[:-1], op_list) for pair in operand_op_pair]
+    pre_format_program.append(repr(operand_list[-1]))
+    program = ' '.join(pre_format_program)
     module = cs._parse_text(program)
     boolop_node = list(module.nodes_of_class(astroid.BoolOp))[0]
-    expected_type = Union[type(left_operand), type(right_operand)]
+    operand_type = type(operand_list[0])
+    homogeneous = True
+    for operand in operand_list:
+        if type(operand) != operand_type:
+            homogeneous = False
+            break
+    if not homogeneous:
+        expected_type = Any
+    else:
+        expected_type = operand_type
     assert boolop_node.type_constraints.type == expected_type
 
 

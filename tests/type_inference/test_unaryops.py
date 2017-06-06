38228124494c812d67429ae0b925ee5efe6e3e67
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-06-06 14:31:13 -0400

    Add test cases for UnaryOp nodes representing boolean and non boolean unary operations.

---
+++ tests/type_inference/test_unaryops.py
@@ -0,0 +1,27 @@
+import astroid
+import nose
+from hypothesis import given, settings
+import tests.custom_hypothesis_support as cs
+settings.load_profile("pyta")
+
+
+@given(cs.non_bool_unary_op, cs.primitive_values)
+def test_unarynop_non_bool_concrete(operator, operand):
+    """Test type setting of UnaryOp node(s) with non-boolean operand."""
+    program = f'{operator} {repr(operand)}\n'
+    module = cs._parse_text(program)
+    unaryop_node = list(module.nodes_of_class(astroid.UnaryOp))[0]
+    assert isinstance(unaryop_node.type_constraints.type, type(bool))
+
+
+@given(cs.unary_bool_operator, cs.primitive_values)
+def test_not_bool(operator, operand):
+    """Test type setting of UnaryOp node representing boolean operation not _."""
+    program = f'{operator} {repr(operand)}\n'
+    module = cs._parse_text(program)
+    unaryop_node = list(module.nodes_of_class(astroid.UnaryOp))[0]
+    assert isinstance(unaryop_node.type_constraints.type, type(bool))
+
+
+if __name__ == '__main__':
+    nose.main()

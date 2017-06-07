09d7819de8639ae1a5363fcbee5ef71be770f925
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-06-07 11:56:24 -0400

    Add test case for homogeneous boolean operations.

--- tests/type_inference/test_boolops.py
+++ tests/type_inference/test_boolops.py
@@ -29,27 +29,27 @@ def test_homogeneous_binary_boolop(op, operand_list):
     assert boolop_node.type_constraints.type == expected_type
 
 
-# @given(hs.lists(cs.binary_bool_operator), hs.lists(cs.primitive_values))
-# def test_heterogeneous_binary_boolop(op_list, operand_list):
-#     """Test type setting of binary BoolOp node(s) representing expression with different binary boolean operations."""
-#     assume(len(op_list) > 0 and len(operand_list) == len(op_list) + 1)
-#     pre_format_program = [str(pair) for operand_op_pair in zip(operand_list[:-1], op_list) for pair in operand_op_pair]
-#     pre_format_program.append(repr(operand_list[-1]))
-#     program = ' '.join(pre_format_program)
-#     module = cs._parse_text(program)
-#     boolop_node = list(module.nodes_of_class(astroid.BoolOp))[0]
-#     operand_type = type(operand_list[0])
-#     homogeneous = True
-#     for operand in operand_list:
-#         if type(operand) != operand_type:
-#             homogeneous = False
-#             break
-#     if not homogeneous:
-#         expected_type = Any
-#     else:
-#         expected_type = operand_type
-#     a = boolop_node.type_constraints.type
-#     assert boolop_node.type_constraints.type == expected_type
+@given(hs.lists(cs.binary_bool_operator), hs.lists(cs.primitive_values))
+def test_heterogeneous_binary_boolop(op_list, operand_list):
+    """Test type setting of binary BoolOp node(s) representing expression with different binary boolean operations."""
+    assume(len(op_list) > 0 and len(operand_list) == len(op_list) + 1)
+    pre_format_program = [str(pair) if pair != '' else repr(pair) for operand_op_pair in zip(operand_list[:-1], op_list)
+                          for pair in operand_op_pair]
+    pre_format_program.append(repr(operand_list[-1]))
+    program = ' '.join(pre_format_program)
+    module = cs._parse_text(program)
+    boolop_node = list(module.nodes_of_class(astroid.BoolOp))[0]
+    operand_type = type(operand_list[0])
+    homogeneous = True
+    for operand in operand_list:
+        if type(operand) != operand_type:
+            homogeneous = False
+            break
+    if not homogeneous:
+        expected_type = Any
+    else:
+        expected_type = operand_type
+    assert boolop_node.type_constraints.type == expected_type
 
 
 if __name__ == '__main__':

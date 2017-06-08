b38853cc4267c8badf042f579617ef2d809eabb1
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-06-08 19:09:52 -0400

    Resolve conflict.

--- tests/custom_hypothesis_support.py
+++ tests/custom_hypothesis_support.py
@@ -29,8 +29,9 @@ index_values = index_types.flatmap(lambda s: s())
 
 
 # Strategies for generating Binary Operators
-non_bool_bin_symbols = ['+', '-', '*', '//', '%', '/', '**', '<<', '>>', '&', '^', '|']
-non_bool_bin_operator = hs.sampled_from(non_bool_bin_symbols)
+non_bool_symbols = ['+', '-', '*', '//', '%', '/', '**', '&', '^', '~', '|', '<<', '>>']
+non_boolean_operator = hs.sampled_from(non_bool_symbols)
+non_bool_unary_op = hs.sampled_from(['-', '+', '~'])
 
 
 # Strategy for generating Boolean Operators

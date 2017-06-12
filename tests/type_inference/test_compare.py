0ae39bc0488f6091ba8bec5c9840acb3c641adc3
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-06-12 16:08:37 -0400

    Reimplement test case. Too many assumes; create strategy?

--- tests/type_inference/test_compare.py
+++ tests/type_inference/test_compare.py
@@ -9,7 +9,22 @@ settings.load_profile("pyta")
 
 @given(cs.primitive_values, hs.lists(hs.tuples(cs.comparator_operator, cs.primitive_values), min_size=1))
 def test_compare_builtins(left_value, operator_value_tuples):
-    """Test type settting of Compare node representing comparison between primitive values."""
+    """Test type setting of Compare node representing comparison between primitive values."""
+    assume(left_value is not None)
+    for operator, value in operator_value_tuples:
+        assume(value is not None)
+    if isinstance(left_value, int):
+        for operator, value in operator_value_tuples:
+            assume(not isinstance(value, float) and not isinstance(value, str) and not isinstance(value, bytes))
+    if isinstance(left_value, float):
+        for operator, value in operator_value_tuples:
+            assume(not isinstance(value, int) and not isinstance(value, str) and not isinstance(value, bytes))
+    if isinstance(left_value, str):
+        for operator, value in operator_value_tuples:
+            assume(not isinstance(value, int) and not isinstance(value, float) and not isinstance(value, bytes))
+    if isinstance(left_value, bytes):
+        for operator, value in operator_value_tuples:
+            assume(not isinstance(value, str) and not isinstance(value, bool) and not isinstance(value, float) and not isinstance(value, int))
     for operator, value in operator_value_tuples:
         if operator == 'in' or operator == 'is':
             assume(isinstance(value, GenericMeta))

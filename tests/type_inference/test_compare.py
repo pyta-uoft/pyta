9e3ed0554f601ab35dfab62245521d3373f4a83b
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-06-12 12:20:58 -0400

    Update test case.

--- tests/type_inference/test_compare.py
+++ tests/type_inference/test_compare.py
@@ -1,17 +1,22 @@
 import astroid
 import nose
 from hypothesis import given, settings, assume
+from typing import GenericMeta
 import tests.custom_hypothesis_support as cs
 import hypothesis.strategies as hs
 settings.load_profile("pyta")
 
 
-@given(cs.comparator_operator, hs.integers(), hs.integers())
-def test_inference_list_subscript(operator, left_value, right_value):
-    """Test type settting of  Compare node representing comparison between homogeneous-typed values."""
-    #TODO: __eq__ has type Callable[[int, object], bool] and int does not unify with object.
-    assume(operator != '==' and operator != '>=' and operator != '<=' and operator != '!=')
-    program = f'{repr(left_value)} {operator} {repr(right_value)}'
+@given(hs.integers(), hs.lists(hs.tuples(cs.comparator_operator, hs.integers()), min_size=1))
+def test_compare_builtins(left_value, operator_value_tuples):
+    """Test type settting of Compare node representing comparison between primitive values."""
+    for operator, value in operator_value_tuples:
+        assume(operator != '==' and operator != '>=' and operator != '<=' and operator != '!=')
+        if operator == 'in' or operator == 'is':
+            assume(isinstance(value, GenericMeta))
+    program = f'{repr(left_value)} '
+    for operator, value in operator_value_tuples:
+        program += ' '.join([operator, repr(value)])
     module, _ = cs._parse_text(program)
     compare_node = list(module.nodes_of_class(astroid.Compare))[0]
     assert compare_node.type_constraints.type == bool

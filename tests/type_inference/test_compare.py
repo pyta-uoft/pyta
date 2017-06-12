c724ddb93e642db836cafcd3703d326c9f3511bd
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-06-12 17:58:15 -0400

    Refactor test cases to handle equality-related comparisons and non-equality-related comparisons.

--- tests/type_inference/test_compare.py
+++ tests/type_inference/test_compare.py
@@ -7,30 +7,35 @@ import hypothesis.strategies as hs
 settings.load_profile("pyta")
 
 
-@given(cs.primitive_values, hs.lists(hs.tuples(cs.comparator_operator, cs.primitive_values), min_size=1))
-def test_compare_builtins(left_value, operator_value_tuples):
-    """Test type setting of Compare node representing comparison between primitive values."""
-    assume(left_value is not None)
-    for operator, value in operator_value_tuples:
-        assume(value is not None)
-    if isinstance(left_value, int):
-        for operator, value in operator_value_tuples:
-            assume(not isinstance(value, float) and not isinstance(value, str) and not isinstance(value, bytes))
-    if isinstance(left_value, float):
-        for operator, value in operator_value_tuples:
-            assume(not isinstance(value, int) and not isinstance(value, str) and not isinstance(value, bytes))
-    if isinstance(left_value, str):
-        for operator, value in operator_value_tuples:
-            assume(not isinstance(value, int) and not isinstance(value, float) and not isinstance(value, bytes))
-    if isinstance(left_value, bytes):
-        for operator, value in operator_value_tuples:
-            assume(not isinstance(value, str) and not isinstance(value, bool) and not isinstance(value, float) and not isinstance(value, int))
-    for operator, value in operator_value_tuples:
-        if operator == 'in' or operator == 'is':
-            assume(isinstance(value, GenericMeta))
+@given(cs.primitive_values, hs.lists(hs.tuples(cs.comparator_operator_equality, cs.primitive_values), min_size=1))
+def test_compare_equality(left_value, operator_value_tuples):
+    """Test type setting of Compare node representing comparators: ''==', '!=', '>=', '<=', 'is'. """
+    # TODO:'is'
+    for operator, _ in operator_value_tuples:
+        assume(operator != 'is')
     program = f'{repr(left_value)} '
     for operator, value in operator_value_tuples:
         program += ' '.join([operator, repr(value)])
+    module, _ = cs._parse_text(program)
+    compare_node = list(module.nodes_of_class(astroid.Compare))[0]
+    assert compare_node.type_constraints.type == bool
+
+
+@given(hs.lists(cs.comparator_operator, min_size=3), cs.homogeneous_list(min_size=4))
+def test_compare_equality(operators, values):
+    """Test type setting of Compare node representing comparators: '<', '>', 'in'. """
+    # TODO: 'in'
+    for value in values:
+        assume(value)
+    for operator in operators:
+        assume(operator != 'in')
+    a = list(zip(operators, values))
+    pre = []
+    for operator, value in a:
+        pre.append(str(operator))
+        pre.append(str(value))
+    # pre_input_program = [str(elt) for tuple in zip(operator, values) for elt in tuple]
+    program = f'{str(values[0])} ' + ' '.join(pre)
     module, _ = cs._parse_text(program)
     compare_node = list(module.nodes_of_class(astroid.Compare))[0]
     assert compare_node.type_constraints.type == bool

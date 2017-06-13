20d7f9031308ac0ee0c5c1d9fc6401af979c2233
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-06-13 11:41:13 -0400

    Update documentation for For Node.

--- nodes/For.py
+++ nodes/For.py
@@ -7,9 +7,8 @@ Attributes:
     - target  (Node)
         - Holds the variable(s) the loop assigns to as a single node. The type
           of the node can be Name, List, Tuple, etc.
-    - iter    (Call)
-        - A function call node which represents the part that iterates over
-          the loop.
+    - iter    (List Node)
+        - A List node representing the iterable.
     - body    (List[Statement])
         - The node to be executed.
     - orelse  (List[Statement])
@@ -18,12 +17,13 @@ Attributes:
 
 Example:
     - target  -> AssignName(i)
-    - iter    -> Call(func = Name(range), args = [3])
+    - iter    -> List(Const.int(Value=1), Const.int(Value=2),
+    Const.int(Value=3)))
     - body    -> [Break()]
     - orelse  -> [Pass()]
 """
 
-for i in range(3):
+for i in [1, 2, 3]:
     break
 else:
     pass

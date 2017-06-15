2aaebdd9b3170c41b45f3fe2fc5fc162898c409f
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-06-15 10:09:26 -0400

    Todo.

--- tests/type_inference/test_for.py
+++ tests/type_inference/test_for.py
@@ -16,7 +16,9 @@ def test_for_list():
               f'    pass\n'
     module, TypeInferrer = cs._parse_text(program)
     for_node = list(module.nodes_of_class(astroid.For))[0]
-
+    # TODO: x should have inferred type of int
+    # x in module's environment is correctly unified, however in For's environment it is not..
+    # is this okay or not what we want?
 
 @given(cs.homogeneous_list(min_size=1), hs.integers())
 def test_inference_list_subscript(input_list, index):

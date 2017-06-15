8e6c5eeea74b44d29598e9471803b9d0cb5ae1d0
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-06-15 13:32:54 -0400

    Reimplement concrete test case for For node, representing type inferrence on local variable of For/else statement.

--- tests/type_inference/test_for.py
+++ tests/type_inference/test_for.py
@@ -16,23 +16,23 @@ def test_for_list():
               f'    pass\n'
     module, TypeInferrer = cs._parse_text(program)
     for_node = list(module.nodes_of_class(astroid.For))[0]
-    # TODO: x should have inferred type of int
-    # x in module's environment is correctly unified, however in For's environment it is not..
-    # is this okay or not what we want?
+    local_type_var = module.type_environment.lookup_in_env('x')
+    inferred_type = TypeInferrer.type_constraints.lookup_concrete(local_type_var)
+    assert inferred_type == int
 
-@given(cs.homogeneous_list(min_size=1), hs.integers())
-def test_inference_list_subscript(input_list, index):
-    """Test whether visitors properly set the type constraint of the a For node representing for/else statement
-     iterating over a more complex iterable (ie, tuples, dicts, nested iterables).
-    """
-    program = f'num1 = \'bob\'\n' \
-              f'for num1, num2 in [(1, 2), (3, 3)]:\n' \
-              f'    x = 3 + num2\n' \
-              f'else:\n' \
-              f'    pass\n' \
-              f'x'
-    module, TypeInferrer = cs._parse_text(program)
-    for_node = list(module.nodes_of_class(astroid.For))[0]
+
+# def test_inference_list_subscript(input_list, index):
+#     """Test whether visitors properly set the type constraint of the a For node representing for/else statement
+#      iterating over a more complex iterable (ie, tuples, dicts, nested iterables).
+#     """
+#     program = f'num1 = \'bob\'\n' \
+#               f'for num1, num2 in [(1, 2), (3, 3)]:\n' \
+#               f'    x = 3 + num2\n' \
+#               f'else:\n' \
+#               f'    pass\n' \
+#               f'x'
+#     module, TypeInferrer = cs._parse_text(program)
+#     for_node = list(module.nodes_of_class(astroid.For))[0]
 
 
 if __name__ == '__main__':

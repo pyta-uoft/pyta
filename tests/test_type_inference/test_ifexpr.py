4988e86c03b3c10722058601e8494c4eaa27e8b2
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-06-19 16:59:39 -0400

    Fix imports. Reimplement test cases using hypothesis.

--- tests/test_type_inference/test_ifexpr.py
+++ tests/test_type_inference/test_ifexpr.py
@@ -1,22 +1,26 @@
 import astroid
 import nose
 from hypothesis import given, settings, assume
-from typing import Callable, Any
+from typing import Any
 import tests.custom_hypothesis_support as cs
+import hypothesis.strategies as hs
 settings.load_profile("pyta")
 
 
-def test_ifexp_homogeneous():
+@given(cs.eval_types.flatmap(lambda s: hs.lists(s(), min_size=2, max_size=2)))
+def test_ifexp_homogeneous(suites):
     """Test the type setting of an IfExp node representing an if statement."""
-    program = f'a = 2 + 1 if True else 0\n'
+    program = f'a = {repr(suites[0])}  if True else {repr(suites[1])}\n'
     module, TypeInferrer = cs._parse_text(program)
     ifexp_node = list(module.nodes_of_class(astroid.IfExp))[0]
     assert ifexp_node.type_constraints.type == ifexp_node.body.type_constraints.type
 
 
-def test_ifexp_heterogeneous():
+@given(hs.lists(cs.eval_values, min_size=2, max_size=2))
+def test_ifexp_heterogeneous(suites):
     """Test the type setting of an IfExp node representing an if statement."""
-    program = f'a = \"bob\" if True else 0\n'
+    assume(type(suites[0]) != type(suites[1]))
+    program = f'a = {repr(suites[0])}  if True else {repr(suites[1])}\n'
     module, TypeInferrer = cs._parse_text(program)
     ifexp_node = list(module.nodes_of_class(astroid.IfExp))[0]
     assert ifexp_node.type_constraints.type == Any

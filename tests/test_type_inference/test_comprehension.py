a8ecdf5cdbd785795379ec3f262141d64cfbf3c4
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-06-25 18:39:16 -0400

    Add TODO for ListComp node test.

--- tests/test_type_inference/test_comprehension.py
+++ tests/test_type_inference/test_comprehension.py
@@ -17,11 +17,14 @@ def test_list_comprehension1():
     target_tvar = listcomp_node.type_environment.lookup_in_env(list(listcomp_node.locals.keys())[0])
     iter_type = listcomp_node.generators[0].iter.type_constraints.type
     assigned_tvar = module.type_environment.lookup_in_env('a')
+    # TODO: is this the correct expected type? num should be unified with the num in ListComp's local scope..?
+    # TODO: since this is a ListComp expr, num should not exist in module's env? so module's should not be unified with
+    # TODO: ListComp's?
     expected_type = TypeInferrer.type_constraints.lookup_concrete(listcomp_node.elt.type_constraints.type)
     assert (TypeInferrer.type_constraints.lookup_concrete(target_tvar) == iter_type.__args__[0]) and \
            (TypeInferrer.type_constraints.lookup_concrete(assigned_tvar) == List[expected_type])
 
-# def test_list_comprehension1():
+# def test_list_comprehension2():
 #     """Test Comprehension node visitor representing a comprehension expression."""
 #     program = f'a = [num1+ num2 for num1, num2 in [[1,2], [3,4]]]'
 #     module, TypeInferrer = cs._parse_text(program)
@@ -29,7 +32,7 @@ def test_list_comprehension1():
 #     listcomp_node = list(module.nodes_of_class(astroid.ListComp))[0]
 #     assert True
 
-# def test_list_comprehension2():
+# def test_list_comprehension3():
 #     """Test Comprehension node visitor representing a comprehension expression."""
 #     program = f'a = [num for sublist in [[1, 2], [3,4]] for num in sublist]'
 #     module, TypeInferrer = cs._parse_text(program)

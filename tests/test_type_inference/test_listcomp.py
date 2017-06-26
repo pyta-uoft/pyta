0f2335f92478e9ba4eed3300e76cf9ecae9b0033
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-06-26 16:37:43 -0400

    Implement test cases using hypothesis for homogeneous and non-homogeneous lists; nested and unnested as well.

--- tests/test_type_inference/test_listcomp.py
+++ tests/test_type_inference/test_listcomp.py
@@ -1,15 +1,32 @@
 import astroid
 import nose
-from hypothesis import settings
+from hypothesis import settings, given
 from typing import List
 import tests.custom_hypothesis_support as cs
+import hypothesis.strategies as hs
 settings.load_profile("pyta")
 
 
-def test_list_comprehension_single_target_name():
+@given(cs.homogeneous_list(min_size=1))
+def test_list_comprehension_single_target_name_homogeneous_iterable(iterable):
     """Test Comprehension node visitor representing a comprehension expression with a single target and a
-    name expression."""
-    program = f'a = [num for num in [1,2,3]]'
+    name expression over a homogeneous list."""
+    program = f'a = [num for num in {iterable}]'
+    module, TypeInferrer = cs._parse_text(program)
+    listcomp_node = list(module.nodes_of_class(astroid.ListComp))[0]
+    target_tvar = listcomp_node.type_environment.lookup_in_env(list(listcomp_node.locals.keys())[0])
+    iter_type = listcomp_node.generators[0].iter.type_constraints.type
+    assigned_tvar = module.type_environment.lookup_in_env('a')
+    expected_type = TypeInferrer.type_constraints.lookup_concrete(listcomp_node.elt.type_constraints.type)
+    assert (TypeInferrer.type_constraints.lookup_concrete(target_tvar) == iter_type.__args__[0]) and \
+           (TypeInferrer.type_constraints.lookup_concrete(assigned_tvar) == List[expected_type])
+
+
+@given(cs.random_list(min_size=1))
+def test_list_comprehension_single_target_name_heterogeneous_iterable(iterable):
+    """Test Comprehension node visitor representing a comprehension expression with a single target and a
+    name expression over a heterogeneous list."""
+    program = f'a = [num for num in {iterable}]'
     module, TypeInferrer = cs._parse_text(program)
     listcomp_node = list(module.nodes_of_class(astroid.ListComp))[0]
     # test whether the target variables have been unified correctly
@@ -22,10 +39,11 @@ def test_list_comprehension_single_target_name():
            (TypeInferrer.type_constraints.lookup_concrete(assigned_tvar) == List[expected_type])
 
 
-def test_list_comprehension_single_target_name_nested():
+@given(hs.lists(cs.homogeneous_list(min_size=1), min_size=1))
+def test_list_comprehension_single_target_name_nested_homogeneous(iterable):
     """Test Comprehension node visitor representing a nested comprehension expression with a name expression and
-    a single target."""
-    program = f'a = [num for sublist in [[1, 2], [3,4]] for num in sublist]'
+    a single target over a homogeneous list."""
+    program = f'a = [num for sublist in {iterable} for num in sublist]'
     module, TypeInferrer = cs._parse_text(program)
     listcomp_node = list(module.nodes_of_class(astroid.ListComp))[0]
     target_tvar = listcomp_node.type_environment.lookup_in_env(list(listcomp_node.locals.keys())[0])
@@ -36,10 +54,41 @@ def test_list_comprehension_single_target_name_nested():
            (TypeInferrer.type_constraints.lookup_concrete(assigned_tvar) == List[expected_type])
 
 
-def test_list_comprehension_multi_target_binop_nested():
+@given(hs.lists(cs.random_list(min_size=1), min_size=1))
+def test_list_comprehension_single_target_name_nested_heterogeneous(iterable):
+    """Test Comprehension node visitor representing a nested comprehension expression with a name expression and
+    a single target over a heterogeneous list."""
+    program = f'a = [num for sublist in {iterable} for num in sublist]'
+    module, TypeInferrer = cs._parse_text(program)
+    listcomp_node = list(module.nodes_of_class(astroid.ListComp))[0]
+    target_tvar = listcomp_node.type_environment.lookup_in_env(list(listcomp_node.locals.keys())[0])
+    iter_type = listcomp_node.generators[0].iter.type_constraints.type
+    assigned_tvar = module.type_environment.lookup_in_env('a')
+    expected_type = TypeInferrer.type_constraints.lookup_concrete(listcomp_node.elt.type_constraints.type)
+    assert (TypeInferrer.type_constraints.lookup_concrete(target_tvar) == iter_type.__args__[0]) and \
+           (TypeInferrer.type_constraints.lookup_concrete(assigned_tvar) == List[expected_type])
+
+
+@given(hs.lists(cs.homogeneous_list(min_size=1), min_size=1))
+def test_list_comprehension_multi_target_binop_nested_homogeneous(iterable):
     """Test Comprehension node visitor representing a nested comprehension expression with multiple targets and binary
-    operations expressions performed on the targets."""
-    program = f'a = [num1 + num2 for sublist in [[1,2], [3,4]] for num1, num2 in sublist]'
+    operations expressions performed on the targets over a homogeneous list."""
+    program = f'a = [num1 + num2 for sublist in {iterable} for num1, num2 in sublist]'
+    module, TypeInferrer = cs._parse_text(program)
+    listcomp_node = list(module.nodes_of_class(astroid.ListComp))[0]
+    target_tvar = listcomp_node.type_environment.lookup_in_env(list(listcomp_node.locals.keys())[0])
+    iter_type = listcomp_node.generators[0].iter.type_constraints.type
+    assigned_tvar = module.type_environment.lookup_in_env('a')
+    expected_type = TypeInferrer.type_constraints.lookup_concrete(listcomp_node.elt.type_constraints.type)
+    assert (TypeInferrer.type_constraints.lookup_concrete(target_tvar) == iter_type.__args__[0]) and \
+           (TypeInferrer.type_constraints.lookup_concrete(assigned_tvar) == List[expected_type])
+
+
+@given(hs.lists(cs.random_list(min_size=1), min_size=1))
+def test_list_comprehension_multi_target_binop_nested_heterogeneous(iterable):
+    """Test Comprehension node visitor representing a nested comprehension expression with multiple targets and binary
+    operations expressions performed on the targets over a homogeneous list."""
+    program = f'a = [num1 + num2 for sublist in {iterable} for num1, num2 in sublist]'
     module, TypeInferrer = cs._parse_text(program)
     listcomp_node = list(module.nodes_of_class(astroid.ListComp))[0]
     target_tvar = listcomp_node.type_environment.lookup_in_env(list(listcomp_node.locals.keys())[0])

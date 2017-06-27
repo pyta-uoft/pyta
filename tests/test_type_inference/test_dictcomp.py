3a60d4225166e768138c592cd3b1790b0a7630d3
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-06-27 15:30:51 -0400

    Redocument and rename test case for clarity; add test case for heterogeneous dictionaries.

--- tests/test_type_inference/test_dictcomp.py
+++ tests/test_type_inference/test_dictcomp.py
@@ -7,9 +7,20 @@ settings.load_profile("pyta")
 
 
 @given(cs.homogeneous_dictionary(min_size=1))
-def test_dict_comprehension_single_target_name(dictionary):
-    """Test DictComp node visitor representing a comprehension expression with a single target and a
-    name expression over a Dictionary."""
+def test_dict_comprehension_reproduce_homogeneous(dictionary):
+    """Test DictComp node visitor representing a comprehension expression reproducing the the key, value pairs
+    of a homogeneous Dictionary."""
+    program = f"a = {{key: {dictionary}[key] for key in {dictionary}}}"
+    module, TypeInferrer = cs._parse_text(program)
+    dictcomp_node = list(module.nodes_of_class(astroid.DictComp))[0]
+    # reproducing the dict with dict. comprehension; the type of the expression will be same as original iterable
+    assert dictcomp_node.type_constraints.type == dictcomp_node.generators[0].iter.type_constraints.type
+
+
+@given(cs.heterogeneous_dictionary(min_size=1))
+def test_dict_comprehension_reproduce_heterogeneous(dictionary):
+    """Test DictComp node visitor representing a comprehension expression reproducing the the key, value pairs
+    of a heterogeneous Dictionary."""
     program = f"a = {{key: {dictionary}[key] for key in {dictionary}}}"
     module, TypeInferrer = cs._parse_text(program)
     dictcomp_node = list(module.nodes_of_class(astroid.DictComp))[0]

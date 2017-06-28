a566bd7a9486964ab92253d730b00561c9e5297a
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-06-28 14:16:34 -0400

    Implement test case for heterogeneous iterable.

--- tests/test_type_inference/test_setcomp.py
+++ tests/test_type_inference/test_setcomp.py
@@ -19,5 +19,18 @@ def test_set_comprehension_reproduce_homogeneous(iterable):
         assert setcomp_node.type_constraints.type == Set[setcomp_node.generators[0].iter.type_constraints.type]
 
 
+@given(cs.heterogeneous_iterable)
+def test_set_comprehension_reproduce_heterogeneous(iterable):
+    """Test SetComp node visitor representing a comprehension expression reproducing a set of elements of
+    a heterogeneous iterable."""
+    program = 'a = {elt for elt in ' + repr(iterable) + '}'
+    module, TypeInferrer = cs._parse_text(program)
+    setcomp_node = list(module.nodes_of_class(astroid.SetComp))[0]
+    if hasattr(setcomp_node.generators[0].iter.type_constraints.type, '__args__'):
+        assert setcomp_node.type_constraints.type == Set[setcomp_node.generators[0].iter.type_constraints.type.__args__[0]]
+    else:
+        assert setcomp_node.type_constraints.type == Set[setcomp_node.generators[0].iter.type_constraints.type]
+
+
 if __name__ == '__main__':
     nose.main()

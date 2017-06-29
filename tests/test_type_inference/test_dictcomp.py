5c057fe5ec5436b097817ed25041fa973477e497
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-06-29 16:18:02 -0400

    Reformat DictComp node test cases.

--- tests/test_type_inference/test_dictcomp.py
+++ tests/test_type_inference/test_dictcomp.py
@@ -9,8 +9,8 @@ settings.load_profile("pyta")
 def test_dict_comprehension_reproduce_homogeneous(dictionary):
     """Test DictComp node visitor representing a comprehension expression reproducing the the key, value pairs
     of a homogeneous Dictionary."""
-    program = f"a = {{key: {dictionary}[key] for key in {dictionary}}}"
-    module, TypeInferrer = cs._parse_text(program)
+    program = f'{{key: {dictionary}[key] for key in {dictionary}}}'
+    module, _ = cs._parse_text(program)
     dictcomp_node = list(module.nodes_of_class(astroid.DictComp))[0]
     # reproducing the dict with dict. comprehension; the type of the expression will be same as original iterable
     assert dictcomp_node.type_constraints.type == dictcomp_node.generators[0].iter.type_constraints.type
@@ -20,8 +20,8 @@ def test_dict_comprehension_reproduce_homogeneous(dictionary):
 def test_dict_comprehension_reproduce_heterogeneous(dictionary):
     """Test DictComp node visitor representing a comprehension expression reproducing the the key, value pairs
     of a heterogeneous Dictionary."""
-    program = f"a = {{key: {dictionary}[key] for key in {dictionary}}}"
-    module, TypeInferrer = cs._parse_text(program)
+    program = f"{{key: {dictionary}[key] for key in {dictionary}}}"
+    module, _ = cs._parse_text(program)
     dictcomp_node = list(module.nodes_of_class(astroid.DictComp))[0]
     assert dictcomp_node.type_constraints.type == dictcomp_node.generators[0].iter.type_constraints.type
 

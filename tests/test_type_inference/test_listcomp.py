0ee466edd0876a91808690b03967e2a720ab2d83
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-06-29 16:23:11 -0400

    Remove test cases for nested comprehensions; fix formatting of others. Simplify.

--- tests/test_type_inference/test_listcomp.py
+++ tests/test_type_inference/test_listcomp.py
@@ -3,7 +3,6 @@ import nose
 from hypothesis import settings, given
 from typing import List
 import tests.custom_hypothesis_support as cs
-import hypothesis.strategies as hs
 settings.load_profile("pyta")
 
 
@@ -11,117 +10,33 @@ settings.load_profile("pyta")
 def test_list_comprehension_single_target_name_homogeneous_iterable(iterable):
     """Test Comprehension node visitor representing a comprehension expression with a single target and a
     name expression over a homogeneous list."""
-    program = f'a = [num for num in {repr(iterable)}]'
-    module, TypeInferrer = cs._parse_text(program)
+    program = f'[num for num in {repr(iterable)}]'
+    module, typeinferrer = cs._parse_text(program)
     listcomp_node = list(module.nodes_of_class(astroid.ListComp))[0]
-    target_tvar = listcomp_node.type_environment.lookup_in_env(list(listcomp_node.locals.keys())[0])
-    iter_type = listcomp_node.generators[0].iter.type_constraints.type
-    assigned_tvar = module.type_environment.lookup_in_env('a')
-    expected_type = TypeInferrer.type_constraints.lookup_concrete(listcomp_node.elt.type_constraints.type)
-    if hasattr(listcomp_node.generators[0].iter.type_constraints.type, '__args__'):
-        assert (TypeInferrer.type_constraints.lookup_concrete(target_tvar) == iter_type.__args__[0]) and \
-               (TypeInferrer.type_constraints.lookup_concrete(assigned_tvar) == List[expected_type])
-    else:
-        assert (TypeInferrer.type_constraints.lookup_concrete(target_tvar) == iter_type) and \
-               (TypeInferrer.type_constraints.lookup_concrete(assigned_tvar) == List[expected_type])
+    expected_type = List[listcomp_node.generators[0].iter.type_constraints.type.__args__[0]]
+    assert listcomp_node.type_constraints.type == expected_type
 
 
-@given(cs.random_list(min_size=1))
+@given(cs.homogeneous_iterable)
 def test_list_comprehension_single_target_name_heterogeneous_iterable(iterable):
     """Test Comprehension node visitor representing a comprehension expression with a single target and a
     name expression over a heterogeneous list."""
-    program = f'a = [num for num in {repr(iterable)}]'
-    module, TypeInferrer = cs._parse_text(program)
+    program = f'[num for num in {repr(iterable)}]'
+    module, typeinferrer = cs._parse_text(program)
     listcomp_node = list(module.nodes_of_class(astroid.ListComp))[0]
-    # test whether the target variables have been unified correctly
-    # does the type of the elt, wrapped in a List form equal the type of concrete lookup of a
-    target_tvar = listcomp_node.type_environment.lookup_in_env(list(listcomp_node.locals.keys())[0])
-    iter_type = listcomp_node.generators[0].iter.type_constraints.type
-    assigned_tvar = module.type_environment.lookup_in_env('a')
-    expected_type = TypeInferrer.type_constraints.lookup_concrete(listcomp_node.elt.type_constraints.type)
-    if hasattr(listcomp_node.generators[0].iter.type_constraints.type, '__args__'):
-        assert (TypeInferrer.type_constraints.lookup_concrete(target_tvar) == iter_type.__args__[0]) and \
-               (TypeInferrer.type_constraints.lookup_concrete(assigned_tvar) == List[expected_type])
-    else:
-        assert (TypeInferrer.type_constraints.lookup_concrete(target_tvar) == iter_type) and \
-               (TypeInferrer.type_constraints.lookup_concrete(assigned_tvar) == List[expected_type])
+    expected_type = List[listcomp_node.generators[0].iter.type_constraints.type.__args__[0]]
+    assert listcomp_node.type_constraints.type == expected_type
 
 
-@given(hs.lists(cs.homogeneous_list(min_size=1), min_size=1))
-def test_list_comprehension_single_target_name_nested_homogeneous(iterable):
-    """Test Comprehension node visitor representing a nested comprehension expression with a name expression and
-    a single target over a homogeneous list."""
-    program = f'a = [num for sublist in {repr(iterable)} for num in sublist]'
-    module, TypeInferrer = cs._parse_text(program)
+@given(cs.valid_identifier(min_size=1))
+def test_list_comprehension_single_target_name_string(iterable):
+    """Test Comprehension node visitor representing a comprehension expression with a single target and a
+    name expression over a string."""
+    program = f'[num for num in {repr(iterable)}]'
+    module, typeinferrer = cs._parse_text(program)
     listcomp_node = list(module.nodes_of_class(astroid.ListComp))[0]
-    target_tvar = listcomp_node.type_environment.lookup_in_env(list(listcomp_node.locals.keys())[0])
-    iter_type = listcomp_node.generators[0].iter.type_constraints.type
-    assigned_tvar = module.type_environment.lookup_in_env('a')
-    expected_type = TypeInferrer.type_constraints.lookup_concrete(listcomp_node.elt.type_constraints.type)
-    if hasattr(listcomp_node.generators[0].iter.type_constraints.type, '__args__'):
-        assert (TypeInferrer.type_constraints.lookup_concrete(target_tvar) == iter_type.__args__[0]) and \
-               (TypeInferrer.type_constraints.lookup_concrete(assigned_tvar) == List[expected_type])
-    else:
-        assert (TypeInferrer.type_constraints.lookup_concrete(target_tvar) == iter_type) and \
-               (TypeInferrer.type_constraints.lookup_concrete(assigned_tvar) == List[expected_type])
-
-
-@given(hs.lists(cs.random_list(min_size=1), min_size=1))
-def test_list_comprehension_single_target_name_nested_heterogeneous(iterable):
-    """Test Comprehension node visitor representing a nested comprehension expression with a name expression and
-    a single target over a heterogeneous list."""
-    program = f'a = [num for sublist in {repr(iterable)} for num in sublist]'
-    module, TypeInferrer = cs._parse_text(program)
-    listcomp_node = list(module.nodes_of_class(astroid.ListComp))[0]
-    target_tvar = listcomp_node.type_environment.lookup_in_env(list(listcomp_node.locals.keys())[0])
-    iter_type = listcomp_node.generators[0].iter.type_constraints.type
-    assigned_tvar = module.type_environment.lookup_in_env('a')
-    expected_type = TypeInferrer.type_constraints.lookup_concrete(listcomp_node.elt.type_constraints.type)
-    if hasattr(listcomp_node.generators[0].iter.type_constraints.type, '__args__'):
-        assert (TypeInferrer.type_constraints.lookup_concrete(target_tvar) == iter_type.__args__[0]) and \
-               (TypeInferrer.type_constraints.lookup_concrete(assigned_tvar) == List[expected_type])
-    else:
-        assert (TypeInferrer.type_constraints.lookup_concrete(target_tvar) == iter_type) and \
-               (TypeInferrer.type_constraints.lookup_concrete(assigned_tvar) == List[expected_type])
-
-
-
-@given(hs.lists(cs.homogeneous_list(min_size=1), min_size=1))
-def test_list_comprehension_multi_target_binop_nested_homogeneous(iterable):
-    """test comprehension node visitor representing a nested comprehension expression with multiple targets and binary
-    operations expressions performed on the targets over a homogeneous list."""
-    program = f'a = [num1 + num2 for sublist in {repr(iterable)} for num1, num2 in sublist]'
-    module, TypeInferrer = cs._parse_text(program)
-    listcomp_node = list(module.nodes_of_class(astroid.ListComp))[0]
-    target_tvar = listcomp_node.type_environment.lookup_in_env(list(listcomp_node.locals.keys())[0])
-    iter_type = listcomp_node.generators[0].iter.type_constraints.type
-    assigned_tvar = module.type_environment.lookup_in_env('a')
-    expected_type = TypeInferrer.type_constraints.lookup_concrete(listcomp_node.elt.type_constraints.type)
-    if hasattr(listcomp_node.generators[0].iter.type_constraints.type, '__args__'):
-        assert (TypeInferrer.type_constraints.lookup_concrete(target_tvar) == iter_type.__args__[0]) and \
-               (TypeInferrer.type_constraints.lookup_concrete(assigned_tvar) == List[expected_type])
-    else:
-        assert (TypeInferrer.type_constraints.lookup_concrete(target_tvar) == iter_type) and \
-               (TypeInferrer.type_constraints.lookup_concrete(assigned_tvar) == List[expected_type])
-
-
-@given(hs.lists(cs.random_list(min_size=1), min_size=1))
-def test_list_comprehension_multi_target_binop_nested_heterogeneous(iterable):
-    """Test Comprehension node visitor representing a nested comprehension expression with multiple targets and binary
-    operations expressions performed on the targets over a homogeneous list."""
-    program = f'a = [num1 + num2 for sublist in {repr(iterable)} for num1, num2 in sublist]'
-    module, TypeInferrer = cs._parse_text(program)
-    listcomp_node = list(module.nodes_of_class(astroid.ListComp))[0]
-    target_tvar = listcomp_node.type_environment.lookup_in_env(list(listcomp_node.locals.keys())[0])
-    iter_type = listcomp_node.generators[0].iter.type_constraints.type
-    assigned_tvar = module.type_environment.lookup_in_env('a')
-    expected_type = TypeInferrer.type_constraints.lookup_concrete(listcomp_node.elt.type_constraints.type)
-    if hasattr(listcomp_node.generators[0].iter.type_constraints.type, '__args__'):
-        assert (TypeInferrer.type_constraints.lookup_concrete(target_tvar) == iter_type.__args__[0]) and \
-               (TypeInferrer.type_constraints.lookup_concrete(assigned_tvar) == List[expected_type])
-    else:
-        assert (TypeInferrer.type_constraints.lookup_concrete(target_tvar) == iter_type) and \
-               (TypeInferrer.type_constraints.lookup_concrete(assigned_tvar) == List[expected_type])
+    expected_type = List[listcomp_node.generators[0].iter.type_constraints.type]
+    assert listcomp_node.type_constraints.type == expected_type
 
 
 if __name__ == '__main__':

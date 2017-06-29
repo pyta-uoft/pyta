d6128d306d803106cfb4add01038a2f6693bc9d7
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-06-29 16:20:33 -0400

    Add test case for SetComp nodes over strings. Fix formatting.

--- tests/test_type_inference/test_setcomp.py
+++ tests/test_type_inference/test_setcomp.py
@@ -10,28 +10,30 @@ settings.load_profile("pyta")
 def test_set_comprehension_reproduce_homogeneous(iterable):
     """Test SetComp node visitor representing a comprehension expression reproducing a set of elements of
     a homogeneous iterable."""
-    program = 'a = {elt for elt in ' + repr(iterable) + '}'
-    module, TypeInferrer = cs._parse_text(program)
+    program = '{elt for elt in ' + repr(iterable) + '}'
+    module, _ = cs._parse_text(program)
     setcomp_node = list(module.nodes_of_class(astroid.SetComp))[0]
-    if hasattr(setcomp_node.generators[0].iter.type_constraints.type, '__args__'):
-        assert setcomp_node.type_constraints.type == Set[setcomp_node.generators[0].iter.type_constraints.type.__args__[0]]
-    else:
-        assert setcomp_node.type_constraints.type == Set[setcomp_node.generators[0].iter.type_constraints.type]
+    assert setcomp_node.type_constraints.type == Set[setcomp_node.generators[0].iter.type_constraints.type.__args__[0]]
 
 
 @given(cs.heterogeneous_iterable)
 def test_set_comprehension_reproduce_heterogeneous(iterable):
     """Test SetComp node visitor representing a comprehension expression reproducing a set of elements of
     a heterogeneous iterable."""
-    program = 'a = {elt for elt in ' + repr(iterable) + '}'
-    module, TypeInferrer = cs._parse_text(program)
+    program = '{elt for elt in ' + repr(iterable) + '}'
+    module, _ = cs._parse_text(program)
     setcomp_node = list(module.nodes_of_class(astroid.SetComp))[0]
-    if hasattr(setcomp_node.generators[0].iter.type_constraints.type, '__args__'):
-        assert setcomp_node.type_constraints.type == \
-               Set[setcomp_node.generators[0].iter.type_constraints.type.__args__[0]]
-    else:
-        assert setcomp_node.type_constraints.type == Set[setcomp_node.generators[0].iter.type_constraints.type]
+    assert setcomp_node.type_constraints.type == Set[setcomp_node.generators[0].iter.type_constraints.type.__args__[0]]
 
+
+@given(cs.valid_identifier(min_size=1))
+def test_set_comprehension_reproduce_string(iterable):
+    """Test SetComp node visitor representing a comprehension expression reproducing a set of elements of
+    a string."""
+    program = '{elt for elt in ' + repr(iterable) + '}'
+    module, _ = cs._parse_text(program)
+    setcomp_node = list(module.nodes_of_class(astroid.SetComp))[0]
+    assert setcomp_node.type_constraints.type == Set[setcomp_node.generators[0].iter.type_constraints.type]
 
 if __name__ == '__main__':
     nose.main()

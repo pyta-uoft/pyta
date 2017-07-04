8a11c084ae2cf611e73f70d2b73018ef35659763
rebeccakay <rebecca.kay@mail.utoronto.ca>
2017-07-04 05:52:23 -0400

    Fixed style issues and added expand all/collapse all

--- python_ta/reporters/templates/script.js
+++ python_ta/reporters/templates/script.js
@@ -1,11 +1,15 @@
 $("body").on("click", ".slider", function () {
     $slider = $(this);
-    //getting the next element
-    $content = $slider.next();
-    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
-    $content.slideToggle(500, function () {
-        //execute this after slideToggle is done
-        //change text of slider based on visibility of content div
+    $collapse = $slider.next();
+    $collapse.slideToggle(500, function () {
+        });
+
+});
+
+$("body").on("click", ".sliderall", function () {
+    $slider = $(this);
+    $contentall = $slider.next();
+    $contentall.slideToggle(500, function () {
         });
 
 });

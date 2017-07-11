dec9973ed9504bb5ab39addf60414152e7276852
rebeccakay <rebecca.kay@mail.utoronto.ca>
2017-07-11 03:04:43 -0400

    Fixed expand_all button

--- python_ta/reporters/templates/script.js
+++ python_ta/reporters/templates/script.js
@@ -1,15 +1,13 @@
 $("body").on("click", ".slider", function () {
-    $slider = $(this);
-    $collapse = $slider.next();
+    $collapse = $(this).parent().next();
     $collapse.slideToggle(500, function () {
         });
 
 });
 
 $("body").on("click", ".sliderall", function () {
-    $slider = $(this);
-    $contentall = $slider.next();
-    $contentall.slideToggle(500, function () {
+    $content = $(this).parent().next();
+    $content.slideToggle(500, function () {
         });
 
 });

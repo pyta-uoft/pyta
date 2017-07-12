1ec7bcfcb64d9767d68e882e83f8c600b9d6f6ea
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-07-12 15:55:47 -0400

    Remove bad try catch.

--- python_ta/transforms/type_inference_visitor.py
+++ python_ta/transforms/type_inference_visitor.py
@@ -335,11 +335,7 @@ class TypeInferer:
             func_t = self.type_constraints.lookup_concrete(
                 node.frame().locals[func_name][0].type_environment.locals['__init__'])
             arg_types = [_ForwardRef(func_name)] + [arg.type_constraints.type for arg in node.args]
-            try:
-                self.type_constraints.unify_call(func_t, *arg_types)
-            except Exception:
-                # TODO: Same issue as AssignAttr node visitor; bad unify to be fixed later - pass for now.
-                pass
+            self.type_constraints.unify_call(func_t, *arg_types)
             node.type_constraints = TypeInfo(_ForwardRef(func_name))
         else:
             func_t = self.type_constraints.lookup_concrete(node.frame().type_environment.locals[func_name])

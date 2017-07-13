f1ecadd6cba5c9c54d71c2c0084797cc8e89ad14
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-07-13 11:07:00 -0400

    Address changes in PR.

--- tests/test_type_inference/test_classdef.py
+++ tests/test_type_inference/test_classdef.py
@@ -14,33 +14,36 @@ def test_classdef_attribute_assign():
               f'        self.name = name\n' \
               f'        self.id = id' \
               f'\n' \
-              f'rogers = Network(\'Rogers\', 5)\n' \
-              f'rogers.name = \'BoB\'\n' \
-              f'self = Network(\'asdf\', 5)\n' \
-              f'self.name = \'asdfaBoB\'\n' \
+              f'rogers = Network("Rogers", 5)\n' \
+              f'rogers.name = "BoB"\n' \
+              f'self = Network("asdf", 5)\n' \
+              f'self.name = "asdfaBoB"\n' \
               f'\n'
     module, inferer = cs._parse_text(program)
     classdef_node = next(module.nodes_of_class(astroid.ClassDef))
-    for attribute_lst in list(classdef_node.instance_attrs.values()):
+    for attribute_lst in classdef_node.instance_attrs.values():
         for instance in attribute_lst:
             attribute_type = inferer.type_constraints\
                 .lookup_concrete(classdef_node.type_environment.lookup_in_env(instance.attrname))
             value_type = inferer.type_constraints.lookup_concrete(instance.parent.value.type_constraints.type)
             assert attribute_type == value_type
 
-# def test_classdef_method_call():
-#     program = f'class Network:\n' \
-#               f'    def __init__(self, name, id):\n' \
-#               f'        self.name = name\n' \
-#               f'        self.id = id' \
-#               f'\n' \
-#               f'rogers = Network(\'Rogers\', 5)\n' \
-#               f'rogers.name, rogers.id = \'BoB\', 10\n' \
-#               f'\n'
-#     module, inferer = cs._parse_text(program)
-#     # get the functionDef node - there is only one in this test case.
-#     classdef_node = next(module.nodes_of_class(astroid.ClassDef))
-#     a = 5
+
+def test_classdef_method_call():
+    """Test whether type of the method call are properly being set"""
+    program = f'class Network:\n' \
+              f'    def __init__(self, name, id):\n' \
+              f'        self.name = name\n' \
+              f'        self.id = id' \
+              f'    def change_name ' \
+              f'\n' \
+              f'rogers = Network(\'Rogers\', 5)\n' \
+              f'rogers.name, rogers.id = \'BoB\', 10\n' \
+              f'\n'
+    module, inferer = cs._parse_text(program)
+    # get the functionDef node - there is only one in this test case.
+    classdef_node = next(module.nodes_of_class(astroid.ClassDef))
+    a = 5
 
 if __name__ == '__main__':
     nose.main()

a6ec0fdff06adad5409891847ab05a915ce0a831
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-07-20 15:56:16 -0400

    Implement hypothesis test case without evaluating types.

--- tests/test_type_inference/test_annassign.py
+++ tests/test_type_inference/test_annassign.py
@@ -27,13 +27,13 @@ def test_annassign_concrete():
         assert variable_type == annotated_type
 
 
-@given(hs.dictionaries(cs.valid_identifier(), cs.annotation, min_size=2))
+@given(hs.dictionaries(cs.valid_identifier(), cs.builtin_type, min_size=2))
 def test_annassign(variables_annotations_dict):
     """Test whether types are being properly set for an AnnAssign node.
     """
     program = f'class Student:\n'
     for variable in variables_annotations_dict:
-        program += f'    {variable}: {variables_annotations_dict[variable]}\n'
+        program += f'    {variable}: {variables_annotations_dict[variable].__name__}\n'
     program += f'    def __init__(self):\n' \
                f'        pass\n'
     module, inferer = cs._parse_text(program)
@@ -41,8 +41,7 @@ def test_annassign(variables_annotations_dict):
         # variable_type = self._find_type(node, node.target.name)
         variable_type = inferer.type_constraints.lookup_concrete(
             inferer._closest_frame(node, node.target.name).type_environment.lookup_in_env(node.target.name))
-        # TODO: we don't want to evaluate.. Just hard code and test builtins?
-        annotated_type = _node_to_type(node.annotation.name)
+        annotated_type = variables_annotations_dict[node.target.name]
         assert variable_type == annotated_type
 
 

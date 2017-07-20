0a4a8300c0099bcd5ad39a462297e36ab18be8d8
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-07-20 14:16:55 -0400

    Implement helper to facilitate type lookups. Update visitors accordingly.

--- python_ta/transforms/type_inference_visitor.py
+++ python_ta/transforms/type_inference_visitor.py
@@ -124,11 +124,15 @@ class TypeInferer:
     ##############################################################################
     def _find_attribute_type(self, node, instance_name, attribute_name):
         """Given the node, class name and attribute name, return the type of the attribute."""
-        class_type = self.type_constraints.lookup_concrete(self._closest_frame(node, instance_name)
-                                                           .type_environment.lookup_in_env(instance_name))
+        class_type = self._find_type(node, instance_name)
         class_name = class_type.__forward_arg__
         class_env = self._closest_frame(node, class_name).locals[class_name][0].type_environment
         return self.type_constraints.lookup_concrete(class_env.lookup_in_env(attribute_name))
+
+    def _find_type(self, node, name):
+        """Given a variable name, return it's concrete type."""
+        tvar = self._closest_frame(node, name).type_environment.lookup_in_env(name)
+        return self.type_constraints.lookup_concrete(tvar)
 
     def visit_const(self, node):
         """Populate type constraints for astroid nodes for num/str/bool/None/bytes literals."""
@@ -199,13 +203,11 @@ class TypeInferer:
 
     def visit_name(self, node):
         try:
-            node.type_constraints = TypeInfo((self.type_constraints.lookup_concrete(
-                self._closest_frame(node, node.name)).type_environment.lookup_in_env(node.name)))
+            node.type_constraints = TypeInfo(self._find_type(node, node.name))
         except KeyError:
             self._closest_frame(node, node.name).type_environment\
                 .create_in_env(self.type_constraints, 'globals', node.name)
-            node.type_constraints = TypeInfo(self._closest_frame(node, node.name)
-                                             .type_environment.lookup_in_env(node.name))
+            node.type_constraints = TypeInfo(self._find_type(node, node.name))
 
     ##############################################################################
     # Operation nodes
@@ -325,8 +327,7 @@ class TypeInferer:
             func_type = parse_annotations(node)
             for arg_type, annotation in zip(arg_types, func_type.__args__[:-1]):
                 self.type_constraints.unify(arg_type, annotation)
-            self.type_constraints.unify(self._closest_frame(node, node.name)
-                                        .type_environment.lookup_in_env(node.name), func_type)
+            self.type_constraints.unify(self._find_type(node, node.name), func_type)
         else:
             # Check whether this is a method in a class
             if isinstance(node.parent, astroid.ClassDef) and isinstance(arg_types[0], TypeVar):
@@ -339,7 +340,7 @@ class TypeInferer:
                 rtype = self.type_constraints.lookup_concrete(node.type_environment.lookup_in_env('return'))
                 func_type = Callable[arg_types, rtype]
             func_type.polymorphic_tvars = [arg for arg in arg_types if isinstance(arg, TypeVar)]
-            self.type_constraints.unify(self._closest_frame(node, node.name).type_environment.lookup_in_env(node.name), func_type)
+            self.type_constraints.unify(self._find_type(node, node.name), func_type)
         node.type_constraints = TypeInfo(NoType)
 
     def visit_call(self, node):
@@ -387,11 +388,9 @@ class TypeInferer:
         rtype = self._handle_call(node, '__iter__', arg_type).type
         if isinstance(node.target, astroid.Tuple):
             for target_node in node.target.elts:
-                target_tvar = self._closest_frame(node, node.target.name).type_environment.lookup_in_env(target_node.name)
-                self.type_constraints.unify(target_tvar, rtype)
+                self.type_constraints.unify(self._find_type(target_node, target_node.name), rtype)
         else:
-            target_tvar = self._closest_frame(node, node.target.name).type_environment.lookup_in_env(node.target.name)
-            self.type_constraints.unify(target_tvar, rtype.__args__[0])
+            self.type_constraints.unify(self._find_type(node.target, node.target.name), rtype.__args__[0])
         node.type_constraints = TypeInfo(NoType)
 
     def visit_listcomp(self, node):

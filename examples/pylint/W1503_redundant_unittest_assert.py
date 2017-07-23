911d3a21a6ab539d3445fbca263f6aa9547553ac
ostrokach <ostrokach@gmail.com>
2017-07-23 01:14:09 -0400

    Update examples for the 'Miscellaneous' section

--- examples/pylint/W1503_redundant_unittest_assert.py
+++ examples/pylint/W1503_redundant_unittest_assert.py
@@ -1,6 +1,11 @@
 import unittest
 
 
+def is_sorted(lst):
+    """Check if <lst> is sorted in ascending order."""
+    return lst == sorted(lst)
+
+
 class TestStringMethods(unittest.TestCase):
     """Simple tests for example purposes."""
 
@@ -8,8 +13,8 @@ class TestStringMethods(unittest.TestCase):
         """Simple tests for example purposes."""
 
         # Valid:
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('bar'.isupper())
+        self.assertTrue(is_sorted([1, 2, 3]))
+        self.assertFalse(is_sorted([1, 3, 2]))
 
         # If a constant is passed as parameter, that condition is always true:
         self.assertTrue('YES')

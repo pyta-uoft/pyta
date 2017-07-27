06eb7139dec78b959c8223672e76664c4c461263
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-07-27 11:35:55 -0400

    Add test case for bad unify for an annotated call

--- tests/test_type_inference/test_bad_unification.py
+++ tests/test_type_inference/test_bad_unification.py
@@ -1,21 +1,9 @@
 import astroid
 import nose
-from hypothesis import given, settings
+from hypothesis import settings
 import tests.custom_hypothesis_support as cs
-import hypothesis.strategies as hs
-from python_ta.typecheck.base import _node_to_type
 settings.load_profile("pyta")
 
-
-# def test_bad_unify_assigned_binop_builtins():
-#     """Test whether unification algorithm is failing properly on trying to call a binary builtin operation
-#      of the wrong type on a pre-assigned variable.
-#     """
-#     program = f'a = 5\n' \
-#               f'a + "bob"\n'
-#     module, inferer = cs._parse_text(program)
-#     # TODO: does the type of the BinOp node correspond to correct TypeErrorInfo object?
-#     boi = 1
 
 def test_bad_unify_user_defined_annotated_call():
     """Test whether unification algorithm is failing as expected on trying to call an annotated user-defined function
@@ -27,8 +15,7 @@ def test_bad_unify_user_defined_annotated_call():
               f'add(1, "bob")\n'
     module, inferer = cs._parse_text(program)
     call_node = list(module.nodes_of_class(astroid.Call))[0]
-    # TODO: fix expected error msg.
-    expected_msg = f'U fukd up Boi'
+    expected_msg = 'Bad unify_call of function typing.Callable[[int, int], int] given args: int, str'
     assert call_node.type_constraints.type.msg == expected_msg
 
 

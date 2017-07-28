0f71ad2788a66f4c42725116dea09e315636d48c
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-07-28 01:16:37 -0400

    Implement test cases.

--- tests/test_type_inference/test_bad_unification.py
+++ tests/test_type_inference/test_bad_unification.py
@@ -2,6 +2,7 @@ import astroid
 import nose
 from hypothesis import settings
 import tests.custom_hypothesis_support as cs
+from typing import Callable
 settings.load_profile("pyta")
 
 
@@ -18,6 +19,22 @@ def test_bad_unify_user_defined_annotated_call():
     expected_msg = 'Bad unify_call of function typing.Callable[[int, int], int] given args: int, str'
     assert call_node.type_constraints.type.msg == expected_msg
 
+
+def test_bad_unify_user_defined_annotated_call_variable_args():
+    """Test whether unification algorithm is failing as expected on trying to call an annotated user-defined function
+    on the wrongly-typed non-primitive arguments.
+    """
+    program = f'def add(num1: int, num2: int) -> int:\n' \
+              f'    return num1 + num2\n' \
+              f'\n' \
+              f'a = 2\n' \
+              f'b = "bob"\n' \
+              f'add(a, b)\n' \
+              f''
+    module, inferer = cs._parse_text(program)
+    call_node = list(module.nodes_of_class(astroid.Call))[0]
+    expected_msg = 'Bad unify_call of function typing.Callable[[int, int], int] given args: int, str'
+    assert call_node.type_constraints.type.msg == expected_msg
 
 
 if __name__ == '__main__':

e3ce4b7a1dde8f0dc8052bb53d79c55a57c932dd
ostrokach <ostrokach@gmail.com>
2017-08-07 23:11:43 -0400

    Add 'return None' back to example

--- examples/pylint/E1128_assignment_from_none.py
+++ examples/pylint/E1128_assignment_from_none.py
@@ -2,9 +2,10 @@ from typing import List
 
 def add_fruit(fruit_basket: List[str], fruit: str) -> None:
     fruit_basket.append(fruit)
+    return None
 
 
-def main() -> None:
+def main():
     fruit_basket = ['apple', 'apple', 'orange']
     new_fruit_basket = add_fruit(fruit_basket, 'banana')  # Error on this line
-    print(new_fruit_basket)  # prints `None`
+    print(new_fruit_basket)  # Prints `None`

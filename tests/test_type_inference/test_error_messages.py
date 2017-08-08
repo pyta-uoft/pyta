53f4cebdfd3d9f2e3cbc76b00bea9876f2069048
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-08-08 15:10:33 -0400

    Convert all error messages to fstrings.

--- tests/test_type_inference/test_error_messages.py
+++ tests/test_type_inference/test_error_messages.py
@@ -12,9 +12,9 @@ def test_bad_attribute_access():
               f'x.wrong_name\n'
     module, inferer = cs._parse_text(program)
     call_node = next(module.nodes_of_class(astroid.Call))
-    expected_msg = "Attribute access error!\
-    				In the Attribute node in line 2:\
-    				the object "x" does not have the attribute "wrong_name"."
+    expected_msg = f'Attribute access error!\n' \
+                   f'In the Attribute node in line 2:\n' \
+                   f'the object "x" does not have the attribute "wrong_name".'
     assert call_node.type_constraints.type.msg == expected_msg
 
 
@@ -25,10 +25,10 @@ def test_builtin_method_call_bad_self():
               f'x.append(1.0)\n'
     module, inferer = cs._parse_text(program)
     call_node = next(module.nodes_of_class(astroid.Call))
-    expected_msg = "In the Call node in line 2, when calling the method "append":\
-                    this function expects to be called on an object of the class List, but was called on an object of\
-                    inferred type int."
-                   	# TODO: class versus type
+    expected_msg = f'In the Call node in line 2, when calling the method "append":\n' \
+                   f'this function expects to be called on an object of the class List, but was called on an object of ' \
+                   f'inferred type int.'
+                    # TODO: class versus type
     assert call_node.type_constraints.type.msg == expected_msg
 
 
@@ -39,9 +39,9 @@ def test_builtin_method_call_bad_argument():
               f'x.extend(1)\n'
     module, inferer = cs._parse_text(program)
     call_node = next(module.nodes_of_class(astroid.Call))
-    expected_msg = "In the Call node in line 2, when calling the method "extend":\
-                    in parameter (1), the function was expecting an object of type iterable \
-                    but was given an object of type int"
+    expected_msg = f'In the Call node in line 2, when calling the method "extend":\n' \
+                   f'in parameter (1), the function was expecting an object of type iterable ' \
+                   f'but was given an object of type int.'
     assert call_node.type_constraints.type.msg == expected_msg
 
 
@@ -54,12 +54,12 @@ def test_non_annotated_function_call_bad_arguments():
               f'add_num("bob", 1.0)\n'
     module, inferer = cs._parse_text(program)
     call_node = next(module.nodes_of_class(astroid.Call))
-    expected_msg = "In the Call node in line 4, there was an error in calling the function "add_num":\
-                     in parameter (1), the function was expecting an object of inferred type' \
-                     int but was given an object of type str' \
-                     in parameter (1), the function was expecting an object of inferred type' \
-                     int but was given an object of type float"
-                     # TODO: should we use the term inferred?
+    expected_msg = f'In the Call node in line 4, there was an error in calling the function "add_num":\n' \
+                   f'in parameter (1), the function was expecting an object of inferred type ' \
+                   f'int but was given an object of type str.\n' \
+                   f'in parameter (1), the function was expecting an object of inferred type ' \
+                   f'int but was given an object of type float.\n'
+                   # TODO: should we use the term inferred?
     assert call_node.type_constraints.type.msg == expected_msg
 
 
@@ -72,9 +72,9 @@ def test_user_defined_annotated_call_wrong_arguments_type():
               f'add_3(1, "bob", 1.0)\n'
     module, inferer = cs._parse_text(program)
     call_node = list(module.nodes_of_class(astroid.Call))[0]
-    expected_msg = "In the Call node in line 4, there was an error in calling the annotated function "add_3":\
-                     in parameter (2), the annotated type is int but was given an object of type str' \
-                     in parameter (3), the annotated type is int but was given an object of type float"
+    expected_msg = f'In the Call node in line 4, there was an error in calling the annotated function "add_3":\n' \
+                   f'in parameter (2), the annotated type is int but was given an object of type str.\n' \
+                   f'in parameter (3), the annotated type is int but was given an object of type float.\n'
     assert call_node.type_constraints.type.msg == expected_msg
 
 
@@ -87,8 +87,8 @@ def test_user_defined_annotated_call_wrong_arguments_number():
               f'add_3()\n'
     module, inferer = cs._parse_text(program)
     call_node = list(module.nodes_of_class(astroid.Call))[0]
-    expected_msg = "In the Call node in line 4, there was an error in calling the function "add_3":\
-                    the function was expecting 3 arguments, but was given 0."
+    expected_msg = f'In the Call node in line 4, there was an error in calling the function "add_3":\n' \
+                   f'the function was expecting 3 arguments, but was given 0.'
     assert call_node.type_constraints.type.msg == expected_msg
 
 
@@ -96,12 +96,12 @@ def test_nested_annotated_function_conflicting_body():
     """ User tries to define an annotated function which has conflicting types within its body.
     """
     program = f'def random_func(int1: int) -> None:\n' \
-              f'    int1 + "bob"\n' \
+              f'    int1 + "bob"\n'
     module, inferer = cs._parse_text(program)
     functiondef_type = inferer.lookup_type(module, "return_int")
-    expected_msg = "In the FunctionDef node in line 1, in the annotated Function Definition of "random_func" in line 1:\
-                    in parameter (1), "int1", the annotated type is int, which conflicts with it's inferred type of' \
-                    str from the function definition's body."
+    expected_msg = f'In the FunctionDef node in line 1, in the annotated Function Definition of "random_func" in line 1:\n' \
+                   f'in parameter (1), "int1", the annotated type is int, which conflicts with the inferred type of ' \
+                   f'str from the function definition body.'
                     # TODO: where in the body, or is this too convoluted? Extract from sets.
     assert functiondef_type.msg == expected_msg
 
@@ -116,9 +116,9 @@ def test_annotated_functiondef_conflicting_return_type():
               f'\n'
     module, inferer = cs._parse_text(program)
     functiondef_type = inferer.lookup_type(module, "return_str")
-    expected_msg = "In the FunctionDef node in line 1, in the annotated Function Definition of "random_func" in line 1:\
-                    the annotated return type is int, which conflicts with it's inferred type of' \
-                    str from the function definition's body."
+    expected_msg = f'In the FunctionDef node in line 1, in the annotated Function Definition of "random_func" in line 1:\n' \
+                   f'the annotated return type is int, which conflicts with the inferred return type of ' \
+                   f'str from the function definition body.'
     assert functiondef_type.msg == expected_msg
 
 
@@ -135,8 +135,9 @@ def test_conflicting_inferred_type_variable():
               f'return_str(x)\n'
     module, inferer = cs._parse_text(program)
     call_node = list(module.nodes_of_class(astroid.Call))[1]
-    expected_msg = "In the Call node in line 8, there was an error in calling the annotated function "return_str":\
-                     in parameter (1), the annotated type is str but was given an object of inferred type int."
+    expected_msg = f'In the Call node in line 8, there was an error in calling the annotated function "return_str":\n' \
+                   f'in parameter (1), the annotated type is str but was given an object of inferred type int.'
+                   # TODO: test case redundant because recursive..?
     assert call_node.type_constraints.type.msg == expected_msg
 
 

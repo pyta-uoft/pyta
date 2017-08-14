08d7a681c5f4f9e39c121a7c2f70a9a26be1635b
Alexey Strokach <ostrokach@gmail.com>
2017-08-13 20:43:12 -0400

    Don't wrap lines to comply with the 80 char limit (new informal limit is 120 chars)

--- examples/pylint/W1303_missing_format_argument_key.py
+++ examples/pylint/W1303_missing_format_argument_key.py
@@ -1,3 +1,2 @@
 # Error on the following line: missing format argument for 'age'
-s = '{last_name}, {fist_name} - {age}'.format(
-    last_name='bond', first_name='james')
+s = '{last_name}, {fist_name} - {age}'.format(last_name='bond', first_name='james')

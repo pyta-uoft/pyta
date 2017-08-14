3934f78cf00b48789eee654216500a59dde0e699
Alexey Strokach <ostrokach@gmail.com>
2017-08-13 20:38:09 -0400

    Correct argument type to be Optional[float]

--- examples/pylint/C0121_singleton_comparison.py
+++ examples/pylint/C0121_singleton_comparison.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-def square(number: float) -> Optional[float]:
+def square(number: Optional[float]) -> Optional[float]:
     if number == None:  # Error on this line
         return None
     else:

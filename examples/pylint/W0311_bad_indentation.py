3c3697bcc4edc613316f924659bc936f8e9df3b3
ostrokach <ostrokach@gmail.com>
2017-08-25 15:06:24 -0400

    Fix a few more examples

--- examples/pylint/W0311_bad_indentation.py
+++ examples/pylint/W0311_bad_indentation.py
@@ -1,2 +1,3 @@
 def print_greeting(name: str) -> None:
+      """Print a greeting message for the person with name <name>."""
       print('Hello {}!'.format(name))  # Bad indentation. Found 6 spaces, expected 4

0397103e0f0656588a1e8196894feda2e078588d
ostrokach <ostrokach@gmail.com>
2017-08-25 18:37:09 -0400

    Fix return type

--- examples/pylint/W0711_binary_op_exception.py
+++ examples/pylint/W0711_binary_op_exception.py
@@ -1,6 +1,4 @@
-from typing import Optional
-
-def divide_and_square(numerator: float, denominator: float) -> Optional[float]:
+def divide_and_square(numerator: float, denominator: float) -> float:
     """Divide the numerator by the denominator and square the result."""
     try:
         return (numerator / denominator) ** 2

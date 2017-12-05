5a86ceaa0c06bcf8da530208c84543f596e41206
david-yz-liu <david@cs.toronto.edu>
2017-12-04 20:08:49 -0500

    Disable tests in test_type_inferences/test_compare.py

--- tests/test_type_inference/test_compare.py
+++ tests/test_type_inference/test_compare.py
@@ -1,11 +1,13 @@
 import astroid
 import nose
+from nose.tools import nottest
 from hypothesis import given, settings, assume
 import tests.custom_hypothesis_support as cs
 import hypothesis.strategies as hs
 settings.load_profile("pyta")
 
 
+@nottest
 @given(cs.primitive_values, hs.lists(hs.tuples(cs.comparator_operator_equality, cs.primitive_values), min_size=1))
 def test_compare_equality(left_value, operator_value_tuples):
     """Test type setting of Compare node representing comparators: ''==', '!=', '>=', '<=', 'is'. """
@@ -20,6 +22,7 @@ def test_compare_equality(left_value, operator_value_tuples):
     assert compare_node.type_constraints.type == bool
 
 
+@nottest
 @given(hs.lists(cs.comparator_operator, min_size=3), cs.homogeneous_list(min_size=4))
 def test_compare_equality(operators, values):
     """Test type setting of Compare node representing comparators: '<', '>', 'in'. """

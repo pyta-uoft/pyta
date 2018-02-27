87639a3ff80c39dd53402d6d1905f367e8ecced4
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2018-02-26 19:25:51 -0500

    Update import and calls to OP_TO_DUNDER_BINARY in test_binops.

--- tests/test_type_inference/test_binops.py
+++ tests/test_type_inference/test_binops.py
@@ -2,7 +2,7 @@ import astroid
 import nose
 from hypothesis import given, settings, assume, HealthCheck
 import tests.custom_hypothesis_support as cs
-from python_ta.transforms.type_inference_visitor import op_to_dunder_binary
+from python_ta.transforms.type_inference_visitor import OP_TO_DUNDER_BINARY
 settings.load_profile("pyta")
 
 
@@ -14,7 +14,7 @@ def test_binop_non_bool_concrete(node):
     binop_node = list(module.nodes_of_class(astroid.BinOp))[0]
     left_type, right_type = binop_node.left.type_constraints.type, binop_node.right.type_constraints.type
     try:
-        exp_func_type = inferer.type_store.lookup_function(op_to_dunder_binary(node.op), left_type, right_type)
+        exp_func_type = inferer.type_store.lookup_function(OP_TO_DUNDER_BINARY[node.op], left_type, right_type)
         exp_return_type = exp_func_type.__args__[-1]
     except KeyError:
         exp_return_type = None

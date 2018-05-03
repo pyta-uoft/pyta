5a9e5b0dde1efcd7ef5381f5a7a4fa9d7332e652
Justin <Justinpark715@gmail.com>
2018-05-03 12:48:22 -0400

    Editing test_unify/tests.py to use helper functions

--- tests/test_unify/tests.py
+++ tests/test_unify/tests.py
@@ -1,163 +1,187 @@
 import astroid
 from typing import *
 from typing import TupleMeta, CallableMeta, _ForwardRef
-from python_ta.transforms.type_inference_visitor import  main
+from python_ta.transforms.type_inference_visitor import main
 from python_ta.typecheck.base import TypeConstraints, _TNode
 from nose import SkipTest
+from nose.tools import eq_
 
-# SkipTest documentation http://nose.readthedocs.io/en/latest/plugins/skip.html
-
-# TODO Should this be moved into a separate setup function?
 skip_msg = "Skipped"
+error_msg = "Error"
 tc = TypeConstraints()
 
-## Unify primitives
-# TODO These functions return 'type', should they return the individual type itself?
-# If not, how to more specifically check value of return type?
-def test_unify_bool():
-    assert isinstance((tc.unify(bool,bool)), type)
 
-def test_unify_str():
-    assert isinstance((tc.unify(str,str)), type)
+# Helper functions
+def unify_helper(arg1, arg2, exp_result):
+    unify_result = tc.unify(arg1, arg2)
+    if exp_result == error_msg:
+        eq_(type(unify_result), str)
+    else:
+        eq_(unify_result, exp_result)
 
-def test_unify_bool_int():
-    assert isinstance((tc.unify(bool, int)), type)
 
-def test_unify_int_bool():
-    assert isinstance((tc.unify(int, bool)), type)
+def setup_typevar(t: type):
+    tn = _TNode(t)
+    tv = tc.fresh_tvar(tn)
+    tc.unify(tv, t)
+    return tv
 
-def test_unify_bool_str():
-    #print(tc.unify(bool,str))
-    assert isinstance((tc.unify(bool,str)), str)
 
-def test_unify_int_str():
-    assert isinstance((tc.unify(int,str)), str)
+# Unify primitives
+def test_same_prim():
+    unify_helper(bool, bool, bool)
+    unify_helper(int, int, int)
+    unify_helper(str, str, str)
 
-## Unify TypeVars
-def test_unify_two_typevars():
-    tn1 = _TNode(str)
-    tvar1 = tc.fresh_tvar(tn1)
-    tc.unify(tvar1, str)
 
-    tn2 = _TNode(str)
-    tvar2 = tc.fresh_tvar(tn2)
-    tc.unify(tvar2, str)
+def test_diff_prim():
+    unify_helper(bool, int, bool)
+    unify_helper(bool, str, error_msg)
+    unify_helper(int, str, error_msg)
+    unify_helper(float, int, error_msg)
 
-    assert tc.resolve(tvar1) == str
-    assert tc.resolve(tvar2) == str
-    assert isinstance(tc.unify(tvar1, tvar2), TypeVar)
 
-def test_unify_two_typevars_wrong_types():
+def test_int_bool():
     raise SkipTest(skip_msg)
-    # This test currently raises a TypeInferenceError through the _merge_sets function_name
-    # Should be returning error message instead
+    # base.py returns the first arg, rather than the subclass
+    unify_helper(int, bool, bool)
 
-    tn1 = _TNode(str)
-    tvar_str = tc.fresh_tvar(tn1)
-    tc.unify(tvar_str, str)
 
-    tn2 = _TNode(int)
-    tvar_int = tc.fresh_tvar(tn2)
-    tc.unify(tvar_int, int)
+# Unify TypeVars
+def test_same_typevars():
+    tc.reset()
+    tv1 = setup_typevar(str)
+    tv2 = setup_typevar(str)
 
-    assert tc.resolve(tvar_str) == str
-    assert tc.resolve(tvar_int) == int
-    assert isinstance(tc.unify(tvar_str, tvar_int), str)
+    eq_(tc.resolve(tv1), str)
+    eq_(tc.resolve(tv2), str)
+    unify_helper(tv1, tv2, tv1)
+    unify_helper(tv2, tv1, tv2)
 
-def test_unify_two_typevars_bools():
-    # This test currently raises a TypeInferenceError through the _merge_sets function_name
-    # Should be returning error message instead
 
-    tn1 = _TNode(bool)
-    tvar1 = tc.fresh_tvar(tn1)
-    tc.unify(tvar1, bool)
+def test_same_typevars_flipped():
+    raise SkipTest(skip_msg)
+    # unify only returns the first TypeVar, without resolving to concrete type
+    tc.reset()
+    tv1 = setup_typevar(str)
+    tv2 = setup_typevar(str)
 
-    tn2 = _TNode(bool)
-    tvar2 = tc.fresh_tvar(tn2)
-    tc.unify(tvar2, bool)
+    unify_helper(tv1, tv2, tv2)
+    unify_helper(tv2, tv1, tv1)
 
-    assert tc.resolve(tvar1) == bool
-    assert tc.resolve(tvar2) == bool
 
-    assert tc.resolve(tvar1) == bool
-    assert tc.resolve(tvar2) == bool
-    assert isinstance(tc.unify(tvar1, tvar2), TypeVar)
+def test_diff_typevars():
+    raise SkipTest(skip_msg)
+    # unify raises excpetion in this case, rather than returning error string
+    tc.reset()
+    tv_str = setup_typevar(int)
+    tv_int = setup_typevar(str)
 
-def test_unify_one_typevar():
-    tn1 = _TNode(str)
-    tvar_str = tc.fresh_tvar(tn1)
-    tc.unify(tvar_str, str)
+    eq_(tc.resolve(tv_str), str)
+    eq_(tc.resolve(tv_int), int)
+    unify_helper(tv_int, tv_str, error_msg)
 
-    assert tc.resolve(tvar_str) == str
-    assert isinstance(tc.unify(tvar_str, str), type)
 
-def test_unify_one_typevar_wrong_type():
-    tn1 = _TNode(str)
-    tvar_str = tc.fresh_tvar(tn1)
-    tc.unify(tvar_str, str)
+def test_one_typevar():
+    tc.reset()
+    tv = setup_typevar(str)
 
-    assert tc.resolve(tvar_str) == str
-    assert isinstance(tc.unify(tvar_str, int), str)
+    eq_(tc.resolve(tv), str)
+    unify_helper(tv, str, str)
+    unify_helper(str, tv, str)
+    unify_helper(tv, int, error_msg)
+    unify_helper(int, tv, error_msg)
 
-def test_unify_one_typevar_wrong_type_flipped():
-    tn1 = _TNode(str)
-    tvar_str = tc.fresh_tvar(tn1)
-    tc.unify(tvar_str, str)
 
-    assert tc.resolve(tvar_str) == str
-    assert isinstance(tc.unify(int, tvar_str), str)
+def test_one_typevar_bool_int():
+    tc.reset()
+    tv = setup_typevar(bool)
 
-## Unify ForwardRefs
-def test_unify_forwardref():
+    eq_(tc.resolve(tv), bool)
+    unify_helper(tv, int, bool)
+    unify_helper(int, tv, bool)
+    unify_helper(tv, str, error_msg)
+
+
+# Unify ForwardRefs
+def test_same_forward_ref():
     fr1 = _ForwardRef('a')
     fr2 = _ForwardRef('a')
-    assert isinstance(tc.unify(fr1, fr2), _ForwardRef)
+    unify_helper(fr1, fr2, fr1)
+    unify_helper(fr1, fr2, fr2)
 
-def test_unify_forwardref_wrong_type():
+
+def test_diff_forward_ref():
     fr1 = _ForwardRef('a')
     fr2 = _ForwardRef('b')
-    assert isinstance(tc.unify(fr1, fr2), str)
+    unify_helper(fr1, fr2, error_msg)
 
-# TODO Shouldn't this result in delaying checking the forwardref until later?
-def test_unify_forwardref_str():
-    fr1 = _ForwardRef('a')
-    assert isinstance(tc.unify(fr1, str), str)
 
-## Unify tuples
-def test_unify_tuple_int():
-    assert isinstance(tc.unify(Tuple[int,int], Tuple[int,int]), TupleMeta)
+def test_one_forward_ref():
+    fr = _ForwardRef('a')
+    unify_helper(fr, str, error_msg)
 
-def test_unify_tuple_str():
-    assert isinstance(tc.unify(Tuple[str,str], Tuple[str,str]), TupleMeta)
 
-def test_unify_tuple_bool_int():
-    assert isinstance(tc.unify(Tuple[bool,bool], Tuple[int,int]), type(Tuple[bool,bool]))
-    assert isinstance(tc.unify(Tuple[bool,bool], Tuple[int,int]), TupleMeta)
+# Unify Tuples
+def test_same_tuple():
+    unify_helper(Tuple[int, int], Tuple[int, int], Tuple[int, int])
+    unify_helper(Tuple[str, str], Tuple[str, str], Tuple[str, str])
 
-def test_unify_tuple_str_int():
+
+def test_tuple_subclass():
+    unify_helper(Tuple[bool, bool], Tuple[int, int], Tuple[bool, bool])
+
+
+def test_tuple_subclass_flipped():
     raise SkipTest(skip_msg)
-    # should return error message
-    assert isinstance(tc.unify(Tuple[str,int], Tuple[int,int]), str)
+    # Should be returning Tuple[bool, bool], subclasses not returning in order
+    unify_helper(Tuple[int, int], Tuple[bool, bool], Tuple[bool, bool])
 
-## Unify lists
-def test_unify_list_str():
-    assert isinstance(tc.unify(List[str], List[str]), type)
 
-def test_unify_list_str_int():
+def test_tuple_str():
     raise SkipTest(skip_msg)
-    # should return error message
-    assert isinstance(tc.unify(List[str], List[int]), str)
+    # _unify_generic not properly checking for error messages, instead attempts
+    # to make invalid ForwardRef
+    unify_helper(Tuple[int, int], Tuple[str, str], error_msg)
 
-## Unify callables
-def test_unify_callable_bool_int():
+
+# Unify list
+def test_same_list():
+    unify_helper(List[str], List[str], List[str])
+    unify_helper(List[int], List[int], List[int])
+
+
+def test_diff_list():
+    raise SkipTest(skip_msg)
+    # _unify_generic not properly checking for error messages, instead attempts
+    # to make invalid ForwardRef
+    unify_helper(List[str], List[int], error_msg)
+
+
+# Unify callables
+def test_callable_subclass():
     c1 = Callable[[bool], bool]
     c2 = Callable[[int], int]
-    assert isinstance(tc.unify(c1, c2), CallableMeta)
-    assert isinstance(tc.unify(c1, c2), type(c1))
 
-def test_unify_callable_bool_str():
+    eq_(tc.resolve(c1), Callable[[bool], bool])
+    eq_(tc.resolve(c2), Callable[[int], int])
+    unify_helper(c1, c2, c1)
+
+
+def test_callable_subclass_flipped():
     raise SkipTest(skip_msg)
-    # should return error message
+    # Should be returning Callable[[bool], bool]
+    c1 = Callable[[bool], bool]
+    c2 = Callable[[int], int]
+
+    unify_helper(c2, c1, c1)
+
+
+def test_diff_callable():
+    raise SkipTest(skip_msg)
+    # _unify_generic not properly checking for error messages, instead attempts
+    # to make invalid ForwardRef
     c1 = Callable[[bool], bool]
     c2 = Callable[[str], str]
-    assert isinstance(tc.unify(c1, c2), str)
+
+    unify_helper(c1, c2, error_msg)

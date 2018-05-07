68e28e02ac9a9c1d2f061a04f338d21a9638775f
Justin <Justinpark715@gmail.com>
2018-05-07 13:42:54 -0400

    Adding imports to test_unify_subclasses.py

--- tests/test_type_constraints/test_unify_subclasses.py
+++ tests/test_type_constraints/test_unify_subclasses.py
@@ -1,4 +1,17 @@
-import test_unify
+from typing import *
+from typing import TupleMeta, CallableMeta, _ForwardRef
+from python_ta.typecheck.base import *
+from python_ta.transforms.type_inference_visitor import main
+from python_ta.typecheck.base import TypeConstraints
+from nose import SkipTest
+from nose.tools import eq_
+from nose import SkipTest
+from nose.tools import eq_
+from test_unify import unify_helper, resolve_helper, setup_typevar
+
+skip_msg = "Skipped"
+error_msg = "Error"
+tc = TypeConstraints()
 
 raise SkipTest(skip_msg)
 
@@ -9,7 +22,6 @@ def test_subclasses():
 
 
 def test_one_typevar_bool_int():
-    raise SkipTest("skip_msg")
     tc.reset()
     tv = setup_typevar(bool)
 
@@ -20,13 +32,11 @@ def test_one_typevar_bool_int():
 
 
 def test_tuple_subclass():
-    raise SkipTest(skip_msg)
     unify_helper(Tuple[bool, bool], Tuple[int, int], Tuple[bool, bool])
     unify_helper(Tuple[int, int], Tuple[bool, bool], Tuple[bool, bool])
 
 
 def test_callable_subclass():
-    raise SkipTest(skip_msg)
     c1 = Callable[[bool], bool]
     c2 = Callable[[int], int]
 

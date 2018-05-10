6525873e38c82b886ee83c54b2648ff12d3eca91
Justin <Justinpark715@gmail.com>
2018-05-10 14:10:11 -0400

    Editing printed tree to incoude inferred types

--- tests/print_ast_from_mod.py
+++ tests/print_ast_from_mod.py
@@ -4,6 +4,8 @@ Sanity checker to pretty print the abstract syntax tree (AST) structure.
 import sys
 import astroid
 from colorama import Back, Fore
+from python_ta.typecheck.base import TypeResult, TypeInfo, TypeFail, TypeConstraints
+from python_ta.transforms.type_inference_visitor import NoType
 
 USAGE = 'Usage: python print_ast.py <str representation of py file>'
 FILL = '  '
@@ -17,19 +19,23 @@ def walker(node, indent=''):
 
     node_name = str(node)[0:str(node).index('(')]
     value = None
+    inf_type = None
     if hasattr(node, 'value'):
         if '(' in str(node.value):
             value = str(node.value)[0:str(node.value).index('(')]
         else:
             value = node.value
+    if hasattr(node, 'inf_type'):
+        if node.inf_type == TypeInfo(NoType):
+            inf_type = "NoType"
+        else:
+            inf_type = str(node.inf_type.getValue())
 
     name = node.name if hasattr(node, 'name') else None
-    print('{}{} {} (name: {}, value: {})'.format(
-        indent, CHAR_TUBE, node_name, name, value))
+    print('{}{} {} (name: {}, value: {}, inf_type: {})'.format(
+        indent, CHAR_TUBE, node_name, name, value, inf_type))
 
-    lines = [line for line in node.as_string().split('\n')]
-    for line in lines: 
-        print(indent + FILL + '>>>' + Fore.BLUE + line + Fore.RESET)
+    print(indent + FILL + '>>> ' + node.as_string())
 
     for child in node.get_children(): 
         walker(child, indent + FILL + CHAR_PIPE)

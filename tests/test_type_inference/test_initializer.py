b60fa2cc20bbc9ee6e0636850f8684a59c1d7ace
Justin <Justinpark715@gmail.com>
2018-05-17 17:40:40 -0400

    Removing case of initializer with multiple arguments

--- tests/test_type_inference/test_initializer.py
+++ tests/test_type_inference/test_initializer.py
@@ -1,10 +1,7 @@
 import astroid
 from typing import _ForwardRef
-from python_ta.transforms.type_inference_visitor import TypeInferer
 import tests.custom_hypothesis_support as cs
-from python_ta.typecheck.base import TypeInfo, TypeFail
 from nose.tools import eq_
-from nose import SkipTest
 
 
 def test_class_with_init():
@@ -26,27 +23,10 @@ def test_class_without_init():
     program = """
     class Foo:
         def fee(self):
-            return 4
+            return 1
 
     foo = Foo()
     """
-    ast_mod, ti = cs._parse_text(program)
-    for call_node in ast_mod.nodes_of_class(astroid.Call):
-        assert isinstance(call_node.inf_type.getValue(), _ForwardRef)
-        eq_(call_node.inf_type.getValue(), _ForwardRef('Foo'))
-
-
-def test_class_with_multi_init():
-    program = """
-    class Foo:
-    
-        def __init__(self, x):
-            self.a = x
-
-    foo = Foo(5, 6)
-    """
-
-    # raise SkipTest("Should indicate that Foo() is called with the wrong number of arguments")
     ast_mod, ti = cs._parse_text(program)
     for call_node in ast_mod.nodes_of_class(astroid.Call):
         assert isinstance(call_node.inf_type.getValue(), _ForwardRef)

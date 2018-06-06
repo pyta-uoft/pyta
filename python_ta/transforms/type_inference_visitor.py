940c1b17e1b6dda3e1bdf33cb3189be8966324e2
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2018-06-06 13:33:37 -0400

    Import error returning function for subscript node and pass it to the appropriate visitor.

--- python_ta/transforms/type_inference_visitor.py
+++ python_ta/transforms/type_inference_visitor.py
@@ -6,7 +6,7 @@ import typing
 from typing import CallableMeta, TupleMeta, Union, _ForwardRef
 from astroid.transforms import TransformVisitor
 from ..typecheck.base import Environment, TypeConstraints, parse_annotations, create_Callable,_node_to_type, TypeResult, TypeInfo, TypeFail
-from ..typecheck.errors import BINOP_TO_METHOD, UNARY_TO_METHOD, binop_error_message, unaryop_error_message
+from ..typecheck.errors import BINOP_TO_METHOD, UNARY_TO_METHOD, binop_error_message, unaryop_error_message, subscript_error_message
 from ..typecheck.type_store import TypeStore
 
 
@@ -380,12 +380,12 @@ class TypeInferer:
     def visit_subscript(self, node: astroid.Subscript) -> None:
         if node.ctx == astroid.Load:
             node.inf_type = self._handle_call(node, '__getitem__', node.value.inf_type.getValue(),
-                                              node.slice.inf_type.getValue())
+                                              node.slice.inf_type.getValue(), error_func=subscript_error_message)
         elif node.ctx == astroid.Store:
             node.inf_type = TypeInfo(NoType) # type assigned when parent Assign node is visited
         elif node.ctx == astroid.Del:
             node.inf_type = self._handle_call(node, '__delitem__', node.value.inf_type.getValue(),
-                                              node.slice.inf_type.getValue())
+                                              node.slice.inf_type.getValue(), error_func=subscript_error_message)
 
     ##############################################################################
     # Loops

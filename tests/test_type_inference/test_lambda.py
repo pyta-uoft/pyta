6f5099b62a7f58937be5ddc0a915c2f3bf748d6d
Justin <Justinpark715@gmail.com>
2018-08-07 15:47:11 -0400

    typecheck: Generalizing str-based test in test_lambda

--- tests/test_type_inference/test_lambda.py
+++ tests/test_type_inference/test_lambda.py
@@ -60,7 +60,8 @@ def test_lambda_polymorphic_simple():
     """
     ast_mod, ti = cs._parse_text(src, reset=True)
     for lambda_node in ast_mod.nodes_of_class(astroid.Lambda):
-        assert str(lambda_node.inf_type.getValue()) == 'typing.Callable[[~_T0], ~_T0]'
+        x = ti.lookup_inf_type(lambda_node, 'x').getValue()
+        assert str(lambda_node.inf_type.getValue()) == f'typing.Callable[[{x}], {x}]'
     for var_node in ast_mod.nodes_of_class(astroid.AssignName):
         if var_node.name == 'y':
             y = ti.lookup_typevar(var_node, 'y')

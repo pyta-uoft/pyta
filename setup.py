e2a9a20b6d4d45f34fbc5e7fd50fcfd74351a7f9
David Liu <david@cs.toronto.edu>
2018-08-21 11:40:37 -0400

    v1.4.0 release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
     name='python-ta',
-    version='1.4.0b1',
+    version='1.4.0',
     description='Code checking tool for teaching Python',
     long_description=readme(),
     url='http://github.com/pyta-uoft/pyta',

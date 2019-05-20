1277feb10f8112872df1e2a86195959185078447
David Liu <david@cs.toronto.edu>
2019-05-20 15:43:32 -0400

    cfg: Begin tool for building control flow graphs.

---
+++ python_ta/cfg/visitor.py
@@ -0,0 +1,60 @@
+import astroid
+from astroid.node_classes import NodeNG
+from .graph import ControlFlowGraph, CFGBlock
+
+
+class CFGVisitor:
+    """An astroid visitor that creates a control flow graph for a given Python module.
+    """
+    cfg: ControlFlowGraph
+    _current_block: CFGBlock
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.cfg = ControlFlowGraph()
+        self._current_block = self.cfg.start
+
+    def __getattr__(self, attr: str):
+        if attr.startswith('visit_'):
+            return self.visit_generic
+        else:
+            raise AttributeError(f"'CFGVisitor' object has not attribute '{attr}'")
+
+    def visit_generic(self, node: NodeNG) -> None:
+        """By default, add the expression to the end of the current block."""
+        self._current_block.statements.append(node)
+
+    def visit_module(self, module: astroid.Module) -> None:
+        self.cfg = ControlFlowGraph()
+        self._current_block = self.cfg.start
+
+        for child in module.body:
+            child.accept(self)
+        self.cfg.link_or_merge(self._current_block, self.cfg.end)
+
+    def visit_if(self, node: astroid.If) -> None:
+        self._current_block.statements.append(node.test)
+        old_curr = self._current_block
+
+        # Handle "then" branch.
+        then_block = self.cfg.create_block(old_curr)
+        self._current_block = then_block
+        for child in node.body:
+            child.accept(self)
+        end_if = self._current_block
+
+        # Handle "else" branch.
+        if node.orelse == []:
+            end_else = old_curr
+        else:
+            else_block = self.cfg.create_block(old_curr)
+            self._current_block = else_block
+            for child in node.orelse:
+                child.accept(self)
+            end_else = self._current_block
+
+        after_if_block = self.cfg.create_block()
+        self.cfg.link_or_merge(end_if, after_if_block)
+        self.cfg.link_or_merge(end_else, after_if_block)
+
+        self._current_block = after_if_block
---
+++ sample_usage/draw_cfg.py
@@ -0,0 +1,54 @@
+import os.path
+from typing import Set
+from astroid.builder import AstroidBuilder
+import graphviz
+from python_ta.cfg import CFGVisitor, ControlFlowGraph, CFGBlock
+
+
+USAGE = 'USAGE: python draw_cfg.py <your-file.py>'
+GRAPH_OPTIONS = {
+    'format': 'jpg',
+    'node_attr': {
+        'shape': 'box',
+        'fontname': 'Courier New'
+    }
+}
+
+
+def display(cfg: ControlFlowGraph, filename: str, view: bool = True) -> None:
+    graph = graphviz.Digraph(name=filename, **GRAPH_OPTIONS)
+    _visit(cfg, cfg.start, graph, set())
+    graph.render(filename, view=view)
+
+
+def _visit(cfg: ControlFlowGraph, block: CFGBlock,
+           graph: graphviz.Digraph, visited: Set[int]) -> None:
+    if block.id in visited:
+        return
+
+    label = '\l'.join([s.as_string() for s in block.statements])
+    graph.node(str(block.id), label=label)
+    visited.add(block.id)
+
+    for edge in block.successors:
+        graph.edge(str(block.id), str(edge.target.id))
+        _visit(cfg, edge.target, graph, visited)
+
+
+def main(filepath: str) -> None:
+    filename = os.path.splitext(os.path.basename(filepath))[0]
+    mod = AstroidBuilder().file_build(filepath)
+    visitor = CFGVisitor()
+    mod.accept(visitor)
+
+    display(visitor.cfg, filename)
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print(USAGE)
+        exit(1)
+
+    filepath = sys.argv[1]
+    main(filepath)

d0fd46e57260359e40ffcbcec3403b833d1c2e03
David Liu <david@cs.toronto.edu>
2019-07-02 22:09:15 -0400

    cfg: Add CFG creation to PyTA parsing.

--- python_ta/patches/__init__.py
+++ python_ta/patches/__init__.py
@@ -1,7 +1,7 @@
 """Monkeypatch pylint behaviour.
 """
 from .checkers import patch_checkers
-from .type import patch_type_inference_transform
+from .transforms import patch_ast_transforms
 from .messages import patch_messages, patch_linter_transform
 from .error_messages import patch_error_messages
 
@@ -9,7 +9,7 @@ from .error_messages import patch_error_messages
 def patch_all():
     """Execute all patches defined in this module."""
     patch_checkers()
-    patch_type_inference_transform()
+    patch_ast_transforms()
     patch_messages()
     patch_linter_transform()
     patch_error_messages()

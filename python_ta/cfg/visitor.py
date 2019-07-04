812e3e3c5db0cbc1d397c8203cc9aba7fd8cb9ab
David Lim <davidhslim6793@gmail.com>
2019-07-04 14:28:49 -0400

    Fix tryexcept visitor

--- python_ta/cfg/visitor.py
+++ python_ta/cfg/visitor.py
@@ -1,7 +1,7 @@
 import astroid
 from astroid.node_classes import NodeNG
 from .graph import ControlFlowGraph, CFGBlock
-from typing import List, Tuple, Dict, Union
+from typing import List, Tuple, Dict, Union, Optional
 
 
 class CFGVisitor:
@@ -12,14 +12,16 @@ class CFGVisitor:
         The top of the stack corresponds to the end of the list.
         (compound statement [while], {'Break'/'Continue': CFGBlock to link to})
     """
-    cfg: ControlFlowGraph
-    _current_block: CFGBlock
+    cfgs: Dict[Union[astroid.FunctionDef, astroid.Module], ControlFlowGraph]
+    _current_cfg: Optional[ControlFlowGraph]
+    _current_block: Optional[CFGBlock]
     _control_boundaries: List[Tuple[NodeNG, Dict[str, CFGBlock]]]
 
     def __init__(self) -> None:
         super().__init__()
-        self.cfg = ControlFlowGraph()
-        self._current_block = self.cfg.start
+        self.cfgs = {}
+        self._current_cfg = None
+        self._current_block = None
         self._control_boundaries = []
 
     def __getattr__(self, attr: str):
@@ -33,19 +35,56 @@ class CFGVisitor:
         self._current_block.add_statement(node)
 
     def visit_module(self, module: astroid.Module) -> None:
-        self.cfg = ControlFlowGraph()
-        self._current_block = self.cfg.start
+        self.cfgs[module] = ControlFlowGraph()
+        self._current_cfg = self.cfgs[module]
+        self._current_block = self._current_cfg.start
+        module.cfg_block = self._current_cfg.start
 
         for child in module.body:
             child.accept(self)
-        self.cfg.link_or_merge(self._current_block, self.cfg.end)
+
+        self._current_cfg.link_or_merge(self._current_block, self._current_cfg.end)
+
+    def visit_classdef(self, node: astroid.ClassDef) -> None:
+        self._current_block.add_statement(node)
+
+        for child in node.body:
+            if isinstance(child, astroid.FunctionDef):
+                child.accept(self)
+
+    def visit_functiondef(self, func: astroid.FunctionDef) -> None:
+        if not isinstance(func.parent, astroid.ClassDef):
+            self._current_block.add_statement(func)
+
+        previous_cfg = self._current_cfg
+        previous_block = self._current_block
+
+        self.cfgs[func] = ControlFlowGraph()
+        self._current_cfg = self.cfgs[func]
+
+        self._control_boundaries.append((func, {astroid.Return.__name__: self._current_cfg.end}))
+
+        self._current_cfg.start.add_statement(func.args)
+        func.cfg_block = self._current_cfg.start
+
+        self._current_block = self._current_cfg.create_block(self._current_cfg.start)
+
+        for child in func.body:
+            child.accept(self)
+
+        self._control_boundaries.pop()
+
+        self._current_cfg.link_or_merge(self._current_block, self._current_cfg.end)
+        self._current_block = previous_block
+        self._current_cfg = previous_cfg
 
     def visit_if(self, node: astroid.If) -> None:
         self._current_block.add_statement(node.test)
+        node.cfg_block = self._current_block
         old_curr = self._current_block
 
         # Handle "then" branch.
-        then_block = self.cfg.create_block(old_curr)
+        then_block = self._current_cfg.create_block(old_curr)
         self._current_block = then_block
         for child in node.body:
             child.accept(self)
@@ -55,15 +94,15 @@ class CFGVisitor:
         if node.orelse == []:
             end_else = old_curr
         else:
-            else_block = self.cfg.create_block(old_curr)
+            else_block = self._current_cfg.create_block(old_curr)
             self._current_block = else_block
             for child in node.orelse:
                 child.accept(self)
             end_else = self._current_block
 
-        after_if_block = self.cfg.create_block()
-        self.cfg.link_or_merge(end_if, after_if_block)
-        self.cfg.link_or_merge(end_else, after_if_block)
+        after_if_block = self._current_cfg.create_block()
+        self._current_cfg.link_or_merge(end_if, after_if_block)
+        self._current_cfg.link_or_merge(end_else, after_if_block)
 
         self._current_block = after_if_block
 
@@ -71,82 +110,132 @@ class CFGVisitor:
         old_curr = self._current_block
 
         # Handle "test" block
-        test_block = self.cfg.create_block()
+        test_block = self._current_cfg.create_block()
         test_block.add_statement(node.test)
-        self.cfg.link_or_merge(old_curr, test_block)
+        node.cfg_block = test_block
+        self._current_cfg.link_or_merge(old_curr, test_block)
 
-        after_while_block = self.cfg.create_block()
+        after_while_block = self._current_cfg.create_block()
 
         # step into while
         self._control_boundaries.append((node, {astroid.Break.__name__: after_while_block,
                                                 astroid.Continue.__name__: test_block}))
 
         # Handle "body" branch
-        body_block = self.cfg.create_block(test_block)
+        body_block = self._current_cfg.create_block(test_block)
         self._current_block = body_block
         for child in node.body:
             child.accept(self)
         end_body = self._current_block
-        self.cfg.link_or_merge(end_body, test_block)
+        self._current_cfg.link_or_merge(end_body, test_block)
 
         # step out of while
         self._control_boundaries.pop()
 
         # Handle "else" branch
-        else_block = self.cfg.create_block(test_block)
+        else_block = self._current_cfg.create_block(test_block)
         self._current_block = else_block
         for child in node.orelse:
             child.accept(self)
         end_else = self._current_block
 
-        self.cfg.link_or_merge(end_else, after_while_block)
+        self._current_cfg.link_or_merge(end_else, after_while_block)
         self._current_block = after_while_block
 
+    def visit_for(self, node: astroid.For) -> None:
+        old_curr = self._current_block
+        old_curr.add_statement(node.iter)
+        node.cfg_block = old_curr
+
+        # Handle "test" block
+        test_block = self._current_cfg.create_block()
+        test_block.add_statement(node.target)
+        self._current_cfg.link_or_merge(old_curr, test_block)
+
+        after_for_block = self._current_cfg.create_block()
+
+        # step into for
+        self._control_boundaries.append((node, {astroid.Break.__name__: after_for_block,
+                                                astroid.Continue.__name__: test_block}))
+
+        # Handle "body" branch
+        body_block = self._current_cfg.create_block(test_block)
+        self._current_block = body_block
+        for child in node.body:
+            child.accept(self)
+        end_body = self._current_block
+        self._current_cfg.link_or_merge(end_body, test_block)
+
+        # step out of for
+        self._control_boundaries.pop()
+
+        # Handle "else" branch
+        else_block = self._current_cfg.create_block(test_block)
+        self._current_block = else_block
+        for child in node.orelse:
+            child.accept(self)
+        end_else = self._current_block
+
+        self._current_cfg.link_or_merge(end_else, after_for_block)
+        self._current_block = after_for_block
+
     def visit_break(self, node: astroid.Break) -> None:
-        self._visit_continue_or_break(node)
+        self._visit_jump(node)
 
     def visit_continue(self, node:astroid.Continue) -> None:
-        self._visit_continue_or_break(node)
+        self._visit_jump(node)
 
-    def _visit_continue_or_break(self, node: Union[astroid.Break, astroid.Continue]) -> None:
+    def visit_return(self, node: astroid.Return) -> None:
+        self._visit_jump(node)
+
+    def _visit_jump(self, node: Union[astroid.Break, astroid.Continue, astroid.Return]) -> None:
         old_curr = self._current_block
         for boundary, exits in reversed(self._control_boundaries):
-            if isinstance(boundary, astroid.While):
-                self.cfg.link(old_curr, exits[type(node).__name__])
+            if type(node).__name__ in exits:
+                self._current_cfg.link(old_curr, exits[type(node).__name__])
                 old_curr.add_statement(node)
                 break
         else:
-            raise SyntaxError(f'\'{type(node).__name__}\' outside loop')
-        unreachable_block = self.cfg.create_block()
+            raise SyntaxError(f'\'{type(node).__name__}\' outside'
+                              f' {"function" if isinstance(node, astroid.Return) else "loop"}')
+        unreachable_block = self._current_cfg.create_block()
         self._current_block = unreachable_block
 
     def visit_tryexcept(self, node: astroid.TryExcept) -> None:
         old_curr = self._current_block
-        after_tryexcept_block = self.cfg.create_block()
+        after_tryexcept_block = self._current_cfg.create_block()
 
+        # Create blocks for handlers and store them
         handlers = []
         for handler in node.handlers:
-            self._current_block = self.cfg.create_block()
+            self._current_block = self._current_cfg.create_block()
             handler.accept(self)
             handlers.append(self._current_block)
+            self._current_cfg.link_or_merge(self._current_block, after_tryexcept_block)
 
-        # Handle "try" block
         for child in node.body:
-            self._current_block = self.cfg.create_block(old_curr)
+            if not isinstance(child, astroid.TryExcept):
+                self._current_block = self._current_cfg.create_block(old_curr)
             child.accept(self)
-            old_curr = self._current_block
-            # Handle "except" blocks
+            # Exception raised
+            # Catch exception
             for handler in handlers:
-                self.cfg.link_or_merge(old_curr, handler)
-                self.cfg.link_or_merge(handler, after_tryexcept_block)
-            # Unhandled exception
-            raise_block = self.cfg.create_block(old_curr)
-            raise_block.add_statement(astroid.Raise())
+                self._current_cfg.link(self._current_block, handler)
+            # Can't catch exception
+            # Still possible to catch if there is an handler in outer block
+            if isinstance(node.parent, astroid.TryExcept):
+                self._current_cfg.link(self._current_block, after_tryexcept_block)
+            # No outer block
+            else:
+                self._current_cfg.link(self._current_block, self._current_cfg.end)
+            old_curr = self._current_block
 
-        # Handle "else" block
-        self._current_block = self.cfg.create_block(old_curr)
-        for child in node.orelse:
-            child.accept(self)
+        # No exception raised
+        # If there is an else block
+        if node.orelse:
+            self._current_block = self._current_cfg.create_block(old_curr)
+            for child in node.orelse:
+                child.accept(self)
 
-        self.cfg.link_or_merge(self._current_block, after_tryexcept_block)
+        self._current_cfg.link(self._current_block, after_tryexcept_block)
         self._current_block = after_tryexcept_block

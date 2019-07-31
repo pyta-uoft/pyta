e408b3480e937c061a59dc040eb58ad906a9e7b0
Kavin Singh <kavin.singh@mail.utoronto.ca>
2019-07-31 12:16:16 -0400

    updated class def visitor

--- tests/test_cfg/test_classdef.py
+++ tests/test_cfg/test_classdef.py
@@ -3,7 +3,8 @@ import astroid
 from python_ta.cfg import CFGVisitor, ControlFlowGraph
 
 
-def build_cfgs(src: str) -> Dict[Union[astroid.FunctionDef, astroid.Module], ControlFlowGraph]:
+def build_cfgs(src: str) -> Dict[
+    Union[astroid.FunctionDef, astroid.Module], ControlFlowGraph]:
     mod = astroid.parse(src)
     t = CFGVisitor()
     mod.accept(t)
@@ -30,8 +31,33 @@ def test_simple_class() -> None:
     keys = list(cfgs)
 
     expected_blocks = [
-        ['x = 0', '\n\nclass A(CFGVisitor):\n    pass\n', 'print(x)'],
+        ['x = 0', 'pass', 'print(x)'],
         []
+    ]
+    assert expected_blocks == _extract_blocks(cfgs[keys[0]])
+
+
+def test_compound_statement_in_class() -> None:
+    src = """
+    x = 0
+    class A(CFGVisitor):
+        if x > 5:
+            print(x)
+        else:
+            print(y)
+    print(z)
+    """
+    cfgs = build_cfgs(src)
+    assert len(cfgs) == 1
+
+    keys = list(cfgs)
+
+    expected_blocks = [
+        ['x = 0', 'x > 5'],
+        ['print(x)'],
+        ['print(z)'],
+        [],
+        ['print(y)']
     ]
     assert expected_blocks == _extract_blocks(cfgs[keys[0]])
 
@@ -42,7 +68,7 @@ def test_class_with_one_method() -> None:
         \"""This is a class docstring\"""
         c: int
         d: str
-    
+
         def __init__(self) -> None:
             self.c = 10
             self.d = 'string'
@@ -53,9 +79,8 @@ def test_class_with_one_method() -> None:
     keys = list(cfgs)
 
     expected_blocks_module = [
-        ['\n\nclass A(CFGVisitor):\n    """This is a class docstring"""\n    '
-         'c: int\n    d: str\n    \n    def __init__(self)->None:\n        '
-         'self.c = 10\n        self.d = \'string\'\n'],
+        ['c: int', 'd: str', '\ndef __init__(self)->None:\n    '
+                             'self.c = 10\n    self.d = \'string\''],
         []
     ]
     assert expected_blocks_module == _extract_blocks(cfgs[keys[0]])
@@ -78,7 +103,7 @@ def test_class_with_multiple_method() -> None:
         def __init__(self) -> None:
             self.c = 10
             self.d = 'string'
-        
+
         def runner(self) -> str:
             return 'run'
     """
@@ -88,10 +113,10 @@ def test_class_with_multiple_method() -> None:
     keys = list(cfgs)
 
     expected_blocks_module = [
-        ['\n\nclass A(CFGVisitor):\n    """This is a class docstring"""\n    '
-         'c: int\n    d: str\n    \n    def __init__(self)->None:\n        '
-         'self.c = 10\n        self.d = \'string\'\n    \n    '
-         'def runner(self)->str:\n        return \'run\'\n'],
+        ['c: int', 'd: str', '\ndef __init__(self)->None:\n    '
+                             'self.c = 10\n    self.d = \'string\'',
+         '\ndef runner(self)->str:\n    '
+         'return \'run\''],
         []
     ]
     assert expected_blocks_module == _extract_blocks(cfgs[keys[0]])

c2fd94a4fe801c6f1aa978bd9c1a1f01cf6a012c
Kavin Singh <kavin.singh@mail.utoronto.ca>
2019-08-29 15:53:47 -0400

    updated tests

--- tests/test_custom_checkers/test_possible_infinite_loop_checker.py
+++ tests/test_custom_checkers/test_possible_infinite_loop_checker.py
@@ -41,10 +41,10 @@ class TestPossiblyUndefinedChecker(pylint.testutils.CheckerTestCase):
         with self.assertNoMessages():
             self.checker.visit_while(while_node)
 
-    def test_message_immutable_type(self):
+    def test_no_message_mutable_type_usage(self):
         src = """
-        x = 'lol'
-        while  x == '123':
+        x = [1, 2, 3]
+        while x:
             print(x)
         """
         mod = astroid.parse(src)
@@ -54,12 +54,57 @@ class TestPossiblyUndefinedChecker(pylint.testutils.CheckerTestCase):
         type_inferer.type_inference_transformer().visit(mod)
 
         while_node, *_ = mod.nodes_of_class(astroid.While)
-        with self.assertAddsMessages(
-                pylint.testutils.Message(
-                    msg_id='possible-infinite-loop',
-                    node=while_node,
-                ),
-        ):
+        with self.assertNoMessages():
+            self.checker.visit_while(while_node)
+
+    def test_no_message_mutable_type_assign(self):
+        src = """
+        x = [1, 2, 3]
+        while x:
+            x = False
+        """
+        mod = astroid.parse(src)
+        mod.accept(CFGVisitor())
+        type_inferer = TypeInferer()
+        type_inferer.environment_transformer().visit(mod)
+        type_inferer.type_inference_transformer().visit(mod)
+
+        while_node, *_ = mod.nodes_of_class(astroid.While)
+        with self.assertNoMessages():
+            self.checker.visit_while(while_node)
+
+    def test_no_message_multiple_vars(self):
+        src = """
+        x = [1, 2, 3]
+        y = 123
+        while x and y:
+            x.pop()
+        """
+        mod = astroid.parse(src)
+        mod.accept(CFGVisitor())
+        type_inferer = TypeInferer()
+        type_inferer.environment_transformer().visit(mod)
+        type_inferer.type_inference_transformer().visit(mod)
+
+        while_node, *_ = mod.nodes_of_class(astroid.While)
+        with self.assertNoMessages():
+            self.checker.visit_while(while_node)
+
+    def test_no_message_global_var(self):
+        src = """
+        x = [1, 2, 3]
+        def func():
+            while x:
+                mutate_x()
+        """
+        mod = astroid.parse(src)
+        mod.accept(CFGVisitor())
+        type_inferer = TypeInferer()
+        type_inferer.environment_transformer().visit(mod)
+        type_inferer.type_inference_transformer().visit(mod)
+
+        while_node, *_ = mod.nodes_of_class(astroid.While)
+        with self.assertNoMessages():
             self.checker.visit_while(while_node)
 
     def test_message_simple(self):
@@ -83,11 +128,75 @@ class TestPossiblyUndefinedChecker(pylint.testutils.CheckerTestCase):
         ):
             self.checker.visit_while(while_node)
 
+    def test_message_immutable_type(self):
+        src = """
+        x = 'lol'
+        while  x == '123':
+            print(x)
+        """
+        mod = astroid.parse(src)
+        mod.accept(CFGVisitor())
+        type_inferer = TypeInferer()
+        type_inferer.environment_transformer().visit(mod)
+        type_inferer.type_inference_transformer().visit(mod)
+
+        while_node, *_ = mod.nodes_of_class(astroid.While)
+        with self.assertAddsMessages(
+                pylint.testutils.Message(
+                    msg_id='possible-infinite-loop',
+                    node=while_node,
+                ),
+        ):
+            self.checker.visit_while(while_node)
+
     def test_message_mutable_type(self):
         src = """
         x = [1, 2, 3]
         while  x:
             print(10)
+        """
+        mod = astroid.parse(src)
+        mod.accept(CFGVisitor())
+        type_inferer = TypeInferer()
+        type_inferer.environment_transformer().visit(mod)
+        type_inferer.type_inference_transformer().visit(mod)
+
+        while_node, *_ = mod.nodes_of_class(astroid.While)
+        with self.assertAddsMessages(
+                pylint.testutils.Message(
+                    msg_id='possible-infinite-loop',
+                    node=while_node,
+                ),
+        ):
+            self.checker.visit_while(while_node)
+
+    def test_message_func_name_identical_to_var(self):
+        src = """
+        x = [1, 2, 3]
+        while x:
+            x()
+        """
+        mod = astroid.parse(src)
+        mod.accept(CFGVisitor())
+        type_inferer = TypeInferer()
+        type_inferer.environment_transformer().visit(mod)
+        type_inferer.type_inference_transformer().visit(mod)
+
+        while_node, *_ = mod.nodes_of_class(astroid.While)
+        with self.assertAddsMessages(
+                pylint.testutils.Message(
+                    msg_id='possible-infinite-loop',
+                    node=while_node,
+                ),
+        ):
+            self.checker.visit_while(while_node)
+
+    def test_message_multiple_vars_immutable_type(self):
+        src = """
+        x = [1, 2, 3]
+        y = 123
+        while x and y:
+            print(y)
         """
         mod = astroid.parse(src)
         mod.accept(CFGVisitor())

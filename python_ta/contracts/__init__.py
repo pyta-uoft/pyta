69604c9cbbcf856cde898bc63b63f2998b2d43ec
Ibrahim Hasan <ibrahim.hasan@mail.utoronto.ca>
2020-02-08 16:29:30 -0500

    Adds class contracts and representation invariants
    This sets up the basic structure for class contracts and adds tests.
    There’s still a long way to go.

--- python_ta/contracts/__init__.py
+++ python_ta/contracts/__init__.py
@@ -1,39 +1,69 @@
 from typing import Any, List, Optional
 import typing
+import inspect
 import wrapt
 
 
-class InvalidPrecondition(Exception):
-    """Exception raised when precondition is invalid
+class InvalidCondition(Exception):
+    """Exception raised when a condition is invalid
     """
 
+
 @wrapt.decorator
-def check_contracts(wrapped, _instance, args, kwargs):
+def check_contracts(wrapped, instance, args, kwargs):
+    if instance is None and inspect.isclass(wrapped):
+        return add_class_contracts(wrapped, args, kwargs)
+    return check_function_contracts(wrapped, instance, args, kwargs)
+
+
+def add_class_contracts(wrapped, args, kwargs):
+    rep_invariants = parse_conditions(
+        wrapped.__doc__ or '', parse_token="Representation Invariant")
+    setattr(wrapped, "__representation_invariants__", rep_invariants)
+    return wrapped(*args, **kwargs)
+
+
+def check_function_contracts(wrapped, instance, args, kwargs):
     params = wrapped.__code__.co_varnames[:wrapped.__code__.co_argcount]
     annotations = typing.get_type_hints(wrapped)
-    for arg, param in zip(args, params):
+
+    args_with_self = (instance,) + args if instance else args
+
+    for arg, param in zip(args_with_self, params):
         if param in annotations:
             assert check_type_annotation(annotations[param], arg),\
                 f'Argument {repr(arg)} did not match type annotation for parameter "{param}: {annotations[param]}"'
 
-    preconditions = parse_preconditions(wrapped.__doc__ or '')
-    function_locals = dict(zip(params, args))
-    for precondition in preconditions:
-        try:
-            check = eval(precondition, wrapped.__globals__, function_locals)
-        except:
-            # TODO: Decide what to do here, e.g. "Invalid precondition"
-            raise InvalidPrecondition(
-                f'Error evaluating precondition: {precondition}')
-        else:
-            assert check,\
-                f'Precondition "{precondition}" violated for arguments {function_locals}'
+    preconditions = parse_conditions(wrapped.__doc__ or '')
+
+    function_locals = dict(zip(params, args_with_self))
+    validate_conditions(wrapped, function_locals, preconditions)
+
     r = wrapped(*args, **kwargs)
+
+    if instance and hasattr(type(instance), "__representation_invariants__"):
+        validate_conditions(wrapped, function_locals,
+                            type(instance).__representation_invariants__)
+
     if 'return' in annotations:
         return_type = annotations['return']
         assert check_type_annotation(return_type, r),\
             f'Return value {r} does not match annotated return type {return_type.__name__}'
     return r
+
+
+def validate_conditions(wrapped, function_locals, conditions):
+    for condition in conditions:
+        try:
+            check = eval(condition, wrapped.__globals__, function_locals)
+        except:
+            # TODO: Decide what to do here, e.g. "Invalid condition"
+            raise InvalidCondition(
+                f'Error evaluating condition: {condition}')
+        else:
+            assert check,\
+                f'Condition "{condition}" violated for arguments {function_locals}'
+
 
 def check_type_annotation(annotation: Optional[type], v: Any) -> bool:
     """Return whether v is compatible with the given type annotation."""
@@ -55,35 +85,36 @@ def check_type_annotation(annotation: Optional[type], v: Any) -> bool:
             return True
 
 
-def parse_preconditions(docstring: str) -> List[str]:
-    """Return a list of preconditions parsed from the given docstring.
+def parse_conditions(docstring: str, parse_token: str = "Precondition") -> List[str]:
+    """Return a list of preconditions/representation invariants parsed from the given docstring. 
+    Uses parse_token to determine what to look for. parse_token defaults to Precondition.
 
     Currently only supports two forms:
 
-    1. A single line of the form "Precondition: <pre>"
-    2. A group of lines starting with "Preconditions:", where each subsequent
-       line is of the form "- <pre>". Each line is considered a separate precondition.
+    1. A single line of the form "<parse_token>: <cond>"
+    2. A group of lines starting with "<parse_token>s:", where each subsequent
+       line is of the form "- <cond>". Each line is considered a separate condition.
        The lines can be separated by blank lines, but no other text.
     """
     lines = [line.strip() for line in docstring.split('\n')]
-    precondition_lines = [i
-                          for i, line in enumerate(lines)
-                          if line.lower().startswith('precondition')]
+    condition_lines = [i
+                       for i, line in enumerate(lines)
+                       if line.lower().startswith(parse_token.lower())]
 
-    if precondition_lines == []:
+    if condition_lines == []:
         return []
 
-    first = precondition_lines[0]
-    if lines[first].startswith('Precondition:'):
-        return [lines[first][len('Precondition:'):].strip()]
-    elif lines[first].startswith('Preconditions:'):
-        preconditions = []
+    first = condition_lines[0]
+    if lines[first].startswith(parse_token + ':'):
+        return [lines[first][len(parse_token + ':'):].strip()]
+    elif lines[first].startswith(parse_token + 's:'):
+        conditions = []
         for line in lines[first + 1:]:
             if line.startswith('-'):
-                preconditions.append(line[1:].strip())
+                conditions.append(line[1:].strip())
             elif line != '':
                 break
-        return preconditions
+        return conditions
     else:
         return []
 

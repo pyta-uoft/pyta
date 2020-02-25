7155f8f1b08b1a8e558fca55bc24860b7c6cd9d3
Ibrahim Hasan <ibrahim.hasan@mail.utoronto.ca>
2020-02-24 19:02:31 -0500

    Adds class contracts and representation invariants (#628)
    * Adds class contracts and representation invariants
    
    This sets up the basic structure for class contracts and adds tests.
    There’s still a long way to go.
    
    * Adds more functionality to class contracts
    
    Makes it such that if a user directly modifies an attribute, the representation
    invariants are checked.
    
    * Refactors it according to David's comments

--- python_ta/contracts/__init__.py
+++ python_ta/contracts/__init__.py
@@ -1,42 +1,105 @@
 from typing import Any, List, Optional
 import typing
+import inspect
 import wrapt
 
 
-class InvalidPrecondition(Exception):
-    """Exception raised when precondition is invalid
+class InvalidAssertion(Exception):
+    """Exception raised when a Assertion is invalid
     """
 
+
 @wrapt.decorator
-def check_contracts(wrapped, _instance, args, kwargs):
+def check_contracts(wrapped, instance, args, kwargs):
+    if instance is None and inspect.isclass(wrapped):
+        return add_class_contracts(wrapped, args, kwargs)
+    else:
+        return check_function_contracts(wrapped, instance, args, kwargs)
+
+
+def add_class_contracts(wrapped, args, kwargs):
+    rep_invariants = parse_assertions(
+        wrapped.__doc__ or '', parse_token="Representation Invariant")
+    setattr(wrapped, "__representation_invariants__", rep_invariants)
+
+    def new_setattr(self, name, value):
+        super_class = wrapped.__mro__[1]
+        super_class.__setattr__(self, name, value)
+        curframe = inspect.currentframe()
+        callframe = inspect.getouterframes(curframe, 2)
+        if callframe[1][3] not in wrapped.__dict__:
+            # Only validating if the attribute is not being set in a instance/class method
+            check_invariants(self)
+
+    wrapped.__setattr__ = new_setattr
+
+    return wrapped(*args, **kwargs)
+
+
+def check_function_contracts(wrapped, instance, args, kwargs):
     params = wrapped.__code__.co_varnames[:wrapped.__code__.co_argcount]
     annotations = typing.get_type_hints(wrapped)
-    for arg, param in zip(args, params):
+
+    args_with_self = (instance,) + args if instance else args
+
+    for arg, param in zip(args_with_self, params):
         if param in annotations:
             assert check_type_annotation(annotations[param], arg),\
                 f'Argument {repr(arg)} did not match type annotation for parameter "{param}: {annotations[param]}"'
 
-    preconditions = parse_preconditions(wrapped.__doc__ or '')
-    function_locals = dict(zip(params, args))
-    for precondition in preconditions:
-        try:
-            check = eval(precondition, wrapped.__globals__, function_locals)
-        except:
-            # TODO: Decide what to do here, e.g. "Invalid precondition"
-            raise InvalidPrecondition(
-                f'Error evaluating precondition: {precondition}')
-        else:
-            assert check,\
-                f'Precondition "{precondition}" violated for arguments {function_locals}'
+    preconditions = parse_assertions(wrapped.__doc__ or '')
+
+    function_locals = dict(zip(params, args_with_self))
+    check_assertions(wrapped, function_locals, preconditions)
+
     r = wrapped(*args, **kwargs)
+
+    if instance and hasattr(type(instance), "__representation_invariants__"):
+        check_invariants(instance)
+
     if 'return' in annotations:
         return_type = annotations['return']
         assert check_type_annotation(return_type, r),\
             f'Return value {r} does not match annotated return type {return_type.__name__}'
     return r
 
+
+def check_invariants(instance):
+    """
+    Checks to see if the representation invariants for the instance are satisfied.
+    """
+    for invariant in type(instance).__representation_invariants__:
+        try:
+            check = eval(invariant, {"self": instance})
+        except:
+            # TODO: Decide what to do here, e.g. "Invalid invariant"
+            raise InvalidAssertion(
+                f'Error evaluating invariant: {invariant}')
+        else:
+            assert check,\
+                f'Invariant "{invariant}" violated.'
+
+
+def check_assertions(wrapped, function_locals, assertions):
+    """
+    Checks if the assertions are still satisfied.
+    """
+    for assertion in assertions:
+        try:
+            check = eval(assertion, wrapped.__globals__, function_locals)
+        except:
+            # TODO: Decide what to do here, e.g. "Invalid assertion"
+            raise InvalidAssertion(
+                f'Error evaluating assertion: {assertion}')
+        else:
+            assert check,\
+                f'Assertion "{assertion}" violated for arguments {function_locals}'
+
+
 def check_type_annotation(annotation: Optional[type], v: Any) -> bool:
-    """Return whether v is compatible with the given type annotation."""
+    """
+    Return whether v is compatible with the given type annotation.
+    """
     if annotation is None:
         return v is None
 
@@ -55,45 +118,35 @@ def check_type_annotation(annotation: Optional[type], v: Any) -> bool:
             return True
 
 
-def parse_preconditions(docstring: str) -> List[str]:
-    """Return a list of preconditions parsed from the given docstring.
+def parse_assertions(docstring: str, parse_token: str = "Precondition") -> List[str]:
+    """Return a list of preconditions/representation invariants parsed from the given docstring. 
+    Uses parse_token to determine what to look for. parse_token defaults to Precondition.
 
     Currently only supports two forms:
 
-    1. A single line of the form "Precondition: <pre>"
-    2. A group of lines starting with "Preconditions:", where each subsequent
-       line is of the form "- <pre>". Each line is considered a separate precondition.
+    1. A single line of the form "<parse_token>: <cond>"
+    2. A group of lines starting with "<parse_token>s:", where each subsequent
+       line is of the form "- <cond>". Each line is considered a separate condition.
        The lines can be separated by blank lines, but no other text.
     """
     lines = [line.strip() for line in docstring.split('\n')]
-    precondition_lines = [i
-                          for i, line in enumerate(lines)
-                          if line.lower().startswith('precondition')]
+    assertion_lines = [i
+                       for i, line in enumerate(lines)
+                       if line.lower().startswith(parse_token.lower())]
 
-    if precondition_lines == []:
+    if assertion_lines == []:
         return []
 
-    first = precondition_lines[0]
-    if lines[first].startswith('Precondition:'):
-        return [lines[first][len('Precondition:'):].strip()]
-    elif lines[first].startswith('Preconditions:'):
-        preconditions = []
+    first = assertion_lines[0]
+    if lines[first].startswith(parse_token + ':'):
+        return [lines[first][len(parse_token + ':'):].strip()]
+    elif lines[first].startswith(parse_token + 's:'):
+        assertions = []
         for line in lines[first + 1:]:
             if line.startswith('-'):
-                preconditions.append(line[1:].strip())
+                assertions.append(line[1:].strip())
             elif line != '':
                 break
-        return preconditions
+        return assertions
     else:
         return []
-
-
-# @check_contracts
-# def _my_sum(numbers: List[int]) -> int:
-#     """Returns the sum of a list of numbers.
-#
-#     Preconditions:
-#       - len(numbers) > 2
-#       - numbers[0] == 3
-#     """
-#     return sum(numbers)
---
+++ tests/test_class_contracts.py
@@ -0,0 +1,85 @@
+import pytest
+from python_ta.contracts import check_contracts
+
+
+@check_contracts
+class Person():
+    """
+    Represent a person.
+
+    Representation Invariants: 
+    - self.age > 0
+    - len(self.name) > 0
+    """
+    
+    @check_contracts
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    @check_contracts
+    def change_name(self, name: str) -> str:
+        self.name = name
+        return name
+
+    @check_contracts
+    def change_age(self, age: int) -> int:
+        """
+        Precondition: age < 150
+        """
+        self.age = age
+        return age
+    
+    @check_contracts
+    def decrease_and_increase_age(self, age: int) -> int:
+        self.age = -10
+        self.age = age
+        return age
+
+
+p1 = Person("David", 31)
+
+
+def test_change_age_invalid_over() -> None:
+    """
+    Change the age to larger than 100. Expect an exception.
+    """
+    with pytest.raises(AssertionError) as excinfo:
+        p1.change_age(200)
+    msg = str(excinfo.value)
+    assert 'age < 150' in msg
+
+def test_change_name_valid() -> None:
+    """
+    Change the name using a valid name.
+    """
+    p1.name = "Ignas"
+    assert p1.name == "Ignas"
+
+def test_change_name_invalid() -> None:
+    """
+    Change the name using a valid name.
+    """
+    with pytest.raises(AssertionError) as excinfo:
+        p1.change_name("")
+    msg = str(excinfo.value)
+    assert 'len(self.name) > 0' in msg
+
+def test_change_age_invalid_under() -> None:
+    """
+    Change the age to a negative number. Expect an exception.
+    """
+    with pytest.raises(AssertionError) as excinfo:
+        p1.change_name("David") # Changing it back to a valid value.
+        p1.age = -10
+    msg = str(excinfo.value)
+    assert 'self.age > 0' in msg
+
+def test_change_age_invalid_in_method() -> None:
+    """
+    Call a method that changes age to something invalid but back to something valid.
+    Expects normal behavior.
+    """
+    age = p1.decrease_and_increase_age(10)
+    assert age == 10
+    

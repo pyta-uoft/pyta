67bbb5c1baa2aaff0cae78fb4c8d5a7546e43637
David Liu <david@cs.toronto.edu>
2021-01-04 20:17:08 -0500

    Update to v1.6.3.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as fh:
 
 setup(
     name='python-ta',
-    version='1.6.2',
+    version='1.6.3',
     description='Code checking tool for teaching Python',
     long_description=long_description,
     long_description_content_type='text/markdown',

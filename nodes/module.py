114e9e6500f6c43c7a5ba3af731f3e56cb9ae3fb
Lorena Buciu <lorena.buciu@mail.utoronto.ca>
2021-05-18 14:32:18 -0400

    update nodes

--- nodes/module.py
+++ nodes/module.py
@@ -1,2 +1,22 @@
-# Anything in this file is wrapped in a module node
+"""
+Module astroid Node
+
+Anything in this file is wrapped in a module node.
+
+Attributes:
+    - body  (Optional[list[Expr]])
+        - The contents of the module
+
+Example:
+    Module(
+        name='',
+        doc=None,
+        file='<?>',
+        path=['<?>'],
+        package=False,
+        pure_python=True,
+        future_imports=set(),
+        body=[Expr(value=Name(name='x'))])
+"""
+
 x
--- nodes/set.py
+++ nodes/set.py
@@ -4,19 +4,21 @@ Set astroid node
 This node represents the Python set object.
 
 Attributes:
-    - elts  (List[Expr])
+    - elts  (list[Expr])
         - The elements in this set, which can be any immutable/hashable
           type expression.
 
 Example 1:
-    - elts  -> [Const(value=0)]
+    Set(elts=[Const(value=0)])
 
 Example 2:
-    - elts  -> [Const(value=1), Const(value=2), Const(value='hi')]
-
+    Set(elts=[
+        Const(value=1),
+        Const(value=2),
+        Const(value='hi')]
 
 Type-checking:
-    Type is Set[T], where T is the most specific class that every element
+    Type is set[T], where T is the most specific class that every element
     of the list is an instance of.
 """
 

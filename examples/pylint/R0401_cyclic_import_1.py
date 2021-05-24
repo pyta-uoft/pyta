e77392190691b593f620a7b4241c52e6ff493397
Simon Chen <simonsc.chen@mail.utoronto.ca>
2021-05-24 18:44:00 -0400

    Add cyclic import examples

---
+++ examples/pylint/R0401_cyclic_import_1.py
@@ -0,0 +1,6 @@
+from R0401_cyclic_import_2 import Chicken
+
+
+class Egg:
+    """ Which came first? """
+    offspring: Chicken
---
+++ examples/pylint/R0401_cyclic_import_2.py
@@ -0,0 +1,6 @@
+from R0401_cyclic_import_1 import Egg
+
+
+class Chicken:
+    """ Which came first? """
+    eggs: list[Egg]

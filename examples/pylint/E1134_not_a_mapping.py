1fabcd27b057d55d9d49e2576d5835ae308fb4c1
Lorena Buciu <lorena.buciu@mail.utoronto.ca>
2021-05-25 17:15:00 -0400

    Example

--- examples/pylint/E1134_not_a_mapping.py
+++ examples/pylint/E1134_not_a_mapping.py
@@ -1,1 +1,14 @@
-# TODO
+# mapping = {'hi': 1, 'hello': 2} # maybe this?  # Error on this line: non-mapping value used in a mapping context
+
+def square(n):
+    return n * n
+
+
+result = map(square, 'test')
+print(result)
+
+if __name__ == '__main__':
+    import python_ta
+    python_ta.check_all(config={
+        'max-line-length': 100
+    })

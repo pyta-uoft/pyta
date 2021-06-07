f80ff65c47b9ba84de66c0d9ab1b497b2b0f8091
Craig Katsube <ckatsube@hotmail.com>
2021-06-06 21:03:21 -0400

    changed error message name

--- python_ta/checkers/invalid_for_target_checker.py
+++ python_ta/checkers/invalid_for_target_checker.py
@@ -6,32 +6,32 @@ from pylint.checkers.utils import check_messages
 from pylint.interfaces import IAstroidChecker
 
 
-class ForTargetSubscriptChecker(BaseChecker):
+class InvalidForTargetChecker(BaseChecker):
     __implements__ = IAstroidChecker
     # name is the same as file name but without _checker part
-    name = 'for_target_subscript'
+    name = 'invalid_for_target'
     # use dashes for connecting words in message symbol
     msgs = {'E9984': ('For loop variable "%s" should not be a part of a larger object.',
-                      'for-target-subscript',
+                      'invalid-for-target',
                       'Used when you have a loop variable in a for loop '
-                      'that is in subscript form'),
+                      'that is in subscript or object attribute form'),
             }
     # this is important so that your checker is executed before others
     priority = -1
     # pass in message symbol as a parameter of check_messages
 
-    @check_messages('for-target-subscript')
+    @check_messages('invalid-for-target')
     def visit_for(self, node: astroid.For) -> None:
         if isinstance(node.target, astroid.Subscript):
-            self.add_message('for-target-subscript',
+            self.add_message('invalid-for-target',
                              node=node.target, args=node.target.as_string())
         # if there are multiple targets
         elif isinstance(node.target, astroid.Tuple):
             for target in node.target.elts:
                 if isinstance(target, astroid.Subscript):
-                    self.add_message('for-target-subscript',
+                    self.add_message('invalid-for-target',
                                      node=target, args=target.as_string())
 
 
 def register(linter):
-    linter.register_checker(ForTargetSubscriptChecker(linter))
+    linter.register_checker(InvalidForTargetChecker(linter))

81289658d5989ebc2a69b0964eebc9e51757450a
David Liu <david@cs.toronto.edu>
2021-07-15 18:06:06 -0400

    reporters: Refactor PyTA custom reporters.
    This makes the following changes to simplify our custom reporters and
    bring them more in line with Pylint's reporter structure. Major changes
    are:
    
    1. Unify reporters with abstract PythonTaReporter class.
    2. Use "out" instance attribute and event hooks from pylint's BaseReporter.
    3. Simplify storing and grouping of messages.
    4. Add code snippets when each message is registered
       (handle_message/handle_node).
    5. Convert PositionReporter into a proper JSONReporter.
    6. Add documentation for reporter types and configuration. (Fixes #669.)

--- python_ta/reporters/__init__.py
+++ python_ta/reporters/__init__.py
@@ -1,8 +1,7 @@
 from .color_reporter import ColorReporter
 from .html_reporter import HTMLReporter
 from .plain_reporter import PlainReporter
-from .stat_reporter import StatReporter
-from .position_reporter import PositionReporter
+from .json_reporter import JSONReporter
 
 # Export tuple of reporter classes for python_ta init file.
-REPORTERS = (ColorReporter, PlainReporter, HTMLReporter, StatReporter, PositionReporter)
+REPORTERS = (ColorReporter, PlainReporter, HTMLReporter, JSONReporter)
--- python_ta/reporters/color_reporter.py
+++ python_ta/reporters/color_reporter.py
@@ -1,53 +1,57 @@
 import sys
-from colorama import Fore, Style, Back, init
+
+from colorama import Fore, Style, Back, colorama_text
+from pylint.interfaces import IReporter
 from .plain_reporter import PlainReporter
 
 
 class ColorReporter(PlainReporter):
-    _SPACE = ' '
-    _BREAK = '\n'
-    _COLOURING = {'black': Fore.BLACK,  # or could be empty
-                  'black-line': Fore.BLACK,
-                  'bold': Style.BRIGHT,
-                  'code-heading': Fore.RED + Style.BRIGHT,
-                  'style-heading': Fore.BLUE + Style.BRIGHT,
-                  'code-name': Fore.RED + Style.BRIGHT,
-                  'style-name': Fore.BLUE + Style.BRIGHT,
-                  'highlight': Style.BRIGHT + Fore.BLACK + Back.CYAN,
-                  'grey': Fore.LIGHTBLACK_EX,
-                  'grey-line': Fore.LIGHTBLACK_EX,
-                  'gbold': Style.BRIGHT + Fore.LIGHTBLACK_EX,
-                  'gbold-line': Style.BRIGHT + Fore.LIGHTBLACK_EX,
-                  'reset': Style.RESET_ALL}
+    """Colorized text reporter. Should only be used to print to stdout.
+    """
+    __implements__ = IReporter
+    name = 'ColorReporter'
 
-    def __init__(self, source_lines=None, module_name=''):
-        super().__init__(source_lines, module_name)
+    _COLOURING = {
+        'black': Fore.BLACK,
+        'black-line': Fore.BLACK,
+        'bold': Style.BRIGHT,
+        'code-heading': Fore.RED + Style.BRIGHT,
+        'style-heading': Fore.BLUE + Style.BRIGHT,
+        'code-name': Fore.RED + Style.BRIGHT,
+        'style-name': Fore.BLUE + Style.BRIGHT,
+        'highlight': Style.BRIGHT + Fore.BLACK + Back.CYAN,
+        'grey': Fore.LIGHTBLACK_EX,
+        'grey-line': Fore.LIGHTBLACK_EX,
+        'gbold': Style.BRIGHT + Fore.LIGHTBLACK_EX,
+        'gbold-line': Style.BRIGHT + Fore.LIGHTBLACK_EX,
+        'reset': Style.RESET_ALL
+    }
 
-    def print_messages(self, level='all'):
+    def print_messages(self, level: str = 'all') -> None:
+        """Print messages for the current file.
+
+        If level == 'all', both errors and style errors are displayed. Otherwise,
+        only errors are displayed.
+        """
         # Check if the OS currently running is Windows
-        init(wrap=(sys.platform == 'win32'), strip=False)
-        super().print_messages(level)
+        with colorama_text(wrap=(sys.platform == 'win32'), strip=False):
+            super().print_messages(level)
 
     @classmethod
-    def _colourify(cls, colour_class, text):
+    def _colourify(cls, colour_class: str, text: str) -> str:
         """
         Adds given ANSI colouring tokens (or key to colouring tokens in the
         class-level dict "_COLOURING") to text as well as final colour reset.
 
         Does not colour indents, except non-space indents.
         Called by _colour_messages_by_type and _add_line.
-        Now applicable both to ColorReporter and HTMLReporter.
-
-        :param str colour_class: key to colour class or ANSI colour token(s)
-        :param str text: text to be coloured
-        :return str
         """
         colour = cls._COLOURING[colour_class]
         new_text = text.lstrip(' ')
         space_count = len(text) - len(new_text)
-        if cls._SPACE != ' ':
-            new_text = new_text.replace(' ', cls._SPACE)
-        return ((space_count * cls._SPACE) + colour + cls._vendor_wrap(colour_class, new_text) +
-                cls._COLOURING['reset'])
-
-    _display = None   # because PyCharm is annoying about this
+        new_text = new_text.replace(' ', cls._SPACE)
+        return (
+            (space_count * cls._SPACE) + colour +
+            new_text +
+            cls._COLOURING['reset']
+        )

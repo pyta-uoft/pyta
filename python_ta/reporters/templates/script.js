aa8a249804350dc195262102a5168b27ff4804d2
Simon Chen <simonsc.chen@mail.utoronto.ca>
2021-07-25 17:53:37 -0400

    Add button WIP

--- python_ta/reporters/templates/script.js
+++ python_ta/reporters/templates/script.js
@@ -1,3 +1,9 @@
 $("body").on("click", ".slider", function () {
     $(this).parent().next().toggleClass("hide-and-maintain-width");
+    $(this).children().children().toggleClass("collapsed");
 });
+
+//$(".collapsible").on("click", ".collapse-trigger", function (evt) {
+//    $(this).closest(".collapsible").children("ul").toggle()
+//    $(this).toggleClass("collapsed")
+//});

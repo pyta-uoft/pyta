08edc43341d45e2f64c2bbf1b5001711de5b096e
Simon Chen <simonsc.chen@mail.utoronto.ca>
2021-07-28 08:19:26 -0400

    Remove temp code

--- python_ta/reporters/templates/script.js
+++ python_ta/reporters/templates/script.js
@@ -2,8 +2,3 @@ $("body").on("click", ".slider", function () {
     $(this).parent().next().toggleClass("hide-and-maintain-width");
     $(this).children().toggleClass("collapsed");
 });
-
-//$(".collapsible").on("click", ".collapse-trigger", function (evt) {
-//    $(this).closest(".collapsible").children("ul").toggle()
-//    $(this).toggleClass("collapsed")
-//});

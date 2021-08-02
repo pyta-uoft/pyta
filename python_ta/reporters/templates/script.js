ddc851f2c0a5e5b09ac5d96994bd4ac5cfac45d7
Craig Katsube <craig.katsube@mail.utoronto.ca>
2021-08-02 14:37:59 -0400

    added collapse/expand buttons

--- python_ta/reporters/templates/script.js
+++ python_ta/reporters/templates/script.js
@@ -1,3 +1,18 @@
 $("body").on("click", ".slider", function () {
     $(this).parent().next().toggleClass("hide-and-maintain-width");
 });
+
+function toggleClosestCollapsible() {  
+    $(this).closest(".collapsible").children("ul").toggle();
+    $(this).children("svg").toggleClass("collapsed");
+};
+
+$(".sidebar button").click(toggleClosestCollapsible);
+$(".sidebar button").keydown(function(event) {
+    const spacePressed = event.key === " ";
+    const enterPressed = event.key === "Enter";
+    if (spacePressed || enterPressed) {
+        event.preventDefault();
+        $(this).trigger("click");
+    }
+})

e13a11e88b08683c392f7620fe2a5b03ab3dd47a
Craig Katsube <craig.katsube@mail.utoronto.ca>
2021-08-03 06:08:47 -0400

    removed redundant event and cleaned up style

--- python_ta/reporters/templates/script.js
+++ python_ta/reporters/templates/script.js
@@ -2,17 +2,7 @@ $("body").on("click", ".slider", function () {
     $(this).parent().next().toggleClass("hide-and-maintain-width");
 });
 
-function toggleClosestCollapsible() {  
+$(".sidebar button").click(function () {  
     $(this).closest(".collapsible").children("ul").toggle();
     $(this).children("svg").toggleClass("collapsed");
-};
-
-$(".sidebar button").click(toggleClosestCollapsible);
-$(".sidebar button").keydown(function(event) {
-    const spacePressed = event.key === " ";
-    const enterPressed = event.key === "Enter";
-    if (spacePressed || enterPressed) {
-        event.preventDefault();
-        $(this).trigger("click");
-    }
-})
+});

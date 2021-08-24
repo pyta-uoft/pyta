05938a2630523004e62b671afd896733f022d015
David Liu <david@cs.toronto.edu>
2021-08-24 14:23:43 -0400

    main: Report non-zero status code on errors. (#756)
    Also introduce --exit-zero option (inspired by pylint) to force the
    program to always report status code 0 instead.

--- python_ta/__main__.py
+++ python_ta/__main__.py
@@ -1,3 +1,6 @@
+import sys
+from typing import List, Optional
+
 import click
 
 from python_ta import check_all, check_errors
@@ -16,7 +19,8 @@ CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 @click.argument(
     "filenames", nargs=-1, type=click.Path(exists=True, dir_okay=True, resolve_path=True)
 )
-def main(config, errors_only, filenames):
+@click.option("--exit-zero", is_flag=True, help="Always return with status code 0", default=False)
+def main(config: Optional[str], errors_only: bool, filenames: List[str], exit_zero: bool) -> None:
     """A code checking tool for teaching Python.
 
     FILENAMES can be a string of a directory, or file to check (`.py` extension optional) or
@@ -25,7 +29,12 @@ def main(config, errors_only, filenames):
     # `config` is None if `-c` flag is not set
     checker = check_errors if errors_only else check_all
     paths = [click.format_filename(fn) for fn in filenames]
-    checker(module_name=paths, config=config)
+    reporter = checker(module_name=paths, config=config)
+
+    if not exit_zero and reporter.has_messages():
+        sys.exit(1)
+    else:
+        sys.exit(0)
 
 
 if __name__ == "__main__":

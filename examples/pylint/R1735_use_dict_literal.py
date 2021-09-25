1cfac0a4b8601cdad0f7ec72033abb2e4e7635e9
Younwoo (Ethan) Choi <younwoo.choi@mail.utoronto.ca>
2021-09-25 00:50:27 -0400

    Update R1735_use_dict_literal.py
    created a new example.

--- examples/pylint/R1735_use_dict_literal.py
+++ examples/pylint/R1735_use_dict_literal.py
@@ -1,3 +1,7 @@
-student_dict = dict() # Error on this line.
+students_info = [[1002123, "Alex H", "CS"], [1001115, "Jack K", "PSY"]]
 
-student_dict["123456789"] = "John Doe"
+cs_student_dict = dict()  # Error on this line.
+
+for student in students_info:
+    if student[2] == "CS":
+        cs_student_dict[student[0]] = student[1]

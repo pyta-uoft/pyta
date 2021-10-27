83c8297864ad7e48118ba779144fbbe94bc9cfdc
Craig Katsube <craig.katsube@mail.utoronto.ca>
2021-10-27 13:46:08 -0400

    refactored pseudo module to encapsulate more of the entire process
    also changed from double exec layer to single

--- python_ta/contracts/__main__.py
+++ python_ta/contracts/__main__.py
@@ -2,7 +2,7 @@ import os.path
 import sys
 import traceback
 import types
-from typing import Any, Dict, TextIO, Tuple
+from typing import List, TextIO, Tuple
 
 import astroid
 import click
@@ -27,25 +27,7 @@ def check_contracts(main: TextIO, extra_mod_names: Tuple, no_main: bool):
     if_main = _extract_if_main(module_node)
 
     if if_main:
-        split_point = if_main.lineno - 1
-        before_main = "\n".join(lines[:split_point])
-        after_main = "\n" * split_point + "\n".join(lines[split_point:])
-
-        line1 = f"exec(__before_main)\n"
-        line2 = "import python_ta.contracts\n"
-        line3 = "python_ta.contracts.check_all_contracts(*__extra_mods, decorate_main=__no_main)\n"
-        line4 = f"exec(__after_main)\n"
-        exec_content = line1 + line2 + line3 + line4
-
-        extras = {
-            "__before_main": before_main,
-            "__after_main": after_main,
-            "__no_main": no_main,
-            "__extra_mods": extra_mod_names,
-            "__builtins__": globals()["__builtins__"],
-            "__name__": "__main__",
-        }
-        duck_main = DuckModule("__main__", extras, exec_content)
+        duck_main = ContractsRunnerModule("__main__", if_main, lines, extra_mod_names, no_main)
 
         true_main = sys.modules["__main__"]
         sys.modules["__main__"] = duck_main
@@ -53,26 +35,43 @@ def check_contracts(main: TextIO, extra_mod_names: Tuple, no_main: bool):
             duck_main.run()
         except Exception:
             exception_message = traceback.format_exc()
-
-            formatted_exception = exception_message.replace("<string>", "run context", 1)
             main_path = os.path.abspath(main.name)
-            formatted_exception = formatted_exception.replace("<string>", main_path)
+            formatted_exception = exception_message.replace("<string>", main_path)
             sys.stderr.write(formatted_exception)
 
         sys.modules["__main__"] = true_main
 
 
-class DuckModule(types.ModuleType):
-    """A pseudo module used for syncing execution and module contexts"""
+class ContractsRunnerModule(types.ModuleType):
+    """A pseudo module used for splitting up execution contexts to insert contract checking
+    before the if __name__ == "__main__" entry point
+    """
 
-    def __init__(self, name: str, default_globals: Dict[str, Any], content: str) -> None:
+    def __init__(
+        self,
+        name: str,
+        if_main: astroid.If,
+        lines: List[str],
+        extra_mod_names: Tuple,
+        no_main: bool,
+    ) -> None:
         super().__init__(name)
-        self.content = content
-        for key, value in default_globals.items():
-            setattr(self, key, value)
+
+        split_point = if_main.lineno - 1
+        self.before_main = "\n".join(lines[:split_point])
+        self.after_main = "\n" * split_point + "\n".join(lines[split_point:])
+        self.extra_mod_names = extra_mod_names
+        self.no_main = no_main
+
+        # not required but prevents __dict__ from being auto populated with all default builtins
+        self.__builtins__ = globals()["__builtins__"]
 
     def run(self) -> None:
-        exec(self.content, self.__dict__)
+        exec(self.before_main, self.__dict__)
+        import python_ta.contracts
+
+        python_ta.contracts.check_all_contracts(*self.extra_mod_names, decorate_main=self.no_main)
+        exec(self.after_main, self.__dict__)
 
 
 def _extract_if_main(module_node: astroid.Module) -> astroid.If:

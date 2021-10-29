7aeb11e76e0e0be6ac81acbe6a4bef7b8eb5a0be
Craig Katsube <craig.katsube@mail.utoronto.ca>
2021-10-29 14:06:09 -0400

    added appropriate exit codes, refactored out astroid, added documentation

--- python_ta/contracts/__main__.py
+++ python_ta/contracts/__main__.py
@@ -4,40 +4,55 @@ import traceback
 import types
 from typing import List, TextIO, Tuple
 
-import astroid
 import click
+
+from . import check_all_contracts
 
 
 @click.command()
-@click.argument("main", type=click.File(mode="r"))
+@click.argument("file", type=click.File(mode="r"))
 @click.argument("extra-mod-names", nargs=-1)
-@click.option("--no-main", "-n", is_flag=True, default=True)
-def check_contracts(main: TextIO, extra_mod_names: Tuple, no_main: bool):
-    """Run python_ta.contracts.check_all_contracts() using the provided arguments
+@click.option("--no-decorate-main", is_flag=True, default=True, help="Disable decorating FILE")
+def check_contracts(file: TextIO, extra_mod_names: Tuple, no_decorate_main: bool):
+    """Run FILE as Python script with PythonTA's contract checking enabled.
 
-    MAIN the Python script as if you were to just run `python MAIN`
+    FILE the Python script as if you were to just run `python FILE`
     EXTRA_MOD_NAMES are the module names to also have contracts checked.
         These are directly passed in, so the format is the module names as if you were importing
-    NO_MAIN is whether to decorate main or not. No flag defaults to True.
     """
-    contents = main.read()
-    module_node = astroid.parse(contents)
+    contents = file.read()
     lines = contents.splitlines()
 
-    if_main = _extract_if_main(module_node)
+    main_lineno = _find_main_lineno(lines)
 
-    if if_main:
-        duck_main = ContractsRunnerModule("__main__", if_main, lines, extra_mod_names, no_main)
+    def _formatted_traceback() -> str:
+        """Gets current traceback as string while removing run and exec frames and replacing
+        the default exec context name "<string>" with the ran file name."""
+        exception_traceback = sys.exc_info()[2]
+
+        stack_size = len(traceback.extract_tb(exception_traceback))
+        # ignores first two frames of traceback (module runner and exec frames)
+        exception_message = traceback.format_exc(limit=-(stack_size - 2))
+
+        main_path = os.path.abspath(file.name)
+        formatted_exception = exception_message.replace("<string>", main_path)
+        return formatted_exception
+
+    if main_lineno:
+        duck_main = ContractsRunnerModule(
+            "__main__", main_lineno, lines, extra_mod_names, no_decorate_main
+        )
 
         true_main = sys.modules["__main__"]
         sys.modules["__main__"] = duck_main
         try:
             duck_main.run()
+        except SystemExit as se:
+            sys.stderr.write(_formatted_traceback())
+            sys.exit(se.args[0])
         except Exception:
-            exception_message = traceback.format_exc()
-            main_path = os.path.abspath(main.name)
-            formatted_exception = exception_message.replace("<string>", main_path)
-            sys.stderr.write(formatted_exception)
+            sys.stderr.write(_formatted_traceback())
+            sys.exit(1)
 
         sys.modules["__main__"] = true_main
 
@@ -50,14 +65,14 @@ class ContractsRunnerModule(types.ModuleType):
     def __init__(
         self,
         name: str,
-        if_main: astroid.If,
+        main_lineno: int,
         lines: List[str],
         extra_mod_names: Tuple,
         no_main: bool,
     ) -> None:
         super().__init__(name)
 
-        split_point = if_main.lineno - 1
+        split_point = main_lineno - 1
         self.before_main = "\n".join(lines[:split_point])
         self.after_main = "\n" * split_point + "\n".join(lines[split_point:])
         self.extra_mod_names = extra_mod_names
@@ -68,16 +83,25 @@ class ContractsRunnerModule(types.ModuleType):
 
     def run(self) -> None:
         exec(self.before_main, self.__dict__)
-        import python_ta.contracts
-
-        python_ta.contracts.check_all_contracts(*self.extra_mod_names, decorate_main=self.no_main)
+        check_all_contracts(*self.extra_mod_names, decorate_main=self.no_main)
         exec(self.after_main, self.__dict__)
 
 
-def _extract_if_main(module_node: astroid.Module) -> astroid.If:
-    if_nodes = module_node.nodes_of_class(astroid.If)
-    return next(
-        (node for node in if_nodes if node.test.as_string() == "__name__ == '__main__'"), None
+def _find_main_lineno(lines: list[str]) -> int:
+    for lineno, line in enumerate(lines, start=1):
+        if _has_main_check(line):
+            return lineno
+    return 0
+
+
+def _has_main_check(line: str):
+    if line.strip() == "":
+        return False
+    keyword, *condition = line.split()
+    spaceless_condition = "".join(condition)
+    return keyword == "if" and (
+        spaceless_condition == "__name__=='__main__':"
+        or spaceless_condition == '__name__=="__main__":'
     )
 
 

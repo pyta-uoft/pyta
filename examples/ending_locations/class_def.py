9d8b0212c08edc71612a812337218ca9c8397018
David Liu <david@cs.toronto.edu>
2021-12-09 15:09:01 -0500

    ending-locations: Fix bug for ClassDefs with no decorators

--- examples/ending_locations/class_def.py
+++ examples/ending_locations/class_def.py
@@ -1,3 +1,8 @@
 @wrapper
 class Foo(base1, base2):
     pass
+
+
+class Bar:
+    def __init__(self):
+        pass
--- examples/ending_locations/function_def.py
+++ examples/ending_locations/function_def.py
@@ -5,3 +5,11 @@ def fun(arg) -> str:
     """
     return_annotation = "cool!"
     return return_annotation
+
+
+def fun2(arg) -> str:
+    """
+    This is a function fun2.
+    """
+    return_annotation = "cool!"
+    return return_annotation

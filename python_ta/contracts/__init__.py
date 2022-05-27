11a21374dd870fb156475a9377b1def8f98d4a75
harshkpatel <64931070+harshkpatel@users.noreply.github.com>
2022-05-27 13:51:23 -0600

    contracts: Fix type error in check_all_contracts (#820)

--- python_ta/contracts/__init__.py
+++ python_ta/contracts/__init__.py
@@ -45,7 +45,7 @@ def check_all_contracts(*mod_names: str, decorate_main: bool = True) -> None:
 
     modules = []
     if decorate_main:
-        mod_names.append("__main__")
+        mod_names = mod_names + tuple("__main__")
 
     for module_name in mod_names:
         modules.append(sys.modules.get(module_name, None))
@@ -56,7 +56,7 @@ def check_all_contracts(*mod_names: str, decorate_main: bool = True) -> None:
             continue
         for name, value in inspect.getmembers(module):
             if inspect.isfunction(value) or inspect.isclass(value):
-                module.__dict__[name] = check_contracts(value, module_names=mod_names)
+                module.__dict__[name] = check_contracts(value, module_names=set(mod_names))
 
 
 @wrapt.decorator

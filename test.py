b07ac1891e12612386d998679f35e7e91e4629d4
peelstnac <chfrn2002@gmail.com>
2022-05-29 22:12:21 -0400

    remove test.py file

--- test.py
+++
@@ -1,9 +0,0 @@
-import python_ta
-
-python_ta.check_all("test.py")
-
-
-def add_two_numbers(x=int, y=int) -> int:
-    # type is assigned instead of annotated here,
-    # should be def add_two_numbers(x: int, y: int) -> int
-    return x + y

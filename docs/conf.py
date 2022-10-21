e259cad8a022320657219e67e03cee29e9b9cb11
Yibing (Amy) Lu <58180122+yibinglu@users.noreply.github.com>
2022-10-21 12:44:19 -0400

    docs: fixed warning of missing _static/ folder when running `make html` (#855)

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = []
 
 html_logo = "images/pyta_logo.svg"
 html_favicon = "images/favicon.png"

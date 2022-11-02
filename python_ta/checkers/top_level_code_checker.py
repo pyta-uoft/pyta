f98610ebd0d4a7f94bc9f8c327ce95d9b1b0a80d
Sophy Sun <sophyzy.sun@mail.utoronto.ca>
2022-11-02 09:38:43 -0400

    Fix implementation

--- python_ta/checkers/top_level_code_checker.py
+++ python_ta/checkers/top_level_code_checker.py
@@ -4,8 +4,6 @@ from astroid import nodes
 from pylint.checkers import BaseChecker
 from pylint.checkers.utils import only_required_for_messages
 
-from python_ta.utils import _is_in_main
-
 
 class TopLevelCodeChecker(BaseChecker):
     name = "Top_Level_Code"
@@ -23,37 +21,45 @@ class TopLevelCodeChecker(BaseChecker):
 
     @only_required_for_messages("forbidden-top-level-code")
     def visit_module(self, node):
-        if (
-            not _is_import(node)
-            and not _is_definition(node)
-            and not _is_constant_assignment(node)
-            and not _is_in_main(node)
-        ):
-            body = node.body
-            self.add_message("forbidden-top-level-code", node=node, args=body)
+        for statement in node.body:
+            if (
+                not _is_import(statement)
+                and not _is_definition(statement)
+                and not _is_constant_assignment(statement)
+                and not _is_main(statement)
+            ):
+                body = node.body
+                self.add_message("forbidden-top-level-code", node=node, args=body)
 
 
 # Helper functions
-def _is_import(node):
-    for statement in node.body:
-        if isinstance(statement, nodes.Import) or isinstance(statement, nodes.ImportFrom):
-            return True
+def _is_import(statement):
+    if isinstance(statement, nodes.Import) or isinstance(statement, nodes.ImportFrom):
+        return True
     return False
 
 
-def _is_definition(node):
-    for statement in node.body:
-        if isinstance(statement, nodes.FunctionDef) or isinstance(statement, nodes.ClassDef):
-            return True
+def _is_definition(statement):
+    if isinstance(statement, nodes.FunctionDef) or isinstance(statement, nodes.ClassDef):
+        return True
     return False
 
 
-def _is_constant_assignment(node):
-    for statement in node.body:
-        if isinstance(statement, nodes.Assign) and re.search(
-            "^[A-Z]+(?:_[A-Z]+)*$", statement.targets[0].name
-        ):
-            return True
+def _is_constant_assignment(statement):
+    if isinstance(statement, nodes.Assign) and re.search(
+        "^[A-Z]+(?:_[A-Z]+)*$", statement.targets[0].name
+    ):
+        return True
+    return False
+
+
+def _is_main(statement):
+    if (
+        isinstance(statement, nodes.If)
+        and statement.test.left.name == "__name__"
+        and statement.test.ops[0][1].value == "__main__"
+    ):
+        return True
     return False
 
 

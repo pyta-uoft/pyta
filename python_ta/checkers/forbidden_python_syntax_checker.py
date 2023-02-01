e4055e1582cb7f38ab791051e304e5769e56c739
sushimon <richard.540102@gmail.com>
2023-02-01 00:25:45 -0500

    Created ForbiddenPythonSyntaxChecker and its associated test suite

---
+++ python_ta/checkers/forbidden_python_syntax_checker.py
@@ -0,0 +1,96 @@
+"""A checker for reporting on the disallowed use of various Python elements.
+"""
+from astroid import nodes
+from pylint.checkers import BaseChecker
+from pylint.checkers.utils import only_required_for_messages
+from pylint.lint import PyLinter
+
+
+class ForbiddenPythonSyntaxChecker(BaseChecker):
+    """A checker class to report on the disallowed use of various Python elements.
+
+    Includes checking the use of:
+      - break
+      - continue
+      - comprehension
+      - for
+      - while
+    """
+
+    name = "forbidden_python_syntax"
+    msgs = {
+        "E9950": (
+            "You may not use break statement(s).",
+            "forbidden-break-usage",
+            "Used when a break statement is found in your code.",
+        ),
+        "E9951": (
+            "You may not use continue statement(s).",
+            "forbidden-continue-usage",
+            "Used when a continue statement is found in your code.",
+        ),
+        "E9952": (
+            "You may not use any comprehensions.",
+            "forbidden-comprehension-usage",
+            "Used when a comprehension is found in your code.",
+        ),
+        "E9953": (
+            "You may not use any for loops.",
+            "forbidden-for-loop-usage",
+            "Used when a for loop is found in your code.",
+        ),
+        "E9954": (
+            "You may not use any while loops.",
+            "forbidden-while-loop-usage",
+            "Used when a while loop is found in your code.",
+        ),
+    }
+    options = (
+        (
+            "allowed-python-elements",
+            {
+                "default": (),
+                "type": "csv",
+                "metavar": "<allowed-elements>",
+                "help": "List of Python elements that are allowed to be used.",
+            },
+        ),
+    )
+
+    # this is important so that your checker is executed before others
+    priority = -1
+
+    @only_required_for_messages("forbidden-break-usage")
+    def visit_break(self, node: nodes.Break) -> None:
+        """Visit a Break node."""
+        if "break" not in self.linter.config.allowed_python_elements:
+            self.add_message("forbidden-break-usage", node=node)
+
+    @only_required_for_messages("forbidden-continue-usage")
+    def visit_continue(self, node: nodes.Continue) -> None:
+        """Visit a Continue node."""
+        if "continue" not in self.linter.config.allowed_python_elements:
+            self.add_message("forbidden-continue-usage", node=node)
+
+    @only_required_for_messages("forbidden-comprehension-usage")
+    def visit_comprehension(self, node: nodes.Comprehension) -> None:
+        """Visit a Comprehension node."""
+        if "comprehension" not in self.linter.config.allowed_python_elements:
+            self.add_message("forbidden-comprehension-usage", node=node)
+
+    @only_required_for_messages("forbidden-for-loop-usage")
+    def visit_for(self, node: nodes.For) -> None:
+        """Visit a For node."""
+        if "for" not in self.linter.config.allowed_python_elements:
+            self.add_message("forbidden-for-loop-usage", node=node)
+
+    @only_required_for_messages("forbidden-while-loop-usage")
+    def visit_while(self, node: nodes.While) -> None:
+        """Visit a While node."""
+        if "for" not in self.linter.config.allowed_python_elements:
+            self.add_message("forbidden-while-loop-usage", node=node)
+
+
+def register(linter: PyLinter) -> None:
+    """Register this checker to the linter."""
+    linter.register_checker(ForbiddenPythonSyntaxChecker(linter))
---
+++ tests/test_custom_checkers/test_forbidden_python_syntax_checker.py
@@ -0,0 +1,183 @@
+"""Test suite for testing the ForbiddenPythonSyntaxChecker."""
+import astroid
+import pylint.testutils
+from astroid import nodes
+
+from python_ta.checkers.forbidden_python_syntax_checker import (
+    ForbiddenPythonSyntaxChecker,
+)
+
+
+class TestForbiddenPythonSyntaxCheckerDisallowedElements(pylint.testutils.CheckerTestCase):
+    CHECKER_CLASS = ForbiddenPythonSyntaxChecker
+    CONFIG = {}
+
+    def set_up(self) -> None:
+        """Perform the set up before each test case executes."""
+        self.setup_method()
+
+    def test_disallow_break_in_code(self) -> None:
+        """Test that the checker correctly reports a break statement in the code when its usage is
+        disallowed.
+        """
+        src = """
+        for i in range(0, 10):
+            break
+        """
+        mod = astroid.parse(src)
+        break_node, *_ = mod.nodes_of_class(nodes.Break)
+
+        with self.assertAddsMessages(
+            pylint.testutils.MessageTest(msg_id="forbidden-break-usage", node=break_node),
+            ignore_position=True,
+        ):
+            self.checker.visit_break(break_node)
+
+    def test_disallow_continue_in_code(self) -> None:
+        """Test that the checker correctly reports a continue statement in the code when its usage
+        is disallowed.
+        """
+        src = """
+        for i in range(0, 10):
+            continue
+        """
+        mod = astroid.parse(src)
+        continue_node, *_ = mod.nodes_of_class(nodes.Continue)
+
+        with self.assertAddsMessages(
+            pylint.testutils.MessageTest(msg_id="forbidden-continue-usage", node=continue_node),
+            ignore_position=True,
+        ):
+            self.checker.visit_continue(continue_node)
+
+    def test_disallow_comprehension_in_code(self) -> None:
+        """Test that the checker correctly reports a comprehension in the code when its usage is
+        disallowed.
+        """
+        src = """
+        comp = [i ** 2 for i in range(1, 11)]
+        """
+        mod = astroid.parse(src)
+        comprehension_node, *_ = mod.nodes_of_class(nodes.Comprehension)
+
+        with self.assertAddsMessages(
+            pylint.testutils.MessageTest(
+                msg_id="forbidden-comprehension-usage", node=comprehension_node
+            ),
+            ignore_position=True,
+        ):
+            self.checker.visit_comprehension(comprehension_node)
+
+    def test_disallow_for_loop_in_code(self) -> None:
+        """Test that the checker correctly reports a for loop in the code when its usage is
+        disallowed.
+        """
+        src = """
+        for i in range(0, 10):
+            print(i)
+        """
+        mod = astroid.parse(src)
+        for_node, *_ = mod.nodes_of_class(nodes.For)
+
+        with self.assertAddsMessages(
+            pylint.testutils.MessageTest(msg_id="forbidden-for-loop-usage", node=for_node),
+            ignore_position=True,
+        ):
+            self.checker.visit_for(for_node)
+
+    def test_disallow_while_loop_in_code(self) -> None:
+        """Test that the checker correctly reports a while loop in the code when its usage is
+        disallowed.
+        """
+        src = """
+        count = 10
+        while count > -1:
+            count -= 1
+        """
+        mod = astroid.parse(src)
+        while_node, *_ = mod.nodes_of_class(nodes.While)
+
+        with self.assertAddsMessages(
+            pylint.testutils.MessageTest(msg_id="forbidden-while-loop-usage", node=while_node),
+            ignore_position=True,
+        ):
+            self.checker.visit_while(while_node)
+
+
+class TestForbiddenPythonSyntaxCheckerAllowedElements(pylint.testutils.CheckerTestCase):
+    CHECKER_CLASS = ForbiddenPythonSyntaxChecker
+    CONFIG = {"allowed_python_elements": ["break", "continue", "comprehension", "for", "while"]}
+
+    def set_up(self) -> None:
+        """Perform the set up before each test case executes."""
+        self.setup_method()
+
+    def test_allow_break_in_code(self) -> None:
+        """Test that the checker correctly doesn't report a break statement when its usage is
+        allowed.
+        """
+        src = """
+        for i in range(0, 10):
+            break
+        """
+        mod = astroid.parse(src)
+        break_node, *_ = mod.nodes_of_class(nodes.Break)
+
+        with self.assertNoMessages():
+            self.checker.visit_break(break_node)
+
+    def test_allow_continue_in_code(self) -> None:
+        """Test that the checker correctly doesn't report a continue statement when its usage is
+        allowed.
+        """
+        src = """
+        for i in range(0, 10):
+            continue
+        """
+        mod = astroid.parse(src)
+        continue_node, *_ = mod.nodes_of_class(nodes.Continue)
+
+        with self.assertNoMessages():
+            self.checker.visit_continue(continue_node)
+
+    def test_allow_comprehension_in_code(self) -> None:
+        """Test that the checker correctly doesn't report a comprehension when its usage is allowed."""
+        src = """
+        comp = [i ** 2 for i in range(1, 11)]
+        """
+        mod = astroid.parse(src)
+        comprehension_node, *_ = mod.nodes_of_class(nodes.Comprehension)
+
+        with self.assertNoMessages():
+            self.checker.visit_comprehension(comprehension_node)
+
+    def test_allow_for_in_code(self) -> None:
+        """Test that the checker correctly doesn't report a for loop when its usage is allowed."""
+        src = """
+        for i in range(0, 10):
+            print(i)
+        """
+        mod = astroid.parse(src)
+        for_node, *_ = mod.nodes_of_class(nodes.For)
+
+        with self.assertNoMessages():
+            self.checker.visit_for(for_node)
+
+    def test_allow_while_in_code(self) -> None:
+        """Test that the checker correctly doesn't report a while loop when its usage is allowed."""
+        src = """
+        count = 10
+        while count > -1:
+            count -= 1
+        """
+        mod = astroid.parse(src)
+        while_node, *_ = mod.nodes_of_class(nodes.While)
+
+        with self.assertNoMessages():
+            self.checker.visit_while(while_node)
+
+
+if __name__ == "__main__":
+    import pytest
+
+    pytest.main(["test_forbidden_python_syntax_checker.py"])

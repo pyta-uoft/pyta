e32d0063c61af05fbd3d1342a7a7b9ff49f9e467
sushimon <richard.540102@gmail.com>
2023-05-17 15:56:22 -0400

    Updated variable name and comments on errors

--- examples/pylint/w3601_bad_chained_comparison.py
+++ examples/pylint/w3601_bad_chained_comparison.py
@@ -2,14 +2,14 @@
 
 x = 5
 
-if 0 < x is 5:  # bad-chained-comparison
+if 0 < x is 5:  # Error on this line
     print("hi")
 
 a = 1
-lis = [1, 2, 3, 4]
+my_list = [1, 2, 3, 4]
 
-if a > 0 not in lis:  # bad-chained-comparison
+if a > 0 not in my_list:  # Error on this line
     print("whee")
 
-if a == x is 1 in lis:  # bad-chained-comparison
+if a == x is 1 in my_list:  # Error on this line
     print("yikes")

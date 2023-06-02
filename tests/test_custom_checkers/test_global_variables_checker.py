43c1c213f16e8ebbe388ac3a8575384f2583aa01
Bruce Liu <liuxb904@gmail.com>
2023-06-02 13:55:36 -0400

    Added test case for import types

--- tests/test_custom_checkers/test_global_variables_checker.py
+++ tests/test_custom_checkers/test_global_variables_checker.py
@@ -7,7 +7,7 @@ from python_ta.checkers.global_variables_checker import GlobalVariablesChecker
 class TestGlobalVariablesChecker(pylint.testutils.CheckerTestCase):
     CHECKER_CLASS = GlobalVariablesChecker
 
-    def test_no_message_global_type_alias_assignment(self):
+    def test_no_message_global_type_alias_assignment_builtin(self):
         """Global type alias assignment allowed, no message."""
         src = """
         MyType = list[list[list[int]]]
@@ -16,7 +16,7 @@ class TestGlobalVariablesChecker(pylint.testutils.CheckerTestCase):
         with self.assertNoMessages():
             self.checker.visit_assignname(mod)
 
-    def test_message_global_type_alias_assignment(self):
+    def test_message_global_type_alias_assignment_builtin(self):
         """Global type alias assignment not allowed, raises a message."""
         src = """
         TypeName = list[int]
@@ -31,3 +31,16 @@ class TestGlobalVariablesChecker(pylint.testutils.CheckerTestCase):
             ignore_position=True,
         ):
             self.checker.visit_assignname(mod)
+
+    def test_no_message_global_type_alias_assignment_import(self):
+        """Global type alias assignment allowed, no message."""
+        src = """
+        import datetime
+        MyType = datetime.date
+        """
+        mod = astroid.parse(src)
+        attribute_node = [child for child in mod.body[1].get_children()][1]
+        attribute_name_node = [child for child in attribute_node.get_children()][0]
+        with self.assertNoMessages():
+            self.checker.visit_import(mod.body[0])
+            self.checker.visit_assignname(attribute_name_node)

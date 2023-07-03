18401eb49ba629e95051f68ebb31bec199bbc465
Bruce Liu <liuxb904@gmail.com>
2023-07-03 14:13:35 -0400

    Added test case for changes made

---
+++ tests/test_contracts_type_alias_abstract_network.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+# This creates a type alias, to save us typing "int | tuple[int, int]" everywhere
+NodeAddress = int | tuple[int, int]
+
+
+###############################################################################
+# The Node class
+###############################################################################
+class Node:
+    address: NodeAddress
+
+    def __init__(self, address: NodeAddress) -> None:
+        self.address = address
+
+
+###############################################################################
+# The AbstractNetwork class
+###############################################################################
+class AbstractNetwork:
+    _nodes: dict[NodeAddress, Node]
+
+    def __init__(self) -> None:
+        self._nodes = {}
---
+++ tests/test_contracts_type_alias_abstract_ring.py
@@ -0,0 +1,11 @@
+from python_ta.contracts import check_contracts
+from tests.test_contracts_type_alias_abstract_network import AbstractNetwork
+
+
+def test_type_alias_as_type_annotation_for_class_attribute_no_error() -> None:
+    @check_contracts
+    class AbstractRing(AbstractNetwork):
+        def __init__(self) -> None:
+            AbstractNetwork.__init__(self)
+
+    AbstractRing()

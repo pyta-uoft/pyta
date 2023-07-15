85fc03e2ac652b3f42fc85d88fe4b47cefc66d7b
Bruce Liu <liuxb904@gmail.com>
2023-07-14 21:34:07 -0400

    Reverted change for the constant

--- python_ta/contracts/__init__.py
+++ python_ta/contracts/__init__.py
@@ -167,32 +167,31 @@ def add_class_invariants(klass: type) -> None:
 
         Check representation invariants for this class when not within an instance method of the class.
         """
-        if ENABLE_CONTRACT_CHECKING:
-            if name in cls_annotations:
+        if name in cls_annotations:
+            try:
+                _debug(f"Checking type of attribute {attr} for {klass.__qualname__} instance")
+                check_type(name, value, cls_annotations[name])
+            except TypeError:
+                raise AssertionError(
+                    f"Value {_display_value(value)} did not match type annotation for attribute "
+                    f"{name}: {_display_annotation(cls_annotations[name])}"
+                ) from None
+        original_attr_value = None
+        if hasattr(super(klass, self), name):
+            original_attr_value = super(klass, self).__getattribute__(name)
+        super(klass, self).__setattr__(name, value)
+        frame_locals = inspect.currentframe().f_back.f_locals
+        if self is not frame_locals.get("self"):
+            # Only validating if the attribute is not being set in a instance/class method
+            if klass_mod is not None and ENABLE_CONTRACT_CHECKING:
                 try:
-                    _debug(f"Checking type of attribute {attr} for {klass.__qualname__} instance")
-                    check_type(name, value, cls_annotations[name])
-                except TypeError:
-                    raise AssertionError(
-                        f"Value {_display_value(value)} did not match type annotation for attribute "
-                        f"{name}: {_display_annotation(cls_annotations[name])}"
-                    ) from None
-            original_attr_value = None
-            if hasattr(super(klass, self), name):
-                original_attr_value = super(klass, self).__getattribute__(name)
-            super(klass, self).__setattr__(name, value)
-            frame_locals = inspect.currentframe().f_back.f_locals
-            if self is not frame_locals.get("self"):
-                # Only validating if the attribute is not being set in a instance/class method
-                if klass_mod is not None:
-                    try:
-                        _check_invariants(self, klass, klass_mod.__dict__)
-                    except PyTAContractError as e:
-                        if original_attr_value is None:
-                            super(klass, self).__delattr__(name)
-                        else:
-                            super(klass, self).__setattr__(name, original_attr_value)
-                        raise AssertionError(str(e)) from None
+                    _check_invariants(self, klass, klass_mod.__dict__)
+                except PyTAContractError as e:
+                    if original_attr_value is None:
+                        super(klass, self).__delattr__(name)
+                    else:
+                        super(klass, self).__setattr__(name, original_attr_value)
+                    raise AssertionError(str(e)) from None
 
     for attr, value in klass.__dict__.items():
         if inspect.isroutine(value):

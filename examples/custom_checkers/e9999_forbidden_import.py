cc9f80d74b4990a528eec55c1092d7e38b779025
a <@>
2023-09-25 13:46:10 -0400

    update tests

--- examples/custom_checkers/e9999_forbidden_import.py
+++ examples/custom_checkers/e9999_forbidden_import.py
@@ -1,5 +1,6 @@
 import copy   # Error on this line
 from sys import path  # Error on this line
 import python_ta  # No error
+import e9999_forbidden_import_local  # No error
 
 __import__('math')  # Error on this line

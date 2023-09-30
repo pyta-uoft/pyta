166d7e1764ae6759e9d7149db949c605139680a2
Utku Egemen Umut <121046082+umututku03@users.noreply.github.com>
2023-09-30 11:31:51 -0400

    Move config related functions into python_ta.config submodule (#953)

--- python_ta/__init__.py
+++ python_ta/__init__.py
@@ -19,10 +19,14 @@ __version__ = "2.6.3.dev"  # Version number
 # First, remove underscore from builtins if it has been bound in the REPL.
 import builtins
 
-from pylint.config.config_file_parser import _ConfigurationFileParser
-from pylint.config.exceptions import _UnrecognizedOptionError
 from pylint.lint import PyLinter
 
+from .config import (
+    find_local_config,
+    load_config,
+    load_messages_config,
+    override_config,
+)
 from .reporters.core import PythonTaReporter
 
 try:
@@ -37,15 +41,12 @@ import tokenize
 import webbrowser
 from builtins import FileNotFoundError
 from os import listdir
-from pathlib import Path
 from typing import AnyStr, Generator, List, Optional, TextIO, Union
 
 import pylint.config
 import pylint.lint
 import pylint.utils
-import toml
 from astroid import MANAGER, modutils
-from pylint.config.config_initialization import _config_initialization
 from pylint.utils.pragma_parser import OPTION_PO
 
 from .patches import patch_all
@@ -119,7 +120,7 @@ def _check(
     current_reporter.set_output(output)
     messages_config_path = linter.config.messages_config_path
     messages_config_default_path = linter._option_dicts["messages-config-path"]["default"]
-    messages_config = _load_messages_config(messages_config_path, messages_config_default_path)
+    messages_config = load_messages_config(messages_config_path, messages_config_default_path)
 
     global PYLINT_PATCHED
     if not PYLINT_PATCHED:
@@ -206,84 +207,6 @@ def _check(
         )
         print('[ERROR] Error message: "{}"'.format(e))
         raise e
-
-
-def _find_local_config(curr_dir: AnyStr) -> Optional[AnyStr]:
-    """Search for a `.pylintrc` configuration file provided in same (user)
-    location as the source file to check.
-    Return absolute path to the file, or None.
-    `curr_dir` is an absolute path to a directory, containing a file to check.
-    For more info see, pylint.config.find_pylintrc
-    """
-    if curr_dir.endswith(".py"):
-        curr_dir = os.path.dirname(curr_dir)
-    if os.path.exists(os.path.join(curr_dir, "config", ".pylintrc")):
-        return os.path.join(curr_dir, "config", ".pylintrc")
-    elif os.path.exists(os.path.join(curr_dir, "config", "pylintrc")):
-        return os.path.join(curr_dir, "config", "pylintrc")
-
-
-def _load_config(linter: PyLinter, config_location: AnyStr) -> None:
-    """Load configuration into the linter."""
-    _config_initialization(linter, args_list=[], config_file=config_location)
-    linter.config_file = config_location
-
-
-def _override_config(linter: PyLinter, config_location: AnyStr) -> None:
-    """Override the default linter configuration options (if possible).
-
-    Snippets taken from pylint.config.config_initialization.
-    """
-    linter.set_current_module(config_location)
-
-    # Read the configuration file.
-    config_file_parser = _ConfigurationFileParser(verbose=True, linter=linter)
-    try:
-        _, config_args = config_file_parser.parse_config_file(file_path=config_location)
-    except OSError as ex:
-        print(ex, file=sys.stderr)
-        sys.exit(32)
-
-    # Override the config options by parsing the provided file.
-    try:
-        linter._parse_configuration_file(config_args)
-    except _UnrecognizedOptionError as exc:
-        unrecognized_options_message = ", ".join(exc.options)
-        linter.add_message("unrecognized-option", args=unrecognized_options_message, line=0)
-
-    # Everything has been set up already so emit any stashed messages.
-    linter._emit_stashed_messages()
-
-    linter.config_file = config_location
-
-
-def _load_messages_config(path: str, default_path: str) -> dict:
-    """Given path (potentially) specified by user and default default_path
-    of messages config file, merge the config files."""
-    merge_into = toml.load(default_path)
-
-    if Path(default_path).resolve() == Path(path).resolve():
-        return merge_into
-
-    try:
-        merge_from = toml.load(path)
-    except FileNotFoundError:
-        print(
-            f"[WARNING] Could not find messages config file at {str(Path(path).resolve())}. Using default messages config file at {str(Path(default_path).resolve())}."
-        )
-        return merge_into
-
-    for category in merge_from:
-        if category not in merge_into:
-            merge_into[category] = {}
-        for checker in merge_from[category]:
-            if checker not in merge_into[category]:
-                merge_into[category][checker] = {}
-            for error_code in merge_from[category][checker]:
-                merge_into[category][checker][error_code] = merge_from[category][checker][
-                    error_code
-                ]
-    return merge_into
 
 
 def reset_linter(
@@ -380,13 +303,13 @@ def reset_linter(
     linter.load_plugin_modules(custom_checkers)
     linter.load_plugin_modules(["python_ta.transforms.setendings"])
 
-    default_config_path = _find_local_config(os.path.dirname(__file__))
-    set_config = _load_config
+    default_config_path = find_local_config(os.path.dirname(__file__))
+    set_config = load_config
 
     if load_default_config:
-        _load_config(linter, default_config_path)
+        load_config(linter, default_config_path)
         # If we do specify to load the default config, we just need to override the options later.
-        set_config = _override_config
+        set_config = override_config
 
     if isinstance(config, str) and config != "":
         set_config(linter, config)
@@ -394,7 +317,7 @@ def reset_linter(
         # If available, use config file at directory of the file being linted.
         pylintrc_location = None
         if file_linted:
-            pylintrc_location = _find_local_config(file_linted)
+            pylintrc_location = find_local_config(file_linted)
 
         # Load or override the options if there is a config file in the current directory.
         if pylintrc_location:

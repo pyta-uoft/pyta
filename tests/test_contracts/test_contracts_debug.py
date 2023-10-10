8f33d56e9d2715dca8e92157486f4c56eb61b15a
merrickliu888 <merrickliu168@gmail.com>
2023-10-09 21:59:19 -0400

    contracts logging test

---
+++ tests/test_contracts/test_contracts_debug.py
@@ -0,0 +1,26 @@
+from python_ta import contracts
+
+contracts.DEBUG_CONTRACTS = True
+from python_ta.contracts import check_contracts
+
+
+def test_contracts_debug(caplog):
+    """Test to see if _debug method is logging messages correctly"""
+
+    @check_contracts
+    def divide(x: int, y: int) -> int:
+        """Return x // y.
+
+        Preconditions:
+            - invalid precondition
+        """
+        return x // y
+
+    divide(6, 2)
+
+    for record in caplog.records:
+        assert record.levelname == "DEBUG"
+    assert (
+        "Warning: precondition invalid precondition could not be parsed as a valid Python expression"
+        in caplog.text
+    )

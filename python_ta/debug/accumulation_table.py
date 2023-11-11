664ea1c59ace922803ea6cfbc32ca0c56f98dfd2
merrickliu888 <merrickliu168@gmail.com>
2023-11-10 19:03:29 -0500

    added top level variable for "N/A"

--- python_ta/debug/accumulation_table.py
+++ python_ta/debug/accumulation_table.py
@@ -13,6 +13,7 @@ from typing import Any, Union
 import astroid
 import tabulate
 
+NO_VALUE = 'N/A'
 
 def num_whitespaces(start_of_loop: str) -> int:
     """Return the number of spaces at the beginning of the accumulation loop"""
@@ -108,13 +109,13 @@ class AccumulationTable:
                 self.loop_variables[loop_var].append(copy.copy(frame.f_locals[loop_var]))
         else:
             for loop_var in self.loop_variables:
-                self.loop_variables[loop_var].append("N/A")
+                self.loop_variables[loop_var].append(NO_VALUE)
 
         for accumulator in self.loop_accumulators:
             if accumulator in frame.f_locals:
                 self.loop_accumulators[accumulator].append(copy.copy(frame.f_locals[accumulator]))
             elif accumulator in self._loop_variables_initialized_in_loop:
-                self.loop_accumulators[accumulator].append("N/A")
+                self.loop_accumulators[accumulator].append(NO_VALUE)
             else:
                 raise NameError
 

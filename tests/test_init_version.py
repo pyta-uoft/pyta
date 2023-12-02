4949be1fc40aa8e4af1c1922e89f7d58de2399ae
merrickliu888 <merrickliu168@gmail.com>
2023-12-02 13:41:56 -0500

    init logging fix

--- tests/test_init_version.py
+++ tests/test_init_version.py
@@ -1,11 +1,13 @@
 """Test for __init__.py system version check"""
 import sys
+import importlib
+import python_ta
 
 
 def test_sys_version_log(caplog, monkeypatch) -> None:
     """Testing if message logged when system version is too low is correct"""
     monkeypatch.setattr(sys, "version_info", (2, 6, 0))
-    import python_ta
+    importlib.reload(python_ta)  # Necessary due to python's import not actually reimporting package
 
     assert caplog.records[0].levelname == "WARNING"
     assert "You need Python 3.7 or later to run PythonTA." in caplog.text

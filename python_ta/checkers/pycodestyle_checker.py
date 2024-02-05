b43f37c6bd9d21584d78c90cad7c8a4eed1fff9d
MariaMa-GitHub <maria.shurui.ma@gmail.com>
2024-02-05 00:53:23 -0500

    Revert "remove git tracking of file"
    This reverts commit 61bd694bbcb12ce5c3fc115744df340753d6eb70.

---
+++ python_ta/checkers/pycodestyle_checker.py
@@ -0,0 +1,53 @@
+"""Checker for the style of the file"""
+from typing import List, Tuple
+
+import pycodestyle
+from astroid import nodes
+from pylint.checkers import BaseRawFileChecker
+from pylint.lint import PyLinter
+
+
+class PycodestyleChecker(BaseRawFileChecker):
+    """A checker class to report PEP8 style errors in the file.
+
+    Use options to specify the list of PEP8 errors to ignore"""
+
+    name = "pep8_errors"
+    msgs = {"E9989": ("Found pycodestyle (PEP8) style error %s at %s", "pep8-errors", "")}
+
+    options = (
+        (
+            "pycodestyle-ignore",
+            {
+                "default": (),
+                "type": "csv",
+                "metavar": "<pycodestyle-ignore>",
+                "help": "List of Pycodestyle errors to ignore",
+            },
+        ),
+    )
+
+    def process_module(self, node: nodes.NodeNG) -> None:
+        style_guide = pycodestyle.StyleGuide(
+            paths=[node.stream().name],
+            reporter=JSONReport,
+            ignore=self.linter.config.pycodestyle_ignore,
+        )
+        report = style_guide.check_files()
+
+        for line_num, msg, code in report.get_file_results():
+            self.add_message("pep8-errors", line=line_num, args=(code, msg))
+
+
+class JSONReport(pycodestyle.StandardReport):
+    def get_file_results(self) -> List[Tuple]:
+        self._deferred_print.sort()
+        return [
+            (line_number, f"line {line_number}, column {offset}: {text}", code)
+            for line_number, offset, code, text, _ in self._deferred_print
+        ]
+
+
+def register(linter: PyLinter) -> None:
+    """Required method to auto-register this checker to the linter"""
+    linter.register_checker(PycodestyleChecker(linter))

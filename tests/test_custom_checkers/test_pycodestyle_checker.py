c5588f81a12317d674816a937ad7e4ed8960c900
elescogna <eleonorascognamiglio03@gmail.com>
2024-02-17 12:34:44 -0500

    Added unit tests for pycodestyle E226 and E203

---
+++ tests/test_custom_checkers/test_pycodestyle_checker.py
@@ -0,0 +1,46 @@
+import os
+
+import pylint.testutils
+from astroid.astroid_manager import MANAGER
+
+from python_ta.checkers.pycodestyle_checker import PycodestyleChecker
+
+DIR_PATH = (
+    os.path.abspath(os.path.join(__file__, "../../.."))
+    + "/examples/custom_checkers/e9989_pycodestyle/"
+)
+
+
+class TestPycodestyleChecker(pylint.testutils.CheckerTestCase):
+    CHECKER_CLASS = PycodestyleChecker
+    CONFIG = {"pycodestyle_ignore": ["E121", "E123", "E126", "E24", "E704", "W503", "W504"]}
+
+    def test_error_e226(self) -> None:
+        """Test that PEP8 error E226 (missing whitespace around arithmetic operator) is triggered"""
+        mod = MANAGER.ast_from_file(DIR_PATH + "e226_error.py")
+        args = ("E226", "line 1, column 5: missing whitespace around arithmetic operator")
+        with self.assertAddsMessages(
+            pylint.testutils.MessageTest(msg_id="pep8-errors", line=1, args=args)
+        ):
+            self.checker.process_module(mod)
+
+    def test_no_error_e226(self) -> None:
+        """Test that PEP8 error E226 (missing whitespace around arithmeic operator) is NOT triggered"""
+        mod = MANAGER.ast_from_file(DIR_PATH + "e226_no_error.py")
+        with self.assertNoMessages():
+            self.checker.process_module(mod)
+
+    def test_error_e203(self) -> None:
+        """Test that PEP8 error E203 (whitespace before ‘,’, ‘;’, or ‘:’) is triggered"""
+        mod = MANAGER.ast_from_file(DIR_PATH + "e203_error.py")
+        args = ("E203", "line 1, column 30: whitespace before ':'")
+        with self.assertAddsMessages(
+            pylint.testutils.MessageTest(msg_id="pep8-errors", line=1, args=args)
+        ):
+            self.checker.process_module(mod)
+
+    def test_no_error_e203(self) -> None:
+        """Test that PEP8 error E203 (whitespace before ‘,’, ‘;’, or ‘:’) is NOT triggered"""
+        mod = MANAGER.ast_from_file(DIR_PATH + "e203_no_error.py")
+        with self.assertNoMessages():
+            self.checker.process_module(mod)

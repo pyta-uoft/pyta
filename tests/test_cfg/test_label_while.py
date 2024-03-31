5c9048956c27e367b14a4f4d97278c5b71b923db
Michael Zhao <82855036+michaelZhaoCode@users.noreply.github.com>
2024-03-31 10:17:09 -0400

    Augmented CFGEdge to store condition node in If/While conditions (#1027)

--- tests/test_cfg/test_label_while.py
+++ tests/test_cfg/test_label_while.py
@@ -1,4 +1,4 @@
-from typing import Set
+from typing import List, Set
 
 import astroid
 
@@ -23,6 +23,15 @@ def _extract_labels(cfg: ControlFlowGraph) -> Set[str]:
 def _extract_num_labels(cfg: ControlFlowGraph) -> int:
     """Return the number of labelled edges in the cfg."""
     return sum(1 for edge in cfg.get_edges() if edge.label is not None)
+
+
+def _extract_edge_conditions(cfg: ControlFlowGraph) -> List[str]:
+    """Return the edge conditions in the given cfg as a list of strings representing the condition."""
+
+    conditions = [
+        edge.condition.as_string() for edge in cfg.get_edges() if edge.condition is not None
+    ]
+    return conditions
 
 
 def test_num_while_labels() -> None:
@@ -159,3 +168,81 @@ def test_type_complex_while_else_labels() -> None:
     """
     expected_labels = {"True", "False"}
     assert _extract_labels(build_cfg(src)) == expected_labels
+
+
+def test_while_conditions() -> None:
+    """Test that the conditions are correctly set for edges produced in a while loop."""
+    src = """
+    i = 0
+    while i < 10:
+        i += 1
+
+    print('not else')
+    """
+    expected_num_conditions = 2
+    found_conditions = _extract_edge_conditions(build_cfg(src))
+    assert all(condition == "i < 10" for condition in found_conditions)
+    assert len(found_conditions) == expected_num_conditions
+
+
+def test_while_else_conditions() -> None:
+    """Test that the conditions are correctly set for edges produced in a while-else loop."""
+    src = """
+    i = 0
+    while i < 10:
+        i += 1
+    else:
+        print('is else')
+
+    print('not else')
+    """
+    expected_num_conditions = 2
+    found_conditions = _extract_edge_conditions(build_cfg(src))
+    assert all(condition == "i < 10" for condition in found_conditions)
+    assert len(found_conditions) == expected_num_conditions
+
+
+def test_complex_while_conditions() -> None:
+    """Test that the conditions are correctly set for edges produced in a complex while loop."""
+    src = """
+    i = 0
+    while i < 10:
+        j = 0
+        while j < 5:
+            j += 1
+        i += 1
+
+        if i > 4:
+            print('hi')
+
+    print('not else')
+    """
+    expected_num_conditions = 6
+    found_conditions = _extract_edge_conditions(build_cfg(src))
+    expected_conditions = ["i < 10", "j < 5", "j < 5", "i > 4", "i > 4", "i < 10"]
+    assert found_conditions == expected_conditions
+    assert len(found_conditions) == expected_num_conditions
+
+
+def test_complex_while_else_conditions() -> None:
+    """Test that the conditions are correctly set for edges produced in a complex while-else loop."""
+    src = """
+    i = 0
+    while i < 10:
+        j = 0
+        while j < 5:
+            j += 1
+        i += 1
+
+        if i > 4:
+            print('hi')
+    else:
+        print('is else')
+
+    print('not else')
+    """
+    expected_num_conditions = 6
+    found_conditions = _extract_edge_conditions(build_cfg(src))
+    expected_conditions = ["i < 10", "j < 5", "j < 5", "i > 4", "i > 4", "i < 10"]
+    assert found_conditions == expected_conditions
+    assert len(found_conditions) == expected_num_conditions

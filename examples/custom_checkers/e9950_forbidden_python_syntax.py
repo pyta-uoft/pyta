6f332f2121926831cba1e10cbde576e114c1eeff
Raine-Yang-UofT <raine.yang@mail.utoronto.ca>
2024-06-03 15:42:33 -0400

    docs: update checker documentation
    Update file paths in checker documentation to match the modified example
    checker names
    Revert an unexpected modification in forbidden_python_syntax checker
    Correctly rename non_ascii_name checker

--- examples/custom_checkers/e9950_forbidden_python_syntax.py
+++ examples/custom_checkers/e9950_forbidden_python_syntax.py
@@ -10,8 +10,3 @@ for i in range(1, 10):  # forbidden python syntax
         break  # forbidden python syntax
 
 squares = [i ** 2 for i in range(1, 10)]  # forbidden python syntax
-
-
-if __name__ == '__main__':
-    import python_ta
-    python_ta.check_all()

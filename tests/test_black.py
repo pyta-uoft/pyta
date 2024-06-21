5976ca6bbfdf22f876dcb1347bda4c321343e3e4
Lana W <lanawehbeh12@gmail.com>
2024-06-21 01:15:45 -0400

    Full test suite - parametrized

--- tests/test_black.py
+++ tests/test_black.py
@@ -1,38 +1,66 @@
+"""
+Contains tests for the black formatting tool integration to python_ta.check_all.
+"""
+
+import contextlib
+import io
 import subprocess
 import sys
 
 import pytest
 
+error_params = [
+    (
+        """def foo():print("Hello, world!")\nimport python_ta\npython_ta.check_all(autoformat=True)""",
+        True,
+        """def foo():\n    print("Hello, world!")\n\n\nimport python_ta\n\npython_ta.check_all(autoformat=True)\n""",
+    ),
+    (
+        """def foo():print("Hello, world!")\nimport python_ta\npython_ta.check_all(autoformat=True, config={'max-line-length': 50})""",
+        True,
+        """def foo():\n    print("Hello, world!")\n\n\nimport python_ta\n\npython_ta.check_all(\n    autoformat=True,\n    config={\'max-line-length\': 50},\n)\n""",
+    ),
+    (
+        """def foo():print("Hello, world!")\nimport python_ta\npython_ta.check_all(autoformat=False)""",
+        False,
+        """def foo():print("Hello, world!")\nimport python_ta\npython_ta.check_all(autoformat=False)""",
+    ),
+]
+
 
 @pytest.fixture
-def unformatted_file(tmp_path):
+@pytest.mark.parametrize("source", error_params)
+def unformatted_file(tmp_path, source):
     # Create a temporary file with unformatted Python code
-    source = """def foo():print("Hello, world!")\nimport python_ta\npython_ta.check_all(autoformat=True)"""
     file_path = tmp_path / "sample_code.py"
     with open(file_path, "w") as f:
         f.write(source)
     return file_path
 
 
-def test_black_formatting(unformatted_file):
-    # Run the unformatted file, which runs python_ta.check_all(autoformat=True)
-    result = subprocess.run(
-        [
-            sys.executable,
-            unformatted_file,
-        ],
-        capture_output=True,
-        text=True,
-    )
+@pytest.mark.parametrize("source, pep8_check, expected_code", error_params)
+def test_black_formatting(source, pep8_check, expected_code, unformatted_file):
+    output = io.StringIO()
 
-    # check black ran successfully
+    with contextlib.redirect_stdout(output):
+        result = subprocess.run(
+            [
+                sys.executable,
+                unformatted_file,
+            ],
+        )
+
+    # Check for pep8_errors in reporter
+    if pep8_check:
+        num_pep8 = output.getvalue().count('"symbol": "pep8-errors"')
+        assert num_pep8 == 0, f"Expected:\n{0}\nBut got:\n{num_pep8}"
+
+    # Check black ran successfully
     assert result.returncode == 0, f"Black failed with {result.stderr}"
 
     # Check if black formatted the file correctly
     with open(unformatted_file, "r") as f:
         formatted_code = f.read()
-
-    expected_code = """def foo():\n    print("Hello, world!")\n\n\nimport python_ta\n\npython_ta.check_all(autoformat=True)\n"""
 
     assert (
         formatted_code == expected_code

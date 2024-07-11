ae1526653c0bdde079efddfa7ef5132ce8642e7b
quiz3 <113859565+quiz3@users.noreply.github.com>
2024-07-11 13:48:22 -0400

    added more test coverage

--- tests/test_transforms/test_expr_wrapper.py
+++ tests/test_transforms/test_expr_wrapper.py
@@ -1,9 +1,11 @@
 """Tests for ExprWrapper in python_ta.transforms."""
 
+from astroid import parse
+
 from python_ta.transforms import ExprWrapper, Z3ParseException
 
 
-def test_expr_wrapper_invalid_node():
+def test_expr_wrapper_invalid_node() -> None:
     exception_caught = False
 
     try:
@@ -12,3 +14,13 @@ def test_expr_wrapper_invalid_node():
         exception_caught = True
 
     assert exception_caught
+
+
+def test_expr_wrapper_assignment() -> None:
+    assignment = "n = x + y - z"
+
+    mod = parse(assignment)
+    node = mod.body[0]
+
+    expr = ExprWrapper(node)
+    assert expr.node is node.value

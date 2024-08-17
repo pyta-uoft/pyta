fe86bd7786a81320a188e89d748fe97cce0e2f66
Lana W <lanawehbeh12@gmail.com>
2024-08-17 18:42:10 -0400

    Additional files for testing snapshot save

---
+++ tests/test_debug/test_snapshot_save_stdout.py
@@ -0,0 +1,12 @@
+"""
+This Python module is designed for testing the snapshot function's ability to, when save is True,
+return the snapshot svg to stdout.
+
+This module is intended exclusively for testing purposes and should not be used for any other purpose.
+"""
+
+from python_ta.debug.snapshot import snapshot
+
+test_var1a = "David is cool!"
+test_var2a = "Students Developing Software"
+snapshot(True, ["--roughjs-config", "seed=12345"], "0.3.1")

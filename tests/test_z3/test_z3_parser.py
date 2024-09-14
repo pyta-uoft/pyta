02e9adbe06d98b7dcd4dde23632f6cbc97112849
Raine-Yang-UofT <147955278+Raine-Yang-UofT@users.noreply.github.com>
2024-09-13 21:57:08 -0400

    Refactor ExprWrapper and rename it to Z3Parser (#1080)
    Remove the passing of `node` argument to Z3Parser constructor.
    Fix error in handling Arguments and Expr nodes.
    Rename test_expr_wrapper to test_z3_parser.

---
+++ tests/test_z3/test_z3_parser.py
@@ -0,0 +1,13 @@
+"""Tests for Z3Parser in python_ta.z3."""
+
+from astroid import extract_node
+
+from python_ta.z3.z3_parser import Z3Parser
+
+
+def test_z3_parser_assignment() -> None:
+    assignment = "n = 2 + 3"
+
+    node = extract_node(assignment)
+    parser = Z3Parser()
+    assert parser.parse(node) == 5

82fd241154957b0a6ce5b25e3afa3654b98d3b4e
leowrites <leo@liuwork.ca>
2024-09-30 12:03:44 -0400

    use kwargs

--- python_ta/debug/__init__.py
+++ python_ta/debug/__init__.py
@@ -1,3 +1,3 @@
 from .accumulation_table import AccumulationTable
 from .recursion_table import RecursionTable
-from .snapshot_manager import SnapshotManager
+from .snapshot_manager import SnapshotTracer
--- python_ta/debug/snapshot_manager.py
+++ python_ta/debug/snapshot_manager.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import copy
 import inspect
 import logging
 import os
@@ -8,64 +9,45 @@ import sys
 import types
 from typing import Any, Iterable, Optional
 
-from python_ta.debug.snapshot import snapshot
+from .snapshot import snapshot
 
 
-class SnapshotManager:
+class SnapshotTracer:
     """
-    A class used for snapshot-based debugging to analyze memory usage at each line in the calling function.
+    A class used for snapshot-based debugging to visualize program memory at each line in the calling function.
 
     Instance attributes:
-        memory_viz_args: The arguments to pass to the MemoryViz CLI.
-        memory_viz_version: The version of MemoryViz to use.
         include: A collection of function names, either as strings or regular expressions, whose variables will be captured.
         output_directory: The directory where the memory model diagrams will be saved. Defaults to the current directory.
     """
 
-    memory_viz_args: Optional[list[str]]
-    memory_viz_version: Optional[str]
     _snapshot_counts: int
-    include: Optional[Iterable[str | re.Pattern]]
     output_directory: Optional[str]
+    _snapshot_args: dict[str, Any]
 
-    def __init__(
-        self,
-        memory_viz_args: Optional[list[str]] = None,
-        memory_viz_version: Optional[str] = "latest",
-        include: Optional[Iterable[str | re.Pattern]] = None,
-        output_directory: Optional[str] = None,
-    ) -> None:
+    def __init__(self, output_directory: Optional[str] = None, **kwargs) -> None:
         """Initialize a SnapshotManager context manager for snapshot-based debugging.
 
         Args:
-            memory_viz_args: The arguments to pass to the MemoryViz CLI.
-            memory_viz_version: The version of MemoryViz to use.
-            include: A collection of function names, either as strings or regular expressions, whose variables will be captured.
             output_directory: The directory to save the snapshots, defaulting to the current directory.
                 **Note**: Use this argument instead of the `--output` flag in `memory_viz_args` to specify the output directory.
+            **kwargs: All other keyword arguments are passed to `python.debug.snapshot`. Refer to the `snapshot` function for more details.
         """
         if sys.version_info < (3, 10, 0):
             logging.warning("You need Python 3.10 or later to use SnapshotManager.")
-        self.memory_viz_version = memory_viz_version
-
-        if memory_viz_args:
-            if any("--output" in arg for arg in memory_viz_args):
-                raise ValueError(
-                    "Use the output_directory parameter to specify a different output path."
-                )
-            self.memory_viz_args = memory_viz_args
-        else:
-            self.memory_viz_args = ["--roughjs-config", "seed=12345"]
-        self._snapshot_counts = 0
-        self.include = include
-
+        if any("--output" in arg for arg in kwargs.get("memory_viz_args", [])):
+            raise ValueError(
+                "Use the output_directory parameter to specify a different output path."
+            )
+        self._snapshot_args = kwargs
         self.output_directory = output_directory if output_directory else "."
+        self._snapshot_counts = 0
 
     def _trace_func(self, frame: types.FrameType, event: str, _arg: Any) -> None:
         """Take a snapshot of the variables in the functions specified in `self.include`"""
         if event == "line" and frame.f_locals:
-            memory_viz_args_copy = self.memory_viz_args.copy()
-            memory_viz_args_copy.extend(
+            snapshot_args = copy.deepcopy(self._snapshot_args)
+            snapshot_args["memory_viz_args"].extend(
                 [
                     "--output",
                     os.path.join(
@@ -75,10 +57,8 @@ class SnapshotManager:
                 ]
             )
             snapshot(
-                include=self.include,
                 save=True,
-                memory_viz_args=memory_viz_args_copy,
-                memory_viz_version=self.memory_viz_version,
+                **snapshot_args,
             )
             self._snapshot_counts += 1
 

68bfa7a2f861b9b9063af95e900aadbd132a0c3a
CulmoneY <yculmone@rogers.com>
2024-10-09 00:42:13 -0400

    Added pytest fixture to prevetn webbrowser openings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,7 @@
+import webbrowser
+from http.server import HTTPServer
+from unittest.mock import Mock
+
 import pytest
 
 import python_ta.contracts
@@ -9,3 +13,11 @@ def disable_contract_checking():
     python_ta.contracts.ENABLE_CONTRACT_CHECKING = False
     yield
     python_ta.contracts.ENABLE_CONTRACT_CHECKING = True
+
+
+@pytest.fixture()
+def prevent_webbrowser_and_httpserver():
+    """Automatically mock webbrowser.open and HTTPServer.handle_request in all tests. this prevents any owser/server
+    code running when running Pytest to avoid CI timeouts, and unexpected browser popups."""
+    webbrowser.open = Mock(return_value=None)
+    HTTPServer.handle_request = Mock(return_value=None)

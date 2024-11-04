0d85cbe993f61d614565a7ecd0e753f2a6aabf1c
Raine-Yang-UofT <raine.yang@mail.utoronto.ca>
2024-11-03 20:50:35 -0500

    feat: revert changes to test_examples

--- examples/custom_checkers/z3_option/z3_e9959_redundant_assignment.py
+++
@@ -1,9 +0,0 @@
-def func(x: int) -> int:
-    '''
-    Preconditions:
-        - x > 10
-    '''
-    a = 10
-    if x > 0:   # by function precondition, this condition is True
-        a = 20  # `a` is reassigned before first use
-    return a
--- examples/custom_checkers/z3_option/z3_r9711_missing_return_statement.py
+++
@@ -1,8 +0,0 @@
-def func(x: int) -> int:
-    '''
-    Preconditions:
-        - x in [1, 2, 3, 4, 5]
-    '''
-    if x > 5:
-        return x
-    print(x)

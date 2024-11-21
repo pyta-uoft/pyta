804b1bc4598b8792f8b8face8af59f62116ff3a1
yoonie-jang <yoonie.jang@mail.utoronto.ca>
2024-11-20 19:36:35 -0500

    change type annotations and rename attributes

--- python_ta/debug/snapshot_tracer.py
+++ python_ta/debug/snapshot_tracer.py
@@ -22,21 +22,22 @@ class SnapshotTracer:
 
     Instance attributes:
         output_directory: The directory where the memory model diagrams will be saved. Defaults to the current directory.
-        open_webstepper: Opens the web-based visualizer.
-        _snapshot_to_line: A list of dictionaries that maps the code line number and the snapshot number.
+        webstepper: Opens the web-based visualizer.
+        _snapshots: A list of dictionaries that maps the code line number and the snapshot number.
         _snapshot_args: A dictionary of keyword arguments to pass to the `snapshot` function.
         _first_line: Line number of the first line in the `with` block.
     """
 
     output_directory: Optional[str]
-    open_webstepper: bool
-    _snapshot_to_line: dict[int, int]
+    webstepper: bool
+    _snapshots: list[dict[int, int]]
     _snapshot_args: dict[str, Any]
+    _first_line: int
 
     def __init__(
         self,
         output_directory: Optional[str] = None,
-        open_webstepper: bool = False,
+        webstepper: bool = False,
         **kwargs,
     ) -> None:
         """Initialize a context manager for snapshot-based debugging.
@@ -44,7 +45,7 @@ class SnapshotTracer:
         Args:
             output_directory: The directory to save the snapshots, defaulting to the current directory.
                 **Note**: Use this argument instead of the `--output` flag in `memory_viz_args` to specify the output directory.
-            open_webstepper: Opens a MemoryViz Webstepper webpage to interactively visualize the resulting memory diagrams.
+            webstepper: Opens a MemoryViz Webstepper webpage to interactively visualize the resulting memory diagrams.
             **kwargs: All other keyword arguments are passed to `python.debug.snapshot`. Refer to the `snapshot` function for more details.
         """
         if sys.version_info < (3, 10, 0):
@@ -57,7 +58,7 @@ class SnapshotTracer:
         self._snapshot_args = kwargs
         self._snapshot_args["memory_viz_args"] = copy.deepcopy(kwargs.get("memory_viz_args", []))
         self.output_directory = os.path.abspath(output_directory if output_directory else ".")
-        self.open_webstepper = open_webstepper
+        self.webstepper = webstepper
         self._first_line = float("inf")
 
     def _trace_func(self, frame: types.FrameType, event: str, _arg: Any) -> None:
@@ -101,7 +102,7 @@ class SnapshotTracer:
         """Remove the trace function and run any post-tracing functions."""
         sys.settrace(None)
         self._func_frame.f_trace = None
-        if self.open_webstepper:
+        if self.webstepper:
             self._build_result_html()
             self._open_html()
 

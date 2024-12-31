e0d6a5b5dab77bd5d87503d5f4e276425d9e9e1f
CulmoneY <yculmone@rogers.com>
2024-12-30 21:35:29 -0500

    modified test cases for coveralls

--- examples/custom_checkers/static_type_checker_examples/e9955_union_attr_error.py
+++ examples/custom_checkers/static_type_checker_examples/e9955_union_attr_error.py
@@ -1,7 +1,8 @@
 from typing import Union
 
 def get_status(status: Union[str, int, float]) -> str:
-    return status.upper()  # Error: Item "int" of "str | int | float" has no attribute "upper"
+    return (status
+            .upper())  # Error: Item "int" of "str | int | float" has no attribute "upper"
                            # Error: Item "float" of "str | int | float" has no attribute "upper"
 
 def get_keys(data: Union[dict, list]) -> list:

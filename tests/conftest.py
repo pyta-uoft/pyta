ab2e81d6888f7b769dc9a32eced1f7b249590d9d
Raine Yang <147955278+Raine-Yang-UofT@users.noreply.github.com>
2025-01-05 17:55:55 -0400

    Refactor CI tests to separate z3 tests (#1126)
    Configure CI matrix with "base" and "z3-solver". z3 is installed only for z3-solver config.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,23 @@
+import re
 from http.server import HTTPServer
 
 import pytest
 
 import python_ta.contracts
+
+Z3_RELATED_TESTS = {
+    r".*test_z3_constraints.*",
+    r".*test_edge_feasibility.*",
+    r".*test_impossible_condition_checker.*",
+    r".*test_redundant_condition_checker.*",
+    r".*test_z3_parser.*",
+    r".*test_z3_visitor.*",
+    r".*test_inconsistent_returns.*",
+    r".*test_missing_return_statements.*",
+    r".*test_one_iteration_checker.*",
+    r".*test_possibly_undefined_checker.*",
+    r".*test_redundant_assignment_checker.*",
+}
 
 
 @pytest.fixture()
@@ -27,3 +42,39 @@ def prevent_webbrowser_and_httpserver(mocker):
     code running when running Pytest to avoid CI timeouts, and unexpected browser popups."""
     mocker.patch("webbrowser.open", return_value=None)
     mocker.patch.object(HTTPServer, "handle_request", return_value=None)
+
+
+def pytest_addoption(parser):
+    """Custom command-line options to enable/disable exclusion of certain tests"""
+    parser.addoption(
+        "--exclude-z3",
+        action="store_true",
+        default=False,
+        help="Exclude test cases the require z3 dependency.",
+    )
+
+
+def pytest_ignore_collect(path, config):
+    """Return True to prevent collecting a test file or directory.
+    Note: this function must return None for test cases not intended to exclude. Otherwise, it will interfere
+    with other configurations such as --exclude flag.
+
+    Refer to the following docstring in Pytest source code:
+
+    Return ``True`` to ignore this path for collection.
+
+    Return ``None`` to let other plugins ignore the path for collection.
+
+    Returning ``False`` will forcefully *not* ignore this path for collection,
+    without giving a chance for other plugins to ignore this path.
+    https://github.com/pytest-dev/pytest/blob/main/src/_pytest/hookspec.py
+    """
+    if config.getoption("--exclude-z3"):
+        # Convert path to string for pattern matching
+        path_str = str(path)
+        if any(re.search(pattern, path_str) for pattern in Z3_RELATED_TESTS):
+            return True
+        else:
+            return None
+
+    return None

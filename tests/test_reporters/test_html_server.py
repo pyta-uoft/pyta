88b398284bed081d2ac46c25009590fe940556f9
CulmoneY <yculmone@rogers.com>
2025-01-29 00:47:22 -0500

    testing to see server opening time

--- tests/test_reporters/test_html_server.py
+++ tests/test_reporters/test_html_server.py
@@ -61,7 +61,7 @@ def test_open_html_in_browser_watch():
         cwd=os.getcwd(),
     )
 
-    time.sleep(1)
+    time.sleep(60)
     try:
         for _ in range(3):
             conn = HTTPConnection("127.0.0.1", 5008)

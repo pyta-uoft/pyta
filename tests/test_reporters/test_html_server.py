52f5d03b0b7a5ac079e908da93f68d26a08fd0d1
CulmoneY <yculmone@rogers.com>
2025-02-02 01:48:06 -0500

    added all changes other than snapshot

--- tests/test_reporters/test_html_server.py
+++ tests/test_reporters/test_html_server.py
@@ -1,6 +1,4 @@
 import os
-import pprint
-import re
 import signal
 import subprocess
 import sys
@@ -10,7 +8,6 @@ from http.client import HTTPConnection, RemoteDisconnected
 from unittest.mock import patch
 
 import pytest
-import requests
 
 from python_ta.reporters.html_server import open_html_in_browser
 
@@ -32,34 +29,26 @@ def wait_for_server(port: int, timeout: int = 30, interval: int = 1):
     return False
 
 
-@patch("webbrowser.open")
-def test_open_html_in_browser_no_watch(mock_webbrowser_open):
+def test_open_html_in_browser_no_watch():
     """
     Test the open_html_in_browser function with watch=False.
     Ensure the server handles a single request and shuts down properly.
     """
-    html_content = b"<html><body><h1>Test HTML - No Watch</h1></body></html>"
-    mock_webbrowser_open.return_value = True
-    thread = threading.Thread(target=open_html_in_browser, args=(html_content, False, 0))
-    thread.start()
-    thread.join(timeout=0.1)
 
-    args, _ = mock_webbrowser_open.call_args
-    port = int(args[0].split(":")[2])
-
-    conn = HTTPConnection("127.0.0.1", port)
-    conn.request("GET", "/")
-    response = conn.getresponse()
-
-    assert response.status == 200
-    assert response.read() == html_content
-
-    with pytest.raises((ConnectionRefusedError, RemoteDisconnected)):
-        new_conn = HTTPConnection("127.0.0.1", port)
-        new_conn.request("GET", "/")
-        new_conn.getresponse()
-    conn.close()
-    new_conn.close()
+    script_path = os.path.normpath(
+        os.path.join(__file__, "../../fixtures/reporters/no_watch_integration.py")
+    )
+    process = subprocess.Popen([sys.executable, script_path])
+    if not wait_for_server(5008):
+        process.send_signal(signal.SIGINT)
+        pytest.fail("Server did not start within the expected timeout")
+    try:
+        with pytest.raises((ConnectionRefusedError, RemoteDisconnected)):
+            new_conn = HTTPConnection("127.0.0.1", 5008)
+            new_conn.request("GET", "/")
+            new_conn.getresponse()
+    finally:
+        process.send_signal(signal.SIGINT)
 
 
 def test_open_html_in_browser_watch():
@@ -74,13 +63,14 @@ def test_open_html_in_browser_watch():
 
     if not wait_for_server(5008):
         process.send_signal(signal.SIGINT)
-        assert False
+        pytest.fail("Server did not start within the expected timeout")
 
     try:
-        for _ in range(3):
+        for i in range(3):
             conn = HTTPConnection("127.0.0.1", 5008)
             conn.request("GET", "/")
             response = conn.getresponse()
             assert response.status == 200
+
     finally:
         process.send_signal(signal.SIGINT)

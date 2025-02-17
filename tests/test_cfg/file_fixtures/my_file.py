7838f3985fe122f02ebfbf01534466a75fbecdf3
David Liu <david@cs.toronto.edu>
2025-02-17 15:38:32 -0500

    Improved control flow graph rendering and file management

--- tests/test_cfg/file_fixtures/my_file.py
+++ tests/test_cfg/file_fixtures/my_file.py
@@ -5,3 +5,18 @@ def foo() -> None:
     for i in range(1, 10):
         if i < 5:
             print("hi")
+
+
+def func_with_while() -> None:
+    """A function with a while loop"""
+    a = 1
+    while a < 100:
+        print(a)
+        a += 1
+
+
+def func_with_unreachable() -> int:
+    """A function with unreachable code"""
+    a = 1
+    return a * 10
+    a = 2

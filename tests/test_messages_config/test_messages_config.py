309e9bab56c16b1a85f1227da7f545f23a5c629f
herenali <herenalihoiching@gmail.com>
2025-03-18 14:49:06 -0400

    Add tests

--- tests/test_messages_config/test_messages_config.py
+++ tests/test_messages_config/test_messages_config.py
@@ -1,126 +1,122 @@
-import subprocess
-import sys
-from os import environ
+import io
+import logging
+
+import pytest
+
+import python_ta
 
 
-def test_valid_message_config_and_pyta_overwrite():
+@pytest.fixture
+def output() -> None:
+    """Create a StringIO object to be passed into the output argument of the check function."""
+    output = io.StringIO()
+    yield output
+    output.close()
+
+
+def test_valid_message_config_and_pyta_overwrite(output: io.StringIO):
     """Test the error messages given a valid messages-config-path with use-pyta-error-messages as True"""
-    output = subprocess.run(
-        [
-            sys.executable,
-            "-m",
-            "python_ta",
-            "--config",
-            "tests/test_messages_config/test_user_config_pyta_overwrite.pylintrc",
-            "tests/test_messages_config/testing_code.py",
-        ],
-        capture_output=True,
-        text=True,
-        env={**environ, "PYTHONIOENCODING": "utf-8"},
+    python_ta.check_all(
+        "tests/test_messages_config/testing_code.py",
+        config="tests/test_messages_config/test_user_config_pyta_overwrite.pylintrc",
+        output=output,
     )
 
-    assert "This custom error message is modified." in output.stdout
-    assert "The first reversed() argument is not a sequence" not in output.stdout
+    assert "This custom error message is modified." in output.getvalue()
+    assert "The first reversed() argument is not a sequence" not in output.getvalue()
 
 
-def test_no_message_config_and_pyta_overwrite():
+def test_no_message_config_and_pyta_overwrite(output: io.StringIO):
     """Test the error messages without a messages-config-path with use-pyta-error-messages as True"""
-    output = subprocess.run(
-        [
-            sys.executable,
-            "-m",
-            "python_ta",
-            "--config",
-            "tests/test_messages_config/test_no_user_config_pyta_overwrite.pylintrc",
-            "tests/test_messages_config/testing_code.py",
-        ],
-        capture_output=True,
-        text=True,
-        env={**environ, "PYTHONIOENCODING": "utf-8"},
+    python_ta.check_all(
+        "tests/test_messages_config/testing_code.py",
+        config="tests/test_messages_config/test_no_user_config_pyta_overwrite.pylintrc",
+        output=output,
     )
 
-    assert "This custom error message is modified." not in output.stdout
-    assert "The first reversed() argument is not a sequence" not in output.stdout
+    assert "This custom error message is modified." not in output.getvalue()
+    assert "The first reversed() argument is not a sequence" not in output.getvalue()
 
 
-def test_valid_message_config_and_no_pyta_overwrite():
+def test_valid_message_config_and_no_pyta_overwrite(output: io.StringIO):
     """Test the error messages given a valid messages-config-path with use-pyta-error-messages as False"""
-    output = subprocess.run(
-        [
-            sys.executable,
-            "-m",
-            "python_ta",
-            "--config",
-            "tests/test_messages_config/test_user_config_no_pyta_overwrite.pylintrc",
-            "tests/test_messages_config/testing_code.py",
-        ],
-        capture_output=True,
-        text=True,
-        env={**environ, "PYTHONIOENCODING": "utf-8"},
+    python_ta.check_all(
+        "tests/test_messages_config/testing_code.py",
+        config="tests/test_messages_config/test_user_config_no_pyta_overwrite.pylintrc",
+        output=output,
     )
 
-    assert "This custom error message is modified." in output.stdout
-    assert "The first reversed() argument is not a sequence" in output.stdout
+    assert "This custom error message is modified." in output.getvalue()
+    assert "The first reversed() argument is not a sequence" in output.getvalue()
 
 
-def test_no_message_config_and_no_pyta_overwrite():
+def test_no_message_config_and_no_pyta_overwrite(output: io.StringIO):
     """Test the error messages without a messages-config-path with use-pyta-error-messages as False"""
-    output = subprocess.run(
-        [
-            sys.executable,
-            "-m",
-            "python_ta",
-            "--config",
-            "tests/test_messages_config/test_no_user_config_no_pyta_overwrite.pylintrc",
-            "tests/test_messages_config/testing_code.py",
-        ],
-        capture_output=True,
-        text=True,
-        env={**environ, "PYTHONIOENCODING": "utf-8"},
+    python_ta.check_all(
+        "tests/test_messages_config/testing_code.py",
+        config="tests/test_messages_config/test_no_user_config_no_pyta_overwrite.pylintrc",
+        output=output,
     )
 
-    assert "This custom error message is modified." not in output.stdout
-    assert "The first reversed() argument is not a sequence" in output.stdout
+    assert "This custom error message is modified." not in output.getvalue()
+    assert "The first reversed() argument is not a sequence" in output.getvalue()
 
 
-def test_valid_message_config_no_section_header_and_pyta_overwrite():
+def test_valid_message_config_no_section_header_and_pyta_overwrite(output: io.StringIO):
     """Test the error messages given a valid messages-config-path with no section headers
     and use-pyta-error-messages as True.
     """
-    output = subprocess.run(
-        [
-            sys.executable,
-            "-m",
-            "python_ta",
-            "--config",
-            "tests/test_messages_config/test_user_config_no_section_header_pyta_overwrite.pylintrc",
-            "tests/test_messages_config/testing_code.py",
-        ],
-        capture_output=True,
-        text=True,
-        env={**environ, "PYTHONIOENCODING": "utf-8"},
+    python_ta.check_all(
+        "tests/test_messages_config/testing_code.py",
+        config="tests/test_messages_config/test_user_config_no_section_header_pyta_overwrite.pylintrc",
+        output=output,
     )
 
-    assert "This custom error message is modified." in output.stdout
-    assert "The first reversed() argument is not a sequence" not in output.stdout
+    assert "This custom error message is modified." in output.getvalue()
+    assert "The first reversed() argument is not a sequence" not in output.getvalue()
 
 
-def test_valid_message_config_no_section_header_and_no_pyta_overwrite():
+def test_valid_message_config_no_section_header_and_no_pyta_overwrite(output: io.StringIO):
     """Test the error messages given a valid messages-config-path with no section headers
-    and use-pyta-error-messages as False."""
-    output = subprocess.run(
-        [
-            sys.executable,
-            "-m",
-            "python_ta",
-            "--config",
-            "tests/test_messages_config/test_user_config_no_section_header_no_pyta_overwrite.pylintrc",
-            "tests/test_messages_config/testing_code.py",
-        ],
-        capture_output=True,
-        text=True,
-        env={**environ, "PYTHONIOENCODING": "utf-8"},
+    and use-pyta-error-messages as False.
+    """
+    python_ta.check_all(
+        "tests/test_messages_config/testing_code.py",
+        config="tests/test_messages_config/test_user_config_no_section_header_no_pyta_overwrite.pylintrc",
+        output=output,
     )
 
-    assert "This custom error message is modified." in output.stdout
-    assert "The first reversed() argument is not a sequence" in output.stdout
+    assert "This custom error message is modified." in output.getvalue()
+    assert "The first reversed() argument is not a sequence" in output.getvalue()
+
+
+def test_valid_message_config_no_section_header_and_incorrect_error_message(
+    output: io.StringIO, caplog
+):
+    """Test the error messages given a valid messages-config-path with no section headers
+    and use-pyta-error-messages as True.
+    """
+    python_ta.check_all(
+        "tests/test_messages_config/testing_code.py",
+        config="tests/test_messages_config/test_user_config_no_section_header_incorrect_error_message.pylintrc",
+        output=output,
+    )
+
+    assert "This custom error message is modified." in output.getvalue()
+    assert "The first reversed() argument is not a sequence" not in output.getvalue()
+    assert ("root", logging.WARNING, "A1234 is not a valid error id.") in caplog.record_tuples
+
+
+def test_valid_message_config_incorrect_section_header(output: io.StringIO):
+    """Test the error messages given a valid messages-config-path with incorrectly defined section headers
+    and use-pyta-error-messages as True. The error messages should still be overridden correctly as
+    all section headers are ignored.
+    """
+    python_ta.check_all(
+        "tests/test_messages_config/testing_code.py",
+        config="tests/test_messages_config/test_user_config_incorrect_section_header.pylintrc",
+        output=output,
+    )
+
+    assert "This custom error message is modified." in output.getvalue()
+    assert "The first reversed() argument is not a sequence" not in output.getvalue()

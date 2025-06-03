92a46ae81b91b597c98cb76e50813380fb4e179e
Towaiji <aliyasiraltowaiji@gmail.com>
2025-06-04 02:38:13 +0400

    Revert: Unnecessary refactoring in python_ta/transforms/z3_visitor.py

--- python_ta/transforms/z3_visitor.py
+++ python_ta/transforms/z3_visitor.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import astroid
-import z3
 from astroid import AstroidError, nodes
 from astroid.transforms import TransformVisitor
 from astroid.util import safe_infer
@@ -13,43 +12,43 @@ from ..z3.z3_parser import Z3ParseException, Z3Parser
 
 class Z3Visitor:
     """
-    Visits FunctionDef nodes, parses their Preconditions docstrings,
-    and attaches the resulting Z3 expressions to node.z3_constraints.
+    The class responsible for visiting astroid nodes (currently only FunctionDef nodes),
+    parsing preconditions, and converting them to z3 expressions to be appended in the
+    z3_constraints attribute of the node.
     """
 
     def __init__(self):
+        """Return a TransformVisitor that sets an environment for every node."""
         visitor = TransformVisitor()
+        # Register transforms
         visitor.register_transform(nodes.FunctionDef, self.set_function_def_z3_constraints)
         self.visitor = visitor
 
     def set_function_def_z3_constraints(self, node: nodes.FunctionDef):
-        # 1) Build a map of argument names to their inferred types.
-        types: dict[str, str] = {}
-        for ann, arg in zip(node.args.annotations, node.args.args):
+        # Parse types
+        types = {}
+        annotations = node.args.annotations
+        arguments = node.args.args
+        for ann, arg in zip(annotations, arguments):
             if ann is None:
                 continue
+            # TODO: what to do about subscripts ex. set[int], list[set[int]], ...
             inferred = safe_infer(ann)
             if isinstance(inferred, nodes.ClassDef):
                 types[arg.name] = inferred.name
-
-        # 2) Extract the list of precondition strings.
+        # Parse preconditions
         preconditions = parse_assertions(node, parse_token="Precondition")
-
-        # 3) Parse each one into a Z3 expression.
-        z3_constraints: list[z3.ExprRef] = []
+        # Get z3 constraints
+        z3_constraints = []
         for pre in preconditions:
+            pre = astroid.parse(pre).body[0]
+            parser = Z3Parser(types)
             try:
-                # Parse via astroid to get an Expr node,
-                # then hand it off to Z3Parser.
-                ast_node = astroid.parse(pre).body[0]
-                parser = Z3Parser(types)
-                transformed = parser.parse(ast_node)
+                transformed = parser.parse(pre)
             except (AstroidError, Z3Exception, Z3ParseException):
                 transformed = None
-
             if transformed is not None:
                 z3_constraints.append(transformed)
-
-        # Attach to the FunctionDef node
+        # Set z3 constraints
         node.z3_constraints = z3_constraints
         return node

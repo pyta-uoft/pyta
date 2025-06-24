9287cef0fbc4f7eb85de4246b0ae46e5164aceb3
Karl-Michaud <karl.michaud@mail.utoronto.ca>
2025-06-23 22:08:31 -0400

    Updated failing CI test AGAIN

--- tests/test_cfg/test_cfg_generator_z3.py
+++ tests/test_cfg/test_cfg_generator_z3.py
@@ -13,19 +13,19 @@ import python_ta.cfg.cfg_generator as cfg_generator
 def test_cfg_z3_enabled() -> None:
     """Test verifies that `generate_cfg` correctly creates a graphviz dot file with the expected content when z3
     enabled."""
-    script_name = "file_fixtures/my_file.py"
-    dot_file_path = os.path.splitext(os.path.basename(script_name))[0] + ".gv"
-    svg_file_path = dot_file_path[:-3] + ".svg"
+    file_path = "../../examples/pylint/r0912_too_many_branches.py"
+    gv_file_path = "r0912_too_many_branches.gv"
+    svg_file_path = "r0912_too_many_branches.svg"
 
     # Create the graphviz files using my_file.py
-    cfg_generator.generate_cfg(mod=script_name, auto_open=False, z3_enabled=True)
+    cfg_generator.generate_cfg(mod=file_path, z3_enabled=True)
 
-    # Open the actual graphviz file for reading
-    gv_file_io = open(dot_file_path)
+    # # Open the actual graphviz file for reading
+    gv_file_io = open(gv_file_path)
 
-    assert "my_file.gv" in gv_file_io.read()
+    assert gv_file_path in gv_file_io.read()
 
     # Teardown: close any open file IO and remove the graphviz generated files
     gv_file_io.close()
-    os.remove(dot_file_path)
+    os.remove(gv_file_path)
     os.remove(svg_file_path)

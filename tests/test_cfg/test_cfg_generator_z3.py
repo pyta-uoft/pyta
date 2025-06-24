200be551285ad58df996bb828f465e25b2e4b56a
Karl-Michaud <karl.michaud@mail.utoronto.ca>
2025-06-23 23:31:02 -0400

    Used monkey patch to bypass AttributeError occuring on GitHub CI

--- tests/test_cfg/test_cfg_generator_z3.py
+++ tests/test_cfg/test_cfg_generator_z3.py
@@ -3,7 +3,6 @@ Test suite for generating z3 control flow graphs using the PythonTA API.
 """
 
 import os.path
-from unittest.mock import patch
 
 import pytest
 
@@ -31,8 +30,20 @@ def test_cfg_z3_enabled() -> None:
     os.remove(svg_file_path)
 
 
-@patch.dict("sys.modules", {"python_ta.transforms.z3_visitor": None})
-def test_cfg_z3_failed_import() -> None:
-    """Test verifies that `generate_cfg` correctly handles import failure"""
+def test_cfg_z3_failed_import(monkeypatch) -> None:
+    """Test verifies that `generate_cfg` correctly handles import failure.
+
+    Note: Test uses monkeypatch since `unittest.mock` to avoid an AttributeError that occurs on GitHub CI.
+    """
+    original = __import__
+
+    def mock_import(name, *args):
+        if "z3_visitor" in name:
+            raise ImportError("Z3Visitor not available")
+        # Use original, since infinite recursion if __import__ is used directly for some reason
+        return original(name, *args)
+
+    monkeypatch.setattr("builtins.__import__", mock_import)
+
     with pytest.raises(ImportError):
         cfg_generator.generate_cfg(z3_enabled=True)

a075933b1aca4a9986b4c757bd53eaf95f6d9617
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2025-09-12 07:59:32 -0400

    Added various tests for node_printers.py functions

---
+++ examples/custom_checkers/e9989_pycodestyle/e261_no_error.py
@@ -0,0 +1,1 @@
+print("hello world")  # this is a comment with at least 2 whitespaces before it
---
+++ examples/custom_checkers/e9989_pycodestyle/e272_error.py
@@ -0,0 +1,4 @@
+x = 4
+y = 2
+if x < 3  or y == 1:  # multiple spaces before 'or' keyword
+    print('x is less than 3 and y is 1')

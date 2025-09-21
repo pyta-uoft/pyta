4279cb65c21b7fdb97b495935a1911ecc43b2b1a
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2025-09-20 22:27:09 -0400

    added more test cases

--- examples/custom_checkers/e9989_pycodestyle/e225_error.py
+++ examples/custom_checkers/e9989_pycodestyle/e225_error.py
@@ -7,3 +7,13 @@ if x==5:  # missing whitespace around multiple character operator
 
 if x>=5 or x<=5 or x!=5:  # missing whitespace around multicharacter operators
     print("x >= 5 or x <= 5 or x != 5")
+
+(x:=10)  # missing whitespace around multicharacter operator
+
+x+=7  # missing whitespace around multicharacter operator
+
+x>>=7  # missing whitespace around multicharacter operator
+
+x<<=7  # missing whitespace around multicharacter operator
+
+x**=8  # missing whitespace around multicharacter operator

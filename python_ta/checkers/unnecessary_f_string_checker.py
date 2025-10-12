39ddbda396f02a01a1f519e178ccb15e3dca3599
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2025-10-12 12:00:09 -0400

    added f-string checker

---
+++ python_ta/checkers/unnecessary_f_string_checker.py
@@ -0,0 +1,40 @@
+"""Checker to check for unncessary f-strings that only consist of unmodified variables"""
+
+from __future__ import annotations
+
+from astroid import nodes
+from pylint.checkers import BaseChecker
+from pylint.checkers.utils import only_required_for_messages
+from pylint.lint import PyLinter
+
+
+class FormattedStringChecker(BaseChecker):
+    """A checker class that reports unnecessary uses of f-strings when they can be replaced
+    with the variable directly"""
+
+    name = "f-string-checker"
+    msgs = {
+        "E9120": (
+            'Unnecessary use of f-strings in the expression "f{%s}". Use "str(%s)" instead.',
+            "f-string-checker",
+            "Used when the use of an f-string is unnecessary and can be replaced with the variable directly",
+        )
+    }
+
+    def visit_joinedstr(self, node: nodes.JoinedStr) -> None:
+        "Visits joined string"
+        if len(node.values) == 1 and isinstance(node.values[0], nodes.FormattedValue):
+            if node.values[0].conversion == -1 and node.values[0].format_spec is None:
+                if isinstance(node.values[0].value, nodes.Name):
+                    varname = node.values[0].value.name
+                    self.add_message(
+                        "f-string-checker",
+                        node=node,
+                        args=(varname, varname),
+                        line=node.lineno,
+                        col_offset=node.col_offset,
+                    )
+
+
+def register(linter: PyLinter) -> None:
+    linter.register_checker(FormattedStringChecker(linter))

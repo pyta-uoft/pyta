5d78f8c6d6072ad992ba55c339778e2b1a476e24
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2025-10-17 17:59:15 -0400

    updated formatting issues

--- python_ta/checkers/unnecessary_f_string_checker.py
+++ python_ta/checkers/unnecessary_f_string_checker.py
@@ -1,4 +1,4 @@
-"""Checker to check for unncessary f-strings that only consist of unmodified variables"""
+"""Checker to check for unncessary f-strings that only consist of single expressions"""
 
 from __future__ import annotations
 
@@ -10,29 +10,34 @@ from pylint.lint import PyLinter
 
 class FormattedStringChecker(BaseChecker):
     """A checker class that reports unnecessary uses of f-strings when they can be replaced
-    with the variable directly"""
+    with the expression directly"""
 
     name = "unnecessary-f-string"
     msgs = {
         "E9920": (
-            'Unnecessary use of f-strings in the expression "f{%s}". Use "str(%s)" instead.',
+            'Unnecessary use of an f-string in the expression `f"{%s}"`. Use `str(%s)` instead.',
             "unnecessary-f-string",
             "Used when the use of an f-string is unnecessary and can be replaced with the variable directly",
         )
     }
 
+    @only_required_for_messages("unnecessary-f-string")
     def visit_joinedstr(self, node: nodes.JoinedStr) -> None:
         "Visits joined string"
-        if len(node.values) == 1 and isinstance(node.values[0], nodes.FormattedValue):
-            if node.values[0].conversion == -1 and node.values[0].format_spec is None:
-                expression = node.values[0].value.as_string()
-                self.add_message(
-                    "unnecessary-f-string",
-                    node=node,
-                    args=(expression, expression),
-                    line=node.lineno,
-                    col_offset=node.col_offset,
-                )
+        if (
+            len(node.values) == 1
+            and isinstance(node.values[0], nodes.FormattedValue)
+            and node.values[0].conversion == -1
+            and node.values[0].format_spec is None
+        ):
+            expression = node.values[0].value.as_string()
+            self.add_message(
+                "unnecessary-f-string",
+                node=node,
+                args=(expression, expression),
+                line=node.lineno,
+                col_offset=node.col_offset,
+            )
 
 
 def register(linter: PyLinter) -> None:

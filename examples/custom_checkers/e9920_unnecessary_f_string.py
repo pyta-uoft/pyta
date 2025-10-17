fed91afbe12f6f75c71c3b319ad39a84e8c46672
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2025-10-17 07:40:40 -0400

    fixed more typos

--- examples/custom_checkers/e9920_unnecessary_f_string.py
+++ examples/custom_checkers/e9920_unnecessary_f_string.py
@@ -8,8 +8,8 @@ def demo_function() -> str:
     x = "hello"
     a = f"{x}"  # error on this line
 
-    b = f"{x =}"  # no error
+    b = f"{x =}"  # no error on this line
 
-    c = f"{x + ' world'}"  # error on this line
+    c = f"{x + ' world'}"  # no error on this line
 
     return x + a + c

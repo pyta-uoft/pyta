0cfbd79a5bf7c4294be596efa7c2e5c4f9e367af
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2025-10-29 03:59:57 +0300

    updated examples and test cases

--- examples/custom_checkers/e9930_simplifiable_if.py
+++ examples/custom_checkers/e9930_simplifiable_if.py
@@ -6,9 +6,20 @@ def examples(x: int) -> int:
     if x > 5:  # can be simplified 
         if x < 10:
             return x + 1
+
+    if x < 4:
+        x = 2
     elif x < -5:  # can be simplified 
         if x > -10:
             return x - 1
-    elif x == 0:  # cannot be simplified
-        return 0
+    
+    if x < 4:  # cannot be simplified
+        if x > 15:
+            x = 2
+    elif x < -5:  # cannot be simplified 
+        if x > -10:
+            return x - 1
+    else:
+        x = 5
+    
     return x
--- tests/test_custom_checkers/test_simplifiable_if_checker.py
+++ tests/test_custom_checkers/test_simplifiable_if_checker.py
@@ -15,10 +15,6 @@ class TestSimplifiableIfChecker(pylint.testutils.CheckerTestCase):
         if x > 5:
             if x < 10:
                 x += 1
-        elif x < -5:
-            x -= 5
-        else:
-            x = 50
         """
 
         mod = parse(src)
@@ -43,8 +39,6 @@ class TestSimplifiableIfChecker(pylint.testutils.CheckerTestCase):
         elif x < -5: #@
             if x > -10:
                 x -= 1
-        else:
-            x = 50
         """
 
         if_node = extract_node(src)
@@ -55,6 +49,33 @@ class TestSimplifiableIfChecker(pylint.testutils.CheckerTestCase):
             ignore_position=True,
         ):
             self.checker.visit_if(if_node)
+
+    def test_doubly_nested_elif(self) -> None:
+        src = """
+        x = 10
+        if x > 1:
+            x = 5
+        elif x < -2: #@
+            if x > -1:
+                if x == 0:
+                    x = 3
+        """
+        elif_node = extract_node(src)
+        with self.assertAddsMessages(
+            pylint.testutils.MessageTest(
+                msg_id="simplifiable-if",
+                node=elif_node.body[0].test,
+                line=5,
+            ),
+            pylint.testutils.MessageTest(
+                msg_id="simplifiable-if",
+                node=elif_node.body[0].body[0].test,
+                line=6,
+            ),
+            ignore_position=True,
+        ):
+            self.checker.visit_if(elif_node)
+            self.checker.visit_if(elif_node.body[0])
 
     def test_doubly_nested_if(self) -> None:
         src = """
@@ -99,3 +120,21 @@ class TestSimplifiableIfChecker(pylint.testutils.CheckerTestCase):
         if_node, *_ = mod.nodes_of_class(nodes.If)
         with self.assertNoMessages():
             self.checker.visit_if(if_node)
+
+    def test_no_simplification_multiple_branches(self) -> None:
+        src = """
+        x = 10
+        if x > 1: #@
+            if x < 1:
+                x = 5
+        elif x < -2: #@
+            if x > -1:
+                if x == 0:
+                    x = 3
+        else:
+            x = 5
+        """
+        if_node, elif_node = extract_node(src)
+        with self.assertNoMessages():
+            self.checker.visit_if(if_node)
+            self.checker.visit_if(elif_node)

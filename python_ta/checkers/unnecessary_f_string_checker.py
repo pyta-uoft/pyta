c46a0482ff2128fcb309ec8e670eb0f160470324
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2026-01-14 14:54:47 -0500

    wrap user input in longer backticks

--- python_ta/checkers/unnecessary_f_string_checker.py
+++ python_ta/checkers/unnecessary_f_string_checker.py
@@ -15,7 +15,7 @@ class FormattedStringChecker(BaseChecker):
     name = "unnecessary-f-string"
     msgs = {
         "E9920": (
-            'Unnecessary use of an f-string in the expression `f"{%s}"`. Use `str(%s)` instead.',
+            "Unnecessary use of an f-string in the expression %s. Use %s instead.",
             "unnecessary-f-string",
             "Used when the use of an f-string is unnecessary and can be replaced with the variable directly",
         )
@@ -30,13 +30,37 @@ class FormattedStringChecker(BaseChecker):
             and node.values[0].format_spec is None
         ):
             expression = node.values[0].value.as_string()
+            max_backtick_count = self.backtick_count(expression)
+            f_string_message = (
+                f'{'`' * max_backtick_count}f"{expression}"{'`' * max_backtick_count}'
+            )
+            str_message = f"{'`' * max_backtick_count}str({expression}){'`' * max_backtick_count}"
             self.add_message(
                 "unnecessary-f-string",
                 node=node,
-                args=(expression, expression),
+                args=(f_string_message, str_message),
                 line=node.lineno,
                 col_offset=node.col_offset,
             )
+
+    def backtick_count(self, input_str: str) -> int:
+        """
+        Return the length of the longest substring of consecutive backticks in input_str + 1
+        """
+        max_length = 0
+        last_backtick = False
+        for char in input_str:
+            if char == "`":
+                if last_backtick:
+                    curr_length += 1
+                else:
+                    curr_length = 1
+                    last_backtick = True
+            elif last_backtick:
+                if max_length < curr_length:
+                    max_length = curr_length
+                last_backtick = False
+        return max_length + 1
 
 
 def register(linter: PyLinter) -> None:

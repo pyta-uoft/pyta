ca0f1d6580a4b8af493e3b85f76b171acc53b917
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2026-01-20 20:34:53 -0500

    updated jinja template to unescape input

--- packages/python-ta/src/python_ta/reporters/html_reporter.py
+++ packages/python-ta/src/python_ta/reporters/html_reporter.py
@@ -2,6 +2,7 @@ import base64
 import os
 import socket
 import sys
+from html import unescape
 
 from jinja2 import Environment, FileSystemLoader
 from markdown_it import MarkdownIt
@@ -108,6 +109,7 @@ class HTMLReporter(PythonTaReporter):
             md=md,
             pyta_logo_data_url=pyta_logo_data_url,
             favicon_data_url=favicon_data_url,
+            unescape=unescape,
         )
 
         # If a filepath was specified, write to the file

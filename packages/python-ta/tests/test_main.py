a942e72bbe8ea34c2e733a3c9399a97bc92ec12a
PraneethS42 <spraneeth42@gmail.com>
2026-01-21 17:38:38 -0500

    Refactored for click's testing helpers

--- packages/python-ta/tests/test_main.py
+++ packages/python-ta/tests/test_main.py
@@ -2,11 +2,12 @@
 installed `python_ta` package.
 """
 
-import subprocess
-import sys
-from os import environ, path
+from os import path
+
+from click.testing import CliRunner
 
 import python_ta
+from python_ta.__main__ import main
 from python_ta.config import DEFAULT_CONFIG_LOCATION
 
 SOURCE_ROOT = path.normpath(path.join(path.dirname(__file__), "../../.."))
@@ -16,88 +17,78 @@ TEST_CONFIG = path.join(TEST_ROOT, "test.pylintrc")
 
 def test_check_no_errors_zero() -> None:
     """Test that python_ta exits with status code 0 when it does not detect errors."""
-    output = subprocess.run(
-        [
-            sys.executable,
-            "-m",
-            "python_ta",
+    runner = CliRunner()
+    output = runner.invoke(
+        main,
+        [  # type: ignore
             "--config",
             TEST_CONFIG,
             path.join(TEST_ROOT, "fixtures", "no_errors.py"),
-        ]
+        ],
     )
 
-    assert output.returncode == 0
+    assert output.exit_code == 0
 
 
 def test_check_errors_nonzero() -> None:
     """Test that python_ta exits with non-zero status code when it detects errors."""
-    output = subprocess.run(
-        [
-            sys.executable,
-            "-m",
-            "python_ta",
+    runner = CliRunner()
+    output = runner.invoke(
+        main,
+        [  # type: ignore
             "--config",
             TEST_CONFIG,
             path.join(SOURCE_ROOT, "examples", "nodes", "name.py"),
-        ]
+        ],
     )
 
-    assert output.returncode != 0
+    assert output.exit_code != 0
 
 
 def test_check_exit_zero() -> None:
     """Test that python_ta --exit-zero always exits with status code 0,
     even when given a file with errors.
     """
-    output = subprocess.run(
-        [
-            sys.executable,
-            "-m",
-            "python_ta",
+    runner = CliRunner()
+    output = runner.invoke(
+        main,
+        [  # type: ignore
             "--exit-zero",
             "--config",
             TEST_CONFIG,
             path.join(SOURCE_ROOT, "examples", "nodes", "name.py"),
         ],
-        env={**environ, "PYTHONIOENCODING": "utf-8"},
     )
 
-    assert output.returncode == 0
+    assert output.exit_code == 0
 
 
 def test_check_version() -> None:
     """Test that python_ta --version outputs python_ta.__version__ to stdout."""
-    stdout = subprocess.run(
-        [
-            sys.executable,
-            "-m",
-            "python_ta",
+    runner = CliRunner()
+    result = runner.invoke(
+        main,
+        [  # type: ignore
             "--config",
             TEST_CONFIG,
             "--version",
         ],
-        capture_output=True,
-        text=True,
-    ).stdout
+    )
 
-    assert stdout.rstrip("\n") == python_ta.__version__
+    assert result.output.rstrip("\n") == python_ta.__version__
 
 
 def test_config_generation() -> None:
     """Test that python_ta --generate-config prints the default config to stdout."""
-    generated_config = subprocess.run(
-        [
-            sys.executable,
-            "-m",
-            "python_ta",
+    runner = CliRunner()
+    result = runner.invoke(
+        main,
+        [  # type: ignore
             "--generate-config",
             "--config",
             TEST_CONFIG,
         ],
-        capture_output=True,
-        text=True,
-    ).stdout
+    )
 
     pylintrc_location = path.join(
         SOURCE_ROOT,
@@ -110,7 +101,7 @@ def test_config_generation() -> None:
     with open(pylintrc_location, "r") as f:
         actual_config = f.read()
 
-    generated_config = generated_config[:-1]  # Remove trailing newline
+    generated_config = result.output[:-1]  # Remove trailing newline
 
     assert generated_config == actual_config
 
@@ -119,15 +110,14 @@ def test_no_config() -> None:
     """Test that python_ta exits with status code 0 when it does not detect errors
     and no config is specified.
     """
-    output = subprocess.run(
-        [
-            sys.executable,
-            "-m",
-            "python_ta",
+    runner = CliRunner()
+    output = runner.invoke(
+        main,
+        [  # type: ignore
             "--output-format",
             "pyta-plain",
             path.join(TEST_ROOT, "fixtures", "no_errors.py"),
         ],
     )
 
-    assert output.returncode == 0
+    assert output.exit_code == 0

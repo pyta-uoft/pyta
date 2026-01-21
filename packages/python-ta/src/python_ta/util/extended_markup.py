4b89157a7c508fd22350ddbb4d89f381f71791dd
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2026-01-20 20:32:34 -0500

    created extended markup class

---
+++ packages/python-ta/src/python_ta/util/extended_markup.py
@@ -0,0 +1,39 @@
+from markupsafe import Markup
+
+
+class ExtendedMarkup(Markup):
+    """
+    Extends the standard Markup class defined in markupsafe to include escaping of markdown characters
+    """
+
+    @classmethod
+    def escape(cls, s):
+        """
+        Escape all markdown characters in s by replacing them with their corresponding escape sequence
+        """
+        markdown_chars = [
+            "#",  # since the escape sequence contains a #, we should escape the # symbol first to avoid escaping the hash in the escape sequence
+            "\\",
+            "`",
+            "*",
+            "_",
+            "{",
+            "}",
+            "[",
+            "]",
+            "(",
+            ")",
+            ".",
+            "!",
+            "|",
+            ">",
+            "~",
+            "<",
+            ">",
+            ":",
+        ]
+        for char in markdown_chars:
+            ascii = ord(char)
+            esc_sequence = f"&#{ascii};"
+            s = s.replace(char, esc_sequence)
+        return s

f6022a872308654c5d637b302c7b8d85d65ecd64
PraneethS42 <spraneeth42@gmail.com>
2026-01-22 23:54:51 -0500

    Formatting changes and disabled line coverage

--- packages/python-ta/tests/test_main.py
+++ packages/python-ta/tests/test_main.py
@@ -19,7 +19,7 @@ def test_check_no_errors_zero() -> None:
     """Test that python_ta exits with status code 0 when it does not detect errors."""
     runner = CliRunner()
     output = runner.invoke(
-        main,  # type: ignore
+        main,
         [
             "--config",
             TEST_CONFIG,
@@ -34,7 +34,7 @@ def test_check_errors_nonzero() -> None:
     """Test that python_ta exits with non-zero status code when it detects errors."""
     runner = CliRunner()
     output = runner.invoke(
-        main,  # type: ignore
+        main,
         [
             "--config",
             TEST_CONFIG,
@@ -51,7 +51,7 @@ def test_check_exit_zero() -> None:
     """
     runner = CliRunner()
     output = runner.invoke(
-        main,  # type: ignore
+        main,
         [
             "--exit-zero",
             "--config",
@@ -67,7 +67,7 @@ def test_check_version() -> None:
     """Test that python_ta --version outputs python_ta.__version__ to stdout."""
     runner = CliRunner()
     result = runner.invoke(
-        main,  # type: ignore
+        main,
         [
             "--config",
             TEST_CONFIG,
@@ -82,7 +82,7 @@ def test_config_generation() -> None:
     """Test that python_ta --generate-config prints the default config to stdout."""
     runner = CliRunner()
     result = runner.invoke(
-        main,  # type: ignore
+        main,
         [
             "--generate-config",
             "--config",
@@ -112,7 +112,7 @@ def test_no_config() -> None:
     """
     runner = CliRunner()
     output = runner.invoke(
-        main,  # type: ignore
+        main,
         [
             "--output-format",
             "pyta-plain",

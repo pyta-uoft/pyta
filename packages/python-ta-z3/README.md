c792cc5cc8147d680a89591d2034ecb8c99c2f68
David Liu <david@cs.toronto.edu>
2026-01-25 18:43:54 -0500

    Create python-ta-z3 package

---
+++ packages/python-ta-z3/README.md
@@ -0,0 +1,1 @@
+# python-ta-z3
--- packages/python-ta/tests/test_custom_checkers/test_redundant_assignment_checker.py
+++ packages/python-ta/tests/test_custom_checkers/test_redundant_assignment_checker.py
@@ -1,10 +1,10 @@
 import astroid
 import pylint.testutils
 from astroid import nodes
+from python_ta_z3 import Z3Visitor
 
 from python_ta.cfg import CFGVisitor
 from python_ta.checkers.redundant_assignment_checker import RedundantAssignmentChecker
-from python_ta.transforms.z3_visitor import Z3Visitor
 
 
 class TestRedundantAssignmentChecker(pylint.testutils.CheckerTestCase):

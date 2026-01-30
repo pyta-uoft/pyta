39a411ff0f7f47a19ac357547fda2eead7663524
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2026-01-29 22:13:46 -0500

    fix bug and add annotations

--- packages/python-ta/src/python_ta/util/extended_markup.py
+++ packages/python-ta/src/python_ta/util/extended_markup.py
@@ -1,37 +1,38 @@
+from typing import Self
+
 from markupsafe import Markup
+
+markdown_chars = [
+    "#",
+    "\\",
+    "`",
+    "*",
+    "_",
+    "{",
+    "}",
+    "[",
+    "]",
+    "(",
+    ")",
+    ".",
+    "!",
+    "|",
+    "~",
+    ":",
+]
 
 
 class ExtendedMarkup(Markup):
-    """
-    Extends the standard Markup class defined in markupsafe to include escaping of markdown characters
-    """
+    """Extends the standard Markup class defined in markupsafe to include escaping of markdown characters"""
 
     @classmethod
-    def escape(cls, s):
-        """
-        Escape all markdown characters in s by replacing them with their corresponding escape sequence
-        """
+    def escape(cls: type[Self], s: str) -> str:
+        """Escape all markdown characters in s by replacing them with their corresponding escape sequence"""
         if isinstance(s, str):
-            markdown_chars = [
-                "#",  # since the escape sequence contains a #, we should escape the # symbol first to avoid escaping the hash in the escape sequence
-                "\\",
-                "`",
-                "*",
-                "_",
-                "{",
-                "}",
-                "[",
-                "]",
-                "(",
-                ")",
-                ".",
-                "!",
-                "|",
-                "~",
-                ":",
-            ]
             for char in markdown_chars:
                 ascii = ord(char)
-                esc_sequence = f"𝔄#{ascii};"
+                esc_sequence = f"\ue000#{ascii};"
                 s = s.replace(char, esc_sequence)
-            return s
+                return s
+        else:
+            return str(s)

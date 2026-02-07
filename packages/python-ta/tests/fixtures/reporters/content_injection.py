debf8d06a381a5f1fa8ae184dfed1c6fa87a5309
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2026-02-07 12:06:18 -0500

    create fixture files

--- packages/python-ta/tests/fixtures/reporters/content_injection.py
+++ packages/python-ta/tests/fixtures/reporters/content_injection.py
@@ -1,17 +1,8 @@
 """This script serves as the entry point for the PyTA HTML reporter tests.
-It is invoked by the test `tests/test_reporters/test_html_report::test_injection()`
+It is invoked by the test `tests/test_reporters/test_html_reporter::test_injection()`
 to ensure that no HTML can be injected into the HTML report."""
-from unittest.mock import patch
 
-from python_ta import check_all
-
-@patch("webbrowser.open", return_value=None)
-def inject_script(mock_webbrowser_open):
+def inject_script():
     a = "hello"
     x = f'{a + "<script>alert(2);</script>"}'
-    check_all(config={
-        "watch": True,
-        "server_port": 2000
-    })
-
-inject_script()
+    return x

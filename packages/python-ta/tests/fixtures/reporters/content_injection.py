a344a8e9f9b5552f59fb61bac5f5ba1e8488ccdb
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2026-02-08 23:57:15 -0500

    fix formatting

--- packages/python-ta/tests/fixtures/reporters/content_injection.py
+++ packages/python-ta/tests/fixtures/reporters/content_injection.py
@@ -2,6 +2,7 @@
 It is invoked by the test `tests/test_reporters/test_html_reporter::test_injection()`
 to ensure that no HTML can be injected into the HTML report."""
 
+
 def inject_script():
     a = "hello"
     x = f'{a + "<script>alert(2);</script>"}'

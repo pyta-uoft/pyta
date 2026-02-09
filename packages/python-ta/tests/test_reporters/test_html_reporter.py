a1a43a201e9218d1d8cad79565a9fd6ec4b9b2cb
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2026-02-08 23:58:27 -0500

    update snapshots and output reading

--- packages/python-ta/tests/test_reporters/test_html_reporter.py
+++ packages/python-ta/tests/test_reporters/test_html_reporter.py
@@ -1,7 +1,6 @@
 import os
 import re
-
-from tests.test_reporters.test_html_server import wait_for_server
+from io import StringIO
 
 from python_ta import check_all
 
@@ -27,41 +26,43 @@ def clean_response_body(body) -> str:
 
 def test_injection(snapshot):
     """Test the HTML injection is properly escaped and is not executed as HTML code"""
+
     script_path = os.path.normpath(
         os.path.join(__file__, "../../fixtures/reporters/content_injection.py")
     )
-    output_path = os.path.normpath(
-        os.path.join(__file__, "../../test_reporters/output_files/injection_script_output.html")
-    )
-    check_all(module_name=script_path, output=output_path)
 
-    with open(output_path, "r") as output_file:
+    buf = StringIO()
 
-        response_body = output_file.read()
-        cleaned_body = clean_response_body(response_body)
+    check_all(module_name=script_path, output=buf)
 
-        assert ESCAPED_SCRIPT in cleaned_body
-        assert UNESCAPED_SCRIPT not in cleaned_body
+    buf.seek(0)
 
-        snapshot.assert_match(cleaned_body, "script_injection.html")
+    response_body = buf.read()
+    cleaned_body = clean_response_body(response_body)
+
+    assert ESCAPED_SCRIPT in cleaned_body
+    assert UNESCAPED_SCRIPT not in cleaned_body
+
+    snapshot.assert_match(cleaned_body, "script_injection.html")
 
 
 def test_markdown_escape(snapshot):
     """Test markdown characters in error messages are properly escaped"""
+
     script_path = os.path.normpath(
         os.path.join(__file__, "../../fixtures/reporters/markdown_escape_script.py")
     )
-    output_path = os.path.normpath(
-        os.path.join(__file__, "../../test_reporters/output_files/markdown_escape_output.html")
-    )
-    check_all(module_name=script_path, output=output_path)
 
-    with open(output_path, "r") as output_file:
+    buf = StringIO()
 
-        response_body = output_file.read()
-        cleaned_body = clean_response_body(response_body)
+    check_all(module_name=script_path, output=buf)
 
-        assert ESCAPED_MARKDOWN in cleaned_body
-        assert UNESCAPED_MARKDOWN not in cleaned_body
+    buf.seek(0)
 
-        snapshot.assert_match(cleaned_body, "markdown_escape.html")
+    response_body = buf.read()
+    cleaned_body = clean_response_body(response_body)
+
+    assert ESCAPED_MARKDOWN in cleaned_body
+    assert UNESCAPED_MARKDOWN not in cleaned_body
+
+    snapshot.assert_match(cleaned_body, "markdown_escape.html")

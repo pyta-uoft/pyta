8dc3ceb1864208357cd420c05493fc41de77ef3a
PraneethS42 <spraneeth42@gmail.com>
2026-02-21 18:32:13 -0500

    Fixes

--- packages/python-ta/src/python_ta/checkers/contract_checker.py
+++ packages/python-ta/src/python_ta/checkers/contract_checker.py
@@ -10,7 +10,7 @@ from pylint.checkers import BaseChecker
 from pylint.checkers.utils import only_required_for_messages
 from pylint.lint import PyLinter
 
-from ..contracts import parse_assertions
+from ..contracts import FUNCTION_RETURN_VALUE, parse_assertions
 
 
 class ContractChecker(BaseChecker):
@@ -32,30 +32,32 @@ class ContractChecker(BaseChecker):
 
     @only_required_for_messages("invalid-precondition-syntax", "invalid-postcondition-syntax")
     def visit_functiondef(self, node: nodes.FunctionDef) -> None:
-        """Visit function definition and check preconditions in docstring."""
+        """Visit function definition and check preconditions and postconditions in docstring."""
 
         if not node.doc_node:
             return
 
         preconditions = parse_assertions(node, parse_token="Precondition")
         for precondition in preconditions:
-            cleaned_condition = re.sub(r"\s+", " ", precondition)
+            cleaned_precondition = re.sub(r"\s+", " ", precondition)
             try:
-                ast.parse(cleaned_condition, mode="eval")
+                ast.parse(cleaned_precondition, mode="eval")
             except SyntaxError:
                 self.add_message(
-                    "invalid-precondition-syntax", node=node, args=(cleaned_condition,)
+                    "invalid-precondition-syntax", node=node, args=(cleaned_precondition,)
                 )
 
         postconditions = parse_assertions(node, parse_token="Postcondition")
         for postcondition in postconditions:
-            cleaned_condition = re.sub(r"\s+", " ", postcondition)
-            parseable_condition = re.sub(r"\$return_value", "_return_value", cleaned_condition)
+            cleaned_postcondition = re.sub(r"\s+", " ", postcondition)
+            parseable_condition = cleaned_postcondition.replace(
+                FUNCTION_RETURN_VALUE, "_return_value"
+            )
             try:
                 ast.parse(parseable_condition, mode="eval")
             except SyntaxError:
                 self.add_message(
-                    "invalid-postcondition-syntax", node=node, args=(cleaned_condition,)
+                    "invalid-postcondition-syntax", node=node, args=(cleaned_postcondition,)
                 )
 
 

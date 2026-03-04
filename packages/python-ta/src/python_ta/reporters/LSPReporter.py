8910015116a3eba1277bf4c454defa2b8e890f66
PraneethS42 <spraneeth42@gmail.com>
2026-03-03 23:02:20 -0500

    bug fixes and serialization

--- packages/python-ta/src/python_ta/reporters/LSPReporter.py
+++ packages/python-ta/src/python_ta/reporters/LSPReporter.py
@@ -5,7 +5,7 @@ from lsprotocol import types
 from pylint.lint import PyLinter
 from pylint.reporters.ureports.nodes import BaseLayout
 
-from .core import PythonTaReporter
+from .core import NewMessage, PythonTaReporter
 
 category_to_lsp = {
     "error": types.DiagnosticSeverity.Error,
@@ -21,28 +21,36 @@ def _lsp_severity(category: str) -> types.DiagnosticSeverity:
     return category_to_lsp.get(category, types.DiagnosticSeverity.Warning)
 
 
+def _serialize(inst, field, value):
+    if isinstance(value, types.DiagnosticSeverity):
+        return value.value
+    return value
+
+
 class LSPReporter(PythonTaReporter):
 
     name = "pyta-lsp"
     OUTPUT_FILENAME = "pyta_lsp_report.json"
+    messages: dict[str, list[NewMessage]]
 
     def display_messages(self, layout: BaseLayout) -> None:
-
         json_output = {}
         for filename, msgs in self.gather_messages().items():
             diagnostics_list = []
             for msg in msgs:
                 diag = types.Diagnostic(
                     range=types.Range(
-                        start=types.Position(line=msg.line, character=msg.column),
-                        end=types.Position(line=msg.end_line, character=msg.end_column),
+                        start=types.Position(line=msg.line - 1, character=msg.column or 0),
+                        end=types.Position(
+                            line=(msg.end_line or msg.line) - 1, character=msg.end_column or 0
+                        ),
                     ),
                     message=msg.msg,
                     severity=_lsp_severity(msg.category),
                     code=msg.msg_id,
                     source="python-ta",
                 )
-                diagnostics_list.append(attrs.asdict(diag))
+                diagnostics_list.append(attrs.asdict(diag, value_serializer=_serialize))
             json_output[filename] = diagnostics_list
         self.writeln(json.dumps(json_output, indent=4))
         self.out.flush()

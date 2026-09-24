53023846d372305cbd025f54fe5c777154b67309
David Liu <david@cs.toronto.edu>
2026-09-24 07:15:29 -0400

    Removed unnecessary _overrid_check_invalid_name_in_main patch (#1394)

--- packages/python-ta/src/python_ta/patches/checkers.py
+++ packages/python-ta/src/python_ta/patches/checkers.py
@@ -1,16 +1,12 @@
 """Patch pylint checker behaviour."""
 
-from pylint.checkers.base import NameChecker
 from pylint.checkers.classes import ClassChecker
 from pylint.checkers.utils import node_frame_class
-
-from python_ta.utils import _is_in_main
 
 
 def patch_checkers():
     """Run patches to modify built-in pylint checker behaviour."""
     _override_check_protected_attribute_access()
-    _override_check_invalid_name_in_main()
 
 
 def _override_check_protected_attribute_access():
@@ -36,23 +32,3 @@ def _override_check_protected_attribute_access():
             old_check_protected_attribute_access(self, node)
 
     ClassChecker._check_protected_attribute_access = _check
-
-
-def _override_check_invalid_name_in_main():
-    """Override invalid-name check for variables in main block.
-
-    pylint normally complains about variable names in the main block
-    that aren't in ALL_CAPS -- in other words, it assumes that all such
-    variables should be constants. We disable this check here so that
-    non-constant variable names are permitted (encourages experimentation
-    in the main block).
-    """
-    old_visit_assignname = NameChecker.visit_assignname
-
-    def patched_visit_assignname(self, node):
-        if _is_in_main(node):
-            self._check_name("variable", node.name, node)
-        else:
-            old_visit_assignname(self, node)
-
-    NameChecker.visit_assignname = patched_visit_assignname
